463c12f86b7e5082ce47f6c94f6b7bfaeb053992
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-04-02 11:44:23 +0200

    rm

--- config/salts.php
+++
@@ -1,9 +0,0 @@
-<?php 
-define('AUTH_KEY',         'NS] +^+ Y#7z!GgHE[x1HqU2_L7XI20`:8d|+`^?&qodjT<p43EEBJI&p)`[MWxk');
-define('SECURE_AUTH_KEY',  ')$ZzB;]%B<3Q~Kok24y}Oq$aT-H!z |C,Ts#%#+%QDcv|P;cH&z&$z?u.{XD?Y{o');
-define('LOGGED_IN_KEY',    '&#/z|lET1D+P),tzYRJQ-YJ|]a?LrEFxJZ-<m;`1|HElzCYP02?hUkv+^/oV%%?r');
-define('NONCE_KEY',        '`VRZpANe(a5|)lzMY4Ku0F~dY.-CFM?ul{eWp@ NxWV3j1&pHt7 qR-:FFZE`|^H');
-define('AUTH_SALT',        '`8*b%c^$,h$:+Y[s5-e?6[m8]^t4T}&~g!h| {>*}L&ZOq;O2namy +0qV+^x<Y7');
-define('SECURE_AUTH_SALT', 'yTKOF2~=K7_Yp=2b-[;`nQ@*o1NO9}9(BpiP24&`-=$P.^UGw3w1(kI9BXsi|Rr9');
-define('LOGGED_IN_SALT',   'XlrIxv8#%y%D;!+wnQxx~[l7K!Lp=#DXj_K717[:qklCt1+~Pkt+iUE]l3az8TV=');
-define('NONCE_SALT',       '{wHhCiLz:/nWE+:7-DeE#|~vOnqBAIK.|Ow}THQb)pn6-eDzBhJRU3uSDqHd%dO{');
