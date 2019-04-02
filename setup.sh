0ffb703898caca3b3088dc04f1987d2917f89122
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-02 12:56:45 +0200

    Adds sub-repository and created install file.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git clone "git@github.com:helsingborg-stad/blade-component-library.git" "source/library"
+if [ -d "source/library" ]; then
+    echo Library repository already installed in source/library. You might want to add this in your git-client-ui.
+else
+    git clone "git@github.com:helsingborg-stad/blade-component-library.git" "source/library"
+fi
