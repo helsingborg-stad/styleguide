c99118b0b424fdd83991abd46e5d0b5becc2e906
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-03 12:28:56 +0200

    Q&D logo fix

--- views/layout/master.blade.php
+++ views/layout/master.blade.php
@@ -33,7 +33,7 @@
         <div class="container">
             <div class="grid">
                 <div class="grid-md-3">
-                    <a href="/"><img id="logotype" src="/assets/img/logotype.svg" alt="Helsingborg Stad" height="35"></a>
+                    <a href="/"><img id="logotype" src="/assets/img/logotype.svg" alt="Helsingborg Stad" height="35" style="margin: 20px 0 12px 0"></a>
                 </div>
             </div>
         </div>
@@ -70,25 +70,15 @@
         <div class="container">
             <div class="grid">
                 <div class="grid-lg-12">
-                    <a href="/" class="logotype"><img src="/logotype.svg" alt="Helsingborg Stad" width="239" height="68"></a>
+                    <a href="/" class="logotype"><img src="/assets/img/logotype.svg" alt="Helsingborg Stad" width="239" height="68"></a>
                 </div>
             </div>
             <div class="grid">
                 <div class="grid-lg-6">
-                    <ul>
-                        <li><strong>Telefonnummer</strong></li>
-                        <li>Helsingborg kontaktcenter: 042-10 50 00</li>
-                    </ul>
-                    <ul>
-                        <li><strong>E-post</strong></li>
-                        <li><a href="mailto:kontaktcenter@helsingborg.se" class="link-item link-item-light">kontaktcenter@helsingborg.se</a></li>
-                    </ul>
+                    
                 </div>
                 <div class="grid-lg-6">
-                    <ul>
-                        <li><strong>Postadress</strong></li>
-                        <li>Namn på verksamheten<br>251 89 Helsingborg</li>
-                    </ul>
+             
                 </div>
             </div>
         </div>
