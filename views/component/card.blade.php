a2497f9d09048fabbdb0f9cc5836e6bee67b8f8f
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-04 15:14:15 +0200

    Test view to render a docblock

--- views/component/card.blade.php
+++ views/component/card.blade.php
@@ -9,34 +9,10 @@
         ')
     !!}
 
-    <div class="markup-preview">
-        <div class="grid">
-            <div class="grid-md-6">
-                {!! component('card') !!}
-            </div>
-            <div class="grid-md-6">
-                {!! component('card') !!}
-            </div>
-        </div>
-    </div>
-
-    <pre><code>
-        <div class="grid">
-            <div class="grid-md-6">
-                {{ component('card') }}
-            </div>
-            <div class="grid-md-6">
-                {{ component('card') }}
-            </div>
-        </div>
-    </code></pre>
-
-    <div class="code-source-file clearfix">
-        <div class="pull-left"><strong>Source file:</strong> ~/source/sass/components/card/_card.scss</div>
-        <div class="pull-right">
-            <a class="link-item link-item-github link-item-outbound" href="https://github.com/helsingborg-stad/styleguide-web/blob/master/source/sass/components/card/_card.scss">View source on GitHub</a>
-        </div>
-    </div>
-
+    @include('layout.doc', ['markup' => component('card')])
 
 @stop
+
+
+
+
---
+++ views/layout/doc.blade.php
@@ -0,0 +1,7 @@
+<section class="example">
+    <h3>Example</h3>
+    <div class="markup-preview">
+    {!! $markup !!}
+    </div>
+    <pre><code>{{ $markup }}</code></pre>
+</section>
