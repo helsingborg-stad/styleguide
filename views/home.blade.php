ec3fbedbadea6ef9f877e3534f2bae94cfd87754
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-12 14:17:06 +0200

    Cards on fron page, equal

--- views/home.blade.php
+++ views/home.blade.php
@@ -15,5 +15,32 @@
 
     ") !!}
 
+    <div class="grid" data-equal-container>
+        <div class="grid-xs-12 grid-md-4">
+            @component_card([
+                'image' => 'https://picsum.photos/300/200?image=919',
+                'title' => 'Usability', 
+                'content' => 'On of the main focus of this styleguide is usability. Read more of our guidelines here.'
+            ])
+        </div>
+
+        <div class="grid-xs-12 grid-md-4">
+            @component_card([
+                'image' => 'https://picsum.photos/300/200?image=743',
+                'title' => 'Blade components', 
+                'content' => 'This styleguide is in many parts crated with our own blade component library to enable swift development and maintaince.'
+            ])
+        </div>
+
+        <div class="grid-xs-12 grid-md-4">
+            @component_card([
+                'image' => 'https://picsum.photos/300/200?image=455',
+                'title' => 'Authors', 
+                'content' => 'The styleguide and lirbraries where created by the awesome development team @ helsingborg stad.'
+            ])
+        </div>
+
+    </div>
+
 </article>
 @stop
