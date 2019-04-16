80b6757544f233908ced09b8a5b9599f5650b4c1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-16 10:55:59 +0200

    Accordion documentation

--- views/pages/component/accordion.blade.php
+++ views/pages/component/accordion.blade.php
@@ -1,11 +1,22 @@
 @extends('layout.master')
 
 @section('content')
-    {!!
-        markdown('
-            #Blank
+    @markdown
+        #Accordion
+        The accordion can be used to display larger lists of content on one page. 
+    @endmarkdown
 
-            Demo page
-        ')
-    !!}
+    @doc(['slug' => 'accordion'])
+
+        @accordion([
+            'list'=> [
+                ['heading' => "Mattis Risus Tristique Commodo", 'content' => "Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor."],
+                ['heading' => "Mattis Risus Tristique Commodo", 'content' => "Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor."],
+                ['heading' => "Mattis Risus Tristique Commodo", 'content' => "Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor."],
+                ['heading' => "Mattis Risus Tristique Commodo", 'content' => "Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor."]
+            ]
+        ])
+        @endaccordion
+
+    @enddoc
 @stop
