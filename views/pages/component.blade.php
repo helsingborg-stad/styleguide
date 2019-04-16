62667923895d410d536e6a8f35e14425e7dc0835
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-16 15:55:20 +0200

    Why use components

--- views/pages/component.blade.php
+++ views/pages/component.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.master')
 
 @section('content')
+    @markdown
+        #Components
+        The component library is a collection of robust views with builtin logic to handle common scenarios. 
 
-    {!! markdown('
-        
-        #Component
+        ##Why use components? 
+        The basic purpose of the component library is that they allows third party developer to manipulate data before it enters the view. 
 
-        Components and stuff
-    
-    
-    ') !!}
+        This simple concept makes it a powerful tool to manipulate display of content without to touch a single line of html. And hey, if thay want to they can replace both controllers and views to make deep customizations.
+    @endmarkdown
 @stop
