d68003ce5736579a4b60c73d428ae06ba74e568c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-17 09:19:15 +0200

    Remove stuff from 404

--- views/pages/404.blade.php
+++ views/pages/404.blade.php
@@ -4,26 +4,9 @@
     <h1>404 - Page cannot be found</h1>
     <p>This page dosen't exists.</p>
     <pre><code>{{$errorMessage}}</code></pre>
-
-        
-
         
         @icon(['icon' => 'home', 'label' => 'Mutch icon, wow!'])
         @endicon
-
-        
-
-        
-
-        @avatar([
-            'name' => "Sebastian Thulin"
-        ])
-        @endavatar
-
-        @avatar([
-            'image' => "https://picsum.photos/70/70?image=64"
-        ])
-        @endavatar
 
         @heading([
             'label' => "Yo, im a heading.",
