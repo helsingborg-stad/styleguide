a73bd01d374e2e9070fde79f14d9c71daf829a02
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-17 16:32:58 +0200

    Documentation fix

--- views/pages/component/alert.blade.php
+++ views/pages/component/alert.blade.php
@@ -6,7 +6,7 @@
         Replaces the buil-in javascript alert('') and confirm('') functions form Javascript with a fancier one.
     @endmarkdown
 
-    @doc(['slug' => 'accordion'])
+    @doc(['slug' => 'alert'])
 
         @alert(['title'=>"Wait a minute!", 'accept' => 'Yes, dont sent me any more.', 'dismiss' => 'Oh no, sorry!'])
             Are you shure that you want to stop getting our newletter, free goddies and awesome tips? 
