54118ea4cd267e3f59bbbfabac7e31ad8a65bf1a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-17 12:44:11 +0200

    Headings

---
+++ views/pages/component/heading.blade.php
@@ -0,0 +1,48 @@
+@extends('layout.master')
+
+@section('content')
+    @markdown
+        #Headings (Titles etc.)
+        A simple heading component. 
+    @endmarkdown
+
+    @doc(['slug' => 'heading'])
+
+        @heading([
+            'label' => "Headline size 1.",
+            'level' => 1
+        ])
+        @endbutton
+
+        @heading([
+            'label' => "Headline size 2.",
+            'level' => 2
+        ])
+        @endbutton
+
+        @heading([
+            'label' => "Headline size 3.",
+            'level' => 3
+        ])
+        @endbutton
+
+        @heading([
+            'label' => "Headline size 4.",
+            'level' => 4
+        ])
+        @endbutton
+
+        @heading([
+            'label' => "Headline size 5.",
+            'level' => 5
+        ])
+        @endbutton
+
+        @heading([
+            'label' => "Headline size 6.",
+            'level' => 6
+        ])
+        @endbutton
+
+    @enddoc
+@stop
