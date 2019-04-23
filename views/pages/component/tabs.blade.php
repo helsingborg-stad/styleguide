dbfb008704db7d7c0ee4bb552cf045bcd7ae8fc7
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-04-23 15:29:53 +0200

    Tabs component example

--- views/pages/component/tabs.blade.php
+++ views/pages/component/tabs.blade.php
@@ -1,11 +1,21 @@
 @extends('layout.master')
 
 @section('content')
-    {!!
-        markdown('
-            #Blank
+    @markdown
+        # Tabs
+    @endmarkdown
 
-            Demo page
-        ')
-    !!}
+    @doc(['slug' => 'tabs'])
+        @tabs(['tabs' => array(
+            array(
+                'title' => 'Tab one',
+                'content' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.'
+            ),
+            array(
+                'title' => 'Tab two',
+                'content' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.'
+            ),
+        )])
+        @endtabs
+    @enddoc
 @stop
