11470c365c5116a088e28bded1090e0820a70fce
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-04-25 15:04:12 +0200

    Loader

--- views/pages/component/loader.blade.php
+++ views/pages/component/loader.blade.php
@@ -1,11 +1,16 @@
 @extends('layout.master')
 
 @section('content')
-    {!!
-        markdown('
-            #Blank
+    @markdown
+        # Loader
+        Displays progress loaders in different shapes.
+    @endmarkdown
 
-            Demo page
-        ')
-    !!}
+    @doc(['slug' => 'loader'])
+        @loader(['shape' => 'circular', 'size' => 'md'])
+        @endloader
+
+        @loader(['shape' => 'linear'])
+        @endloader
+    @enddoc
 @stop
