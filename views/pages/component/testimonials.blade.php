4a5c3919ce2142af4cfd00197a4a2460f7a6f668
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-04-25 10:47:22 +0200

    Testimonials example

--- views/pages/component/testimonials.blade.php
+++ views/pages/component/testimonials.blade.php
@@ -1,11 +1,25 @@
 @extends('layout.master')
 
 @section('content')
-    {!!
-        markdown('
-            #Blank
+    @markdown
+        # Testimonials
+    @endmarkdown
 
-            Demo page
-        ')
-    !!}
+    @doc(['slug' => 'testimonials'])
+        @testimonials(['testimonials' => array(
+            array(
+                'name' => 'Curabitur blandit',
+                'title' => 'Tempus porttitor',
+                'testimonial' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.',
+                'image' => 'https://picsum.photos/300/200?image=1026'
+            ),
+            array(
+                'name' => 'Curabitur blandit',
+                'title' => 'Tempus porttitor',
+                'testimonial' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.',
+                'image' => 'https://picsum.photos/300/200?image=1026'
+            ),
+        )])
+        @endtestimonials
+    @enddoc
 @stop
