f314f60e528bcb6e291d5b6d2ff3401adc68576a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-02 13:48:03 +0200

    Try https if ssh is failing.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 if [ -d "source/library" ]; then
     echo Library repository already installed in source/library. You might want to add this in your git-client-ui.
 else
-    git clone "git@github.com:helsingborg-stad/blade-component-library.git" "source/library"
+    git clone "git@github.com:helsingborg-stad/blade-component-library.git" "source/library" || git clone "https://github.com/helsingborg-stad/blade-component-library.git" "source/library"
 fi
