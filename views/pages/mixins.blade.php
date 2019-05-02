a1d85df172508b6940f60ea44882a9a13e654629
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-02 11:57:01 +0200

    Mqcss

---
+++ views/pages/mixins.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.master')
+
+@section('content')
+<article>
+
+    @markdown
+        #Mixins
+        Our mixin library relies on the awesome work of burbon, sassline and mqcss. We have, however extended with some of our very own mixins documentated here. 
+
+        ##Reference documentation
+        - [Burbon documentation](https://www.bourbon.io/docs/latest/)
+        - [Sassline documentation](https://github.com/jakegiltsoff/sassline)
+        - [MqCss documentation](https://github.com/Dan503/mq-scss)
+    @endmarkdown
+
+</article>
+@stop
