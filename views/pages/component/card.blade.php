825591aac3a9e3152da757b758dc8e9294ccad3d
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-05-06 16:04:50 +0200

    cards

--- views/pages/component/card.blade.php
+++ views/pages/component/card.blade.php
@@ -3,41 +3,53 @@
 @section('content')
 
     @markdown
-        #Cards
+    #Cards
 
-        Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+    Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
     @endmarkdown
 
     @doc(['slug' => 'card'])
-        <div  class="grid">
-            <div class="grid-s-12 grid-md-6">
-                @card(
-                    [
-                    'slot' => 'Detta är en slot',
-                    'image' => 'https://picsum.photos/300/200?image=1077',
-                    'title' => 'Plats ingalunda varit, miljoner.',
-                    'content' => 'Löksås ipsum dimmhöljd björnbär regn faktor sitt del har gamla, fram faktor dimma sista precis därmed annat ännu söka, från se ingalunda dag vi äng plats är. Söka denna erfarenheter flera vidsträckt fram dock stora söka, genom dimma blivit enligt vemod söka nya gör annan, mot annat där enligt faktor dimma rännil.'
-                    ])
-                @endcard
 
-            </div>
+    <div class="grid">
 
-            <div class="grid-s-12 grid-md-6">
-                @card(
-                    ['image' => 'https://picsum.photos/300/200?image=1077',
-                    'title' => 'Plats ingalunda varit, miljoner.',
-                    'content' => 'Löksås ipsum dimmhöljd björnbär regn faktor sitt del har gamla, fram faktor dimma sista precis därmed annat ännu söka, från se ingalunda dag vi äng plats är. Söka denna erfarenheter flera vidsträckt fram dock stora söka, genom dimma blivit enligt vemod söka nya gör annan, mot annat där enligt faktor dimma rännil.'
-                ])
-                @endcard
+        <div class="grid-s-12 grid-md-6">
+            @card([
+            'href' => 'http://styleguide.helsingborg.se/card',
+            'image' => 'https://picsum.photos/300/200?image=1077',
+            'title' => 'Plats ingalunda varit, miljoner.',
+            'content' => 'Löksås ipsum dimmhöljd björnbär regn faktor sitt del har gamla, fram faktor dimma sista precis
+            därmed annat ännu söka, från se ingalunda dag vi äng plats är. Söka denna erfarenheter flera vidsträckt fram
+            dock stora söka, genom dimma blivit enligt vemod söka nya gör annan, mot annat där enligt faktor dimma
+            rännil.'
+            ])
 
-            </div>
+            @endcard
+
+        </div>
+
+        <div class="grid-s-12 grid-md-6">
+            @card([
+            'href' => 'http://styleguide.helsingborg.se/card',
+            'image' => 'https://picsum.photos/300/200?image=1077',
+            'title' => 'Plats ingalunda varit, miljoner.',
+            'content' => 'Löksås ipsum dimmhöljd björnbär regn faktor sitt del har gamla, fram faktor dimma sista precis
+            därmed annat ännu söka, från se ingalunda dag vi äng plats är. Söka denna erfarenheter flera vidsträckt fram
+            dock stora söka, genom dimma blivit enligt vemod söka nya gör annan, mot annat där enligt faktor dimma
+            rännil.'
+
+            ])
+
+            @endcard
+
         </div>
 
 
-
+    </div>
 
     @enddoc
-    
+
+
+
 
 @stop
 
