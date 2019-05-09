db8c9c02be51b5ed585ffc1a80094a9ca5888e0a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-09 14:20:54 +0200

    Larger images in cards on front page.

--- views/pages/home.blade.php
+++ views/pages/home.blade.php
@@ -18,7 +18,7 @@
     <div class="grid" data-equal-container>
         <div class="grid-xs-12 grid-md-4">
             @card([
-                'image' => 'https://picsum.photos/300/200?image=919',
+                'image' => 'https://picsum.photos/300/225?image=919',
                 'title' => 'Usability', 
                 'content' => 'On of the main focus of this styleguide is usability. Read more of our guidelines here.',
                 'href' => '/about/accessibility'
@@ -28,7 +28,7 @@
 
         <div class="grid-xs-12 grid-md-4">
             @card([
-                'image' => 'https://picsum.photos/300/200?image=743',
+                'image' => 'https://picsum.photos/300/225?image=743',
                 'title' => 'Blade components', 
                 'content' => 'This styleguide is in many parts crated with our own blade component library to enable swift development and maintaince.',
                 'href' => '/about/styleguide-structre'
@@ -38,7 +38,7 @@
 
         <div class="grid-xs-12 grid-md-4">
             @card([
-                'image' => 'https://picsum.photos/300/200?image=455',
+                'image' => 'https://picsum.photos/300/225?image=455',
                 'title' => 'Authors', 
                 'content' => 'The styleguide and lirbraries where created by the awesome development team @ helsingborg stad.',
                 'href' => '/about/authors'
