8f91737f5959d3c0a184ab81b222697f4539825f
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-09 16:46:33 +0200

    Remove builtin padding in cards.

--- views/pages/icons.blade.php
+++ views/pages/icons.blade.php
@@ -8,10 +8,12 @@
         Can be utilized by the component <?php echo "@icon(['icon' => 'home']) @endicon"; ?>. This page represents the complete list of icons avabile. 
     @endmarkdown
 
-    @foreach(HbgStyleGuide\Helper\Icons::getJson() as $iconKey => $utf)
-        @icon(['icon' => str_replace("icon--", "", $iconKey), 'size' => 'xl'])
-        @endicon
-    @endforeach
+    @paper(['padding' => 3])
+        @foreach(HbgStyleGuide\Helper\Icons::getJson() as $iconKey => $utf)
+            @icon(['icon' => str_replace("icon--", "", $iconKey), 'size' => 'xl'])
+            @endicon
+        @endforeach
+    @endpaper
 
 </article>
 @stop
