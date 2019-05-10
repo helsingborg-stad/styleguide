be96281b165f3f53232f04e0fe5ac06837f1d864
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-10 15:11:09 +0200

    Isntalled ripple js/sass

---
+++ views/pages/about/browser-support.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.master')
+
+@section('content')
+<article>
+
+    {!! markdown("
+
+        # Browser support
+        This styleguide has support for modern browsers, and IE 11. We do not guarantee support for older browsers. They may however render the styleguide well.
+        
+    ") !!}
+
+</article>
+@stop
