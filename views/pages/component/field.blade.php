e02cc088114a5f31fd981a95ad9a092f71804e57
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-05-17 15:40:30 +0200

    Textarea

--- views/pages/component/field.blade.php
+++ views/pages/component/field.blade.php
@@ -10,15 +10,31 @@
 
     @field([
 
-        'label' => 'Im a simple textfield',
+        'label' => 'Text field',
         'classList' => [],
         'textarea' => false,
         'attributeList' => [
-            'name' => 'simpleTextField',
-            'id' => 'ImAnId',
-            'placeholder' => 'simpleTextField',
+            'name' => 'adress',
+            'id' => '303',
+            'placeholder' => 'TextField',
             'type' => 'text'
         ]
+    ])
+    @endfield
+
+    @enddoc
+
+    @doc(['slug' => 'field'])
+
+    @field([
+
+    'label' => 'Textarea',
+    'classList' => [],
+    'textarea' => true,
+    'attributeList' => [
+    'name' => 'message',
+    'id' => '909',
+    ]
     ])
     @endfield
 
