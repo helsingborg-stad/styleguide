1b62f9515de3da889d16c94e84f1639c6eb2b46d
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-05-17 10:21:50 +0200

    Testimonials

--- views/pages/component/testimonials.blade.php
+++ views/pages/component/testimonials.blade.php
@@ -6,20 +6,29 @@
     @endmarkdown
 
     @doc(['slug' => 'testimonials'])
-        @testimonials(['testimonials' => array(
-            array(
-                'name' => 'Curabitur blandit',
-                'title' => 'Tempus porttitor',
-                'testimonial' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.',
-                'image' => 'https://picsum.photos/300/200?image=1026'
-            ),
-            array(
-                'name' => 'Curabitur blandit',
-                'title' => 'Tempus porttitor',
-                'testimonial' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.',
-                'image' => 'https://picsum.photos/300/200?image=1026'
-            ),
-        )])
+        @testimonials(
+            ['perRow' => 3,
+            'testimonials' => array(
+                array(
+                    'name' => 'Kendal Ozzel',
+                    'title' => 'Admiral',
+                    'testimonial' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                    'image' => 'https://picsum.photos/300/200?image=1026'
+                ),
+                array(
+                    'name' => 'Maximilian Veers',
+                    'title' => 'General',
+                    'testimonial' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.',
+                    'image' => 'https://picsum.photos/300/200?image=1026'
+                ),
+                array(
+                    'name' => 'Shaef Corssin',
+                    'title' => 'Captain',
+                    'testimonial' => 'Nobis laborum corrupti dolorem voluptatem impedit.',
+                    'image' => 'https://picsum.photos/300/200?image=1026'
+                ),
+            )]
+        )
         @endtestimonials
     @enddoc
 @stop
