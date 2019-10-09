c3029e080c371db8ed11d28e6e761f1d8a430556
Alexander Boman Skoug <alexander.boman.skoug@hiq.se>
2019-10-09 19:33:00 +0200

    Add documentation, wip

--- views/pages/utilities/spacing.blade.php
+++ views/pages/utilities/spacing.blade.php
@@ -3,12 +3,43 @@
 @section('content')
 <article>
 
-    {!! markdown("
-
-        #Utilties
-        Lorem ipsum...
-
-    ") !!}
+    @markdown
+        #Spacing (Margin & Padding)
+    @endmarkdown
+    @utility_doc(['slug' => 'spacing', 'page_config' => true])
+        <div class="grid">
+            <div class="grid-s-12 grid-md-6 u-pl--6">
+                @card([
+                'href' => 'http://styleguide.helsingborg.se/card',
+                'image' => 'https://picsum.photos/300/200?image=1077',
+                'title' => 'Padding left is base * 6',
+                'content' => 'Löksås ipsum dimmhöljd björnbär regn faktor sitt del har gamla, fram faktor dimma sista precis
+                därmed annat ännu söka, från se ingalunda dag vi äng plats är. Söka denna erfarenheter flera vidsträckt fram
+                dock stora söka, genom dimma blivit enligt vemod söka nya gör annan, mot annat där enligt faktor dimma
+                rännil.'
+                ])
+    
+                @endcard
+    
+            </div>
+    
+            <div class="grid-s-12 grid-md-6 u-my--7">
+                @card([
+                'href' => 'http://styleguide.helsingborg.se/card',
+                'image' => 'https://picsum.photos/300/200?image=1077',
+                'title' => 'Margin on Y-axis is base * 7',
+                'content' => 'Löksås ipsum dimmhöljd björnbär regn faktor sitt del har gamla, fram faktor dimma sista precis
+                därmed annat ännu söka, från se ingalunda dag vi äng plats är. Söka denna erfarenheter flera vidsträckt fram
+                dock stora söka, genom dimma blivit enligt vemod söka nya gör annan, mot annat där enligt faktor dimma
+                rännil.'
+    
+                ])
+    
+                @endcard
+    
+            </div>
+        </div>
+    @endutility_doc
 
 </article>
 @stop
