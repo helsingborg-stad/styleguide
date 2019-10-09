ec8583d07f09d2a2fa4d58e5ee5bb888a00dc770
Alexander Boman Skoug <alexander.boman.skoug@hiq.se>
2019-10-09 09:23:45 +0200

    added filter docs

--- views/pages/script/filter-list.blade.php
+++ views/pages/script/filter-list.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     @markdown
-        #TODO: Filtering lists (search)
+        #TODO: Filtering lists, tables, etc (search)
         Hide elements based on match with data attributes.
     @endmarkdown
 
     @paper
         @table([
             'list' => [
-                ['js-bind', 'Event to bind on', 'keyup'],
-                ['js-action', 'Should always contain "toggleClass" for css toggling of classes.', 'toggleClass'],
-                ['js-toggle-target', 'Target element to toggle the class on, should be a class.', '.somediv'],
-                ['js-toggle-class', 'The class to toggle on the target element.', '.is-active']
+                ['js-filter-container', 'Container of what should be filterable, has to contain unique id.', 'unique_id'],
+                ['js-filter-item', 'The diffrent items that should be filtered through.', ''],
+                ['js-filter-data', 'Elements inside item that contain data which should be used in the filter.', ''],
+                ['js-filter-input', 'The input for a specific container, has to contain same id as parent.', 'unique_id']
             ],
             'headings' => ['Attribute', 'Description', 'Example value'],
             'showFooter' => false,
