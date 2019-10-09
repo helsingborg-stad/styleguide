533161f83ab1541a900d226fbe66c95b710ae329
Alexander Boman Skoug <alexander.boman.skoug@hiq.se>
2019-10-09 20:24:50 +0200

    added docs and fixes

--- views/pages/utilities/screen-readers.blade.php
+++ views/pages/utilities/screen-readers.blade.php
@@ -3,12 +3,20 @@
 @section('content')
 <article>
 
-    {!! markdown("
-
-        #Utilties
-        Lorem ipsum...
-
-    ") !!}
+    @markdown
+        #Screen readers
+    @endmarkdown
+    @utility_doc(['slug' => 'screen-readers', 'page_config' => true])
+        <div class="u-border--1 u-border--primary u-mb--1">
+            <p>This container a sr-only text</p>
+            <div class="u-sr-only">Screen reader only</div>
+        </div>
+        
+        <div class="u-border--1 u-border--secondary">
+                <p>This container a sr-only--focusable text</p>
+            <div class="u-sr-only--focusable">Screen reader only</div>
+        </div>
+    @endutility_doc
 
 </article>
 @stop
