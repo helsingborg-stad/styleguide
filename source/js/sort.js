d0d2803eb16d9efb0f82eeb5a36f4760baf64087
Eric Rosenborg <eric.rosenborg@hiq.se>
2019-10-10 10:38:19 +0200

    Refactoring

--- source/js/sort.js
+++ source/js/sort.js
@@ -1,31 +1,30 @@
 const CONTAINER = 'js-sort-container';
 const BUTTON = 'js-sort-button';
 const SORTABLE = 'js-sort-sortable';
-const ORDER = 'js-sort-order'
-const DATA = 'js-sort-data'
+const ORDER = 'js-sort-order';
+const DATA = 'js-sort-data';
 
 function compare(a, b) {
     return a.data.toLowerCase().localeCompare(b.data.toLowerCase());
 }
 
-function appendSortable(button, container, buttonId){
+function appendSortable(container, dataId){
     let sorted = [];
-    let sortOrder = container.getAttribute(ORDER)
-    let sortData = container.querySelectorAll(`[${DATA}="${buttonId}"]`);
-    
+    let sortOrder = container.getAttribute(ORDER);
+    let sortData = container.querySelectorAll(`[${DATA}="${dataId}"]`);
     let comparableData = [...sortData].map((data) => 
         {return {data: data.innerText, index: data.closest(`[${SORTABLE}]`)}
     });
 
     if(sortOrder === 'asc'){
-        sorted = comparableData.sort(compare)
-        container.setAttribute(ORDER, "desc")
+        sorted = comparableData.sort(compare);
+        container.setAttribute(ORDER, "desc");
     }else{
-        sorted = comparableData.reverse(compare)
-        container.setAttribute(ORDER, "asc")
+        sorted = comparableData.reverse(compare);
+        container.setAttribute(ORDER, "asc");
     }
     sorted.forEach( sort => {
-        container.appendChild(sort.index)
+        container.appendChild(sort.index);
     });
 }
 
@@ -33,13 +32,13 @@ const sortClass = () => {
     const sortContainers = document.querySelectorAll(`[${CONTAINER}]`);
     
     sortContainers.forEach( container =>{
-        const sortButtons = container.querySelectorAll(`[${BUTTON}]`)
+        const sortButtons = container.querySelectorAll(`[${BUTTON}]`);
         sortButtons.forEach( (button) => {
-            
-            let buttonId = button.getAttribute(BUTTON);
+
+            let dataId = button.getAttribute(BUTTON);
            
             button.addEventListener('click', (event)=>{
-                appendSortable(button, container, buttonId);
+                appendSortable(container, dataId);
             });
         });
     });
