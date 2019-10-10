a051dfeaa393690ebb7fe0145ae69f1be6e14419
Eric Rosenborg <eric.rosenborg@hiq.se>
2019-10-10 09:46:57 +0200

    Create support for multiple components at the same time

--- source/js/sort.js
+++ source/js/sort.js
@@ -4,49 +4,52 @@ const SORTABLE = 'js-sort-sortable';
 const ORDER = 'js-sort-order'
 const DATA = 'js-sort-data'
 
-var sort_data = function(a, b) {
+function compare(a, b) {
     return a.data.toLowerCase().localeCompare(b.data.toLowerCase());
 }
 
+function appendSortable(button, container, buttonId){
+    let sorted = [];
+    let sortOrder = container.getAttribute(ORDER)
+    let sortData = container.querySelectorAll(`[${DATA}="${buttonId}"]`)
+    
+    let comparableData = [...sortData].map((data) => 
+        {return {data: data.innerText, index: data.closest(`[${SORTABLE}]`)}
+    });
+
+    if(sortOrder === 'asc'){
+        sorted = comparableData.sort(compare)
+        container.setAttribute(ORDER, "desc")
+    }else{
+        sorted = comparableData.reverse(compare)
+        container.setAttribute(ORDER, "asc")
+    }
+    sorted.forEach( sort => {
+        container.appendChild(sort.index)
+    })
+}
+
+function appendIcon(button){
+    let icon = document.createElement('i')
+    icon.className = 'c-icon c-icon--size-xl c-icon--arrow-drop-down';
+    button.appendChild(icon) 
+}
 
 const sortClass = () => {
-    const sortButtons = document.querySelectorAll(`[${BUTTON}]`)
-    const sortContainer = document.querySelectorAll(`[${CONTAINER}]`);
+    const sortContainers = document.querySelectorAll(`[${CONTAINER}]`);
     
-    sortContainer.forEach( container =>{
-        sortButtons.forEach( button => {
-
-//            let icon = document.createElement('<i class="c-icon c-icon--size-m c-icon--arrow-drop-down"></i>')
-            let icon = document.createElement('i')
-            icon.className = 'c-icon c-icon--size-m c-icon--arrow-drop-down';
-
-            button.appendChild(icon) 
+    sortContainers.forEach( container =>{
+        const sortButtons = container.querySelectorAll(`[${BUTTON}]`)
+        sortButtons.forEach( (button) => {
+            appendIcon(button);
+            
+            let buttonId = button.getAttribute(BUTTON);
+           
             button.addEventListener('click', (event)=>{
-                let sorted = [];
-                let columnIndex = button.getAttributeNode(BUTTON).value
-                let sortOrder = container.getAttributeNode(ORDER).value
-                let sortableElements = container.querySelectorAll(`[${SORTABLE}]`)
-                let sortData = container.querySelectorAll(`[${DATA}="${columnIndex}"]`)
-                let comparableData = [...sortData].map((data) => { return {data: data.innerText, index: data.parentElement.getAttributeNode('js-sort-sortable').value}})
-
-                if(sortOrder === 'asc'){
-                    sorted = comparableData.sort(sort_data)
-                    container.setAttribute(ORDER, "desc")
-                }else{
-                    sorted = comparableData.reverse(sort_data)
-                    container.setAttribute(ORDER, "asc")
-                }
-                
-                sorted.forEach( (sort,index) => {
-                    let toAppend = sortableElements[sort.index]
-                    toAppend.setAttribute('js-sort-sortable', index)
-                    container.appendChild(toAppend)
-                })
+                appendSortable(button, container, buttonId);
             });
         });
     });
-    
-    
 }
 
 export default sortClass;
