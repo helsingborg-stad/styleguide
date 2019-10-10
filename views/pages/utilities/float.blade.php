fd1019244194ce551dbde6ce2e3232b2eee78271
Eric Rosenborg <eric.rosenborg@hiq.se>
2019-10-10 17:18:38 +0200

    Remove old floatcss, add documentation for float class.

--- views/pages/utilities/float.blade.php
+++ views/pages/utilities/float.blade.php
@@ -3,12 +3,53 @@
 @section('content')
 <article>
 
-    {!! markdown("
+    @markdown
+        #Float
+    @endmarkdown
 
-        #Utilties
-        Lorem ipsum...
+    @utility_doc(['slug' => 'float', 'utilitySlug' => "float"])
+        <div>
+            @button([
+                    'href' => '#btn-1', 
+                    'isOutlined' => true, 
+                    'isPrimary' => false,
+                    'classList' => ['u-float--right']
+                ])
+                    Button one
+                @endbutton
 
-    ") !!}
+                @button([
+                    'href' => '#btn-2', 
+                    'isOutlined' => true, 
+                    'isPrimary' => true,
+                    'classList' => ['u-float--right']
+                ])
+                    Button two
+                @endbutton
+
+                @button([
+                    'href' => '#btn-3', 
+                    'isOutlined' => false, 
+                    'isPrimary' => true,
+                    'classList' => ['u-float--right']
+                ])
+                Button three
+                @endbutton
+
+                @button([
+                    'href' => '#btn-3',
+                    'isOutlined' => false,
+                    'isPrimary' => false,
+                    'isCircle' => true,
+                    'classList' => ['u-float--right']
+                ])
+                Button four
+            @endbutton
+        </div>
+    @endutility_doc
+
+
+    
 
 </article>
 @stop
