559cbbab83591240dfdad7afeb5b203a4037df82
Eric Rosenborg <eric.rosenborg@hiq.se>
2019-10-15 14:00:10 +0200

    Add more docuemtnation

---
+++ views/pages/script/sort.blade.php
@@ -0,0 +1,19 @@
+@extends("layout.master")
+
+@section("content")
+    @markdown
+        #Sort
+    @endmarkdown
+
+    @script_doc(["viewDoc" => ["type" => "script", "root" => "sort", "config" => "sort"]])
+        <div js-sort-container js-sort-order="asc">
+            <button js-sort-button="111-0">Sort</button>
+            <div class="grid" js-sort-data-container>
+                <div class="u-border--1 u-border--success u-color__bg--danger" js-sort-sortable js-sort-data="111-0">2</div>
+                <div class="u-border--1 u-border--success u-color__bg--danger" js-sort-sortable js-sort-data="111-0">3</div>
+                <div class="u-border--1 u-border--success u-color__bg--danger" js-sort-sortable js-sort-data="111-0">50</div>
+                <div class="u-border--1 u-border--success u-color__bg--danger" js-sort-sortable js-sort-data="111-0">1</div>
+            </div>
+        </div>
+    @endscript_doc
+@stop
