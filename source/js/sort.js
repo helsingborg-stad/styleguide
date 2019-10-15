0de32d2f49a09d7377352fe53ab6918fe928b5c8
Eric Rosenborg <eric.rosenborg@hiq.se>
2019-10-15 11:42:37 +0200

    Fix bug in sort, add documentation

--- source/js/sort.js
+++ source/js/sort.js
@@ -1,48 +1,53 @@
 
-class Sort{
-    
-    constructor(){
+class Sort {
+
+    constructor() {
         this.CONTAINER = 'js-sort-container';
         this.BUTTON = 'js-sort-button';
         this.SORTABLE = 'js-sort-sortable';
         this.ORDER = 'js-sort-order';
         this.DATA = 'js-sort-data';
+        this.DATACONTAINER = 'js-sort-data-container'
     }
 
     compare(a, b) {
         return a.data.toLowerCase().localeCompare(b.data.toLowerCase());
     }
 
-    appendSortable(container, dataId){
+    appendSortable(container, dataId) {
         let sorted = [];
         let sortOrder = container.getAttribute(this.ORDER);
         let sortData = container.querySelectorAll(`[${this.DATA}="${dataId}"]`);
-        let comparableData = [...sortData].map((data) => 
-            {return {data: data.innerText, index: data.closest(`[${this.SORTABLE}]`)}
+        console.log(`[${this.DATA}="${dataId}"]`)
+        let comparableData = [...sortData].map((data) => {
+            return { data: data.innerText, index: data.closest(`[${this.SORTABLE}]`) }
         });
-    
-        if(sortOrder === 'asc'){
+
+        if (sortOrder === 'asc') {
             sorted = comparableData.sort(this.compare);
             container.setAttribute(this.ORDER, "desc");
-        }else{
+        } else {
             sorted = comparableData.reverse(this.compare);
             container.setAttribute(this.ORDER, "asc");
         }
-        sorted.forEach( sort => {
-            container.appendChild(sort.index);
+
+        sorted.forEach(sort => {
+            sort.index.closest(`[${this.DATACONTAINER}]`).appendChild(sort.index);
         });
     }
 
-    applySort(){
+    applySort() {
+        console.log("LOL")
         const sortContainers = document.querySelectorAll(`[${this.CONTAINER}]`);
-    
-        sortContainers.forEach( container =>{
+
+        sortContainers.forEach(container => {
             const sortButtons = container.querySelectorAll(`[${this.BUTTON}]`);
-            sortButtons.forEach( (button) => {
+            sortButtons.forEach((button) => {
 
                 let dataId = button.getAttribute(this.BUTTON);
-            
-                button.addEventListener('click', (event)=>{
+
+                button.addEventListener('click', (event) => {
+                    console.log("HEJ!")
                     this.appendSortable(container, dataId);
                 });
             });
