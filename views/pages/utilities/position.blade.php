df096c443f25d6055ecebdda1c5754e711efe7c5
Alexander Boman Skoug <alexander.boman.skoug@hiq.se>
2019-10-16 10:40:13 +0200

    Added docs

--- views/pages/utilities/position.blade.php
+++ views/pages/utilities/position.blade.php
@@ -3,12 +3,47 @@
 @section('content')
 <article>
 
-    {!! markdown("
+    @markdown
+        #Position
+    @endmarkdown
+    @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'position', 'config' => 'position']])
+        @button([
+            'href' => '#btn-1', 
+            'isOutlined' => true, 
+            'isPrimary' => false,
+            'classList' => ['u-disabled']
+        ])
+            Disabled Button
+        @endbutton
+    @endutility_doc
 
-        #Utilties
-        Lorem ipsum...
+    @markdown
+        ###Fixed
+    @endmarkdown
+    @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'position', 'config' => 'fixed']])
+        @button([
+            'href' => '#btn-1', 
+            'isOutlined' => true, 
+            'isPrimary' => false,
+            'classList' => ['u-disabled']
+        ])
+            Disabled Button
+        @endbutton
+    @endutility_doc
 
-    ") !!}
+    @markdown
+        ###Sticky
+    @endmarkdown
+    @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'position', 'config' => 'sticky']])
+        @button([
+            'href' => '#btn-1', 
+            'isOutlined' => true, 
+            'isPrimary' => false,
+            'classList' => ['u-disabled']
+        ])
+            Disabled Button
+        @endbutton
+    @endutility_doc
 
 </article>
 @stop
