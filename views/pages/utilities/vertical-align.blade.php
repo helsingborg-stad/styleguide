85416cd7e4964a2c37154903680055c4a526055e
Eric Rosenborg <eric.rosenborg@hiq.se>
2019-10-16 11:08:57 +0200

    Add documentation for vertical-align, delete old align and duplicates that somehow ended up in overflow doc dir

--- views/pages/utilities/vertical-align.blade.php
+++ views/pages/utilities/vertical-align.blade.php
@@ -7,40 +7,17 @@
         #Vertical alignments
     @endmarkdown
 
-    @utility_doc(['slug' => 'vertical-align', 'page_config' => true])
-        @table([
-            'list' => [
-                ['u-align-baseline ', 'Align according to baseline'],
-                ['u-align-middle', 'Align middle'],
-                ['u-align-bottom', 'Align with the bottom'],
-                ['u-align-text-bottom', 'Align with the bottom of current row'],
-                ['u-align-text-top', 'Align with the top of current row'],
-            ],
-            'headings' => ['Class', 'Description'],
-            'showFooter' => false
-        ])
-        @endtable
+    @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'vertical-align', 'config' => 'vertical-align']])
+        
+            <span class="u-align--baseline">baseline</span>
+            <span class="u-align--top">top</span>
+            <span class="u-align--middle">middle</span>
+            <span class="u-align--bottom">bottom</span>
+            <span class="u-align--text-top">text-top</span>
+            <span class="u-align--text-bottom">text-bottom</span>
+     
     @endutility_doc
 
-    @markdown
-        <h3>Vertical alignments</h3>
-    @endmarkdown
-    @utility_doc(['slug' => 'vertical-align2', 'page_config' => true])
-        @table([
-            'list' => [
-                ['u-align-baseline ', 'Align according to baseline'],
-                ['u-align-middle', 'Align middle'],
-                ['u-align-bottom', 'Align with the bottom'],
-                ['u-align-text-bottom', 'Align with the bottom of current row'],
-                ['u-align-text-top', 'Align with the top of current row'],
-            ],
-            'headings' => ['Class', 'Description'],
-            'showFooter' => false
-        ])
-        @endtable
-    @endutility_doc
-
-    
 
 </article>
 @stop
