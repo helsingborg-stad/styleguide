f779a42e7822e66ea2c621b40a6cfc3161a60ca3
Alexander Boman Skoug <alexander.boman.skoug@hiq.se>
2019-10-22 11:24:02 +0200

    Hotfix: Position docs

--- views/pages/utilities/position.blade.php
+++ views/pages/utilities/position.blade.php
@@ -7,42 +7,18 @@
         #Position
     @endmarkdown
     @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'position', 'config' => 'position']])
-        @button([
-            'href' => '#btn-1', 
-            'isOutlined' => true, 
-            'isPrimary' => false,
-            'classList' => ['u-disabled']
-        ])
-            Disabled Button
-        @endbutton
     @endutility_doc
 
     @markdown
         ###Fixed
     @endmarkdown
     @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'position', 'config' => 'fixed']])
-        @button([
-            'href' => '#btn-1', 
-            'isOutlined' => true, 
-            'isPrimary' => false,
-            'classList' => ['u-disabled']
-        ])
-            Disabled Button
-        @endbutton
     @endutility_doc
 
     @markdown
         ###Sticky
     @endmarkdown
     @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'position', 'config' => 'sticky']])
-        @button([
-            'href' => '#btn-1', 
-            'isOutlined' => true, 
-            'isPrimary' => false,
-            'classList' => ['u-disabled']
-        ])
-            Disabled Button
-        @endbutton
     @endutility_doc
 
 </article>
