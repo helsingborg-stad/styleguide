d53e9d8e107a4e7f2687f2c46362f98d90d94666
Alexander Boman Skoug <alexander.boman.skoug@hiq.se>
2019-10-22 11:15:54 +0200

    Hotfix: Screen readers docs

--- views/pages/utilities/screen-readers.blade.php
+++ views/pages/utilities/screen-readers.blade.php
@@ -9,12 +9,12 @@
     @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'screen-readers', 'config' => 'screen-readers']])
         <div class="u-border--1 u-border--primary u-mb--1">
             <p>This container a sr-only text</p>
-            <button class="u-sr-only">Screen reader only</button>
+            <button class="u-sr__only">Screen reader only</button>
         </div>
         
         <div class="u-border--1 u-border--secondary">
             <p>This container a sr-only--focusable text</p>
-            <button class="u-sr-only--focusable">Screen reader only</button>
+            <button class="u-sr__only--focusable">Screen reader only</button>
         </div>
     @endutility_doc
 
