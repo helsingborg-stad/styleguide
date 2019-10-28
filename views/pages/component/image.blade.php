f0d2c69df309edf027c4432b93f0973f2b6e3d05
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-10-28 10:55:42 +0100

    Squashed commit of the following:
    commit 414cd3fa5bb8539d2ac163e65815d2579d61c585
    Author: Carl Johan Silvergrund <johan.silvergrund@hiq.se>
    Date:   Mon Oct 28 10:54:51 2019 +0100
    
        Added displayParams - for multiple examples on doc
    
    commit 7f9f1643017e96857a647fc136bfdd8d18124470
    Author: Carl Johan Silvergrund <johan.silvergrund@hiq.se>
    Date:   Fri Oct 25 17:55:25 2019 +0200
    
        Removed 50% rounded
    
    commit 6c5b73b727754b98994da3df57e7f8e2c3f1a2f0
    Author: Carl Johan Silvergrund <johan.silvergrund@hiq.se>
    Date:   Fri Oct 25 17:55:10 2019 +0200
    
        Removed rounded 50%
    
    commit b7e61643b1ecfcf70e212e1a2266c9f26f9d5e89
    Author: Carl Johan Silvergrund <johan.silvergrund@hiq.se>
    Date:   Fri Oct 25 16:20:13 2019 +0200
    
        Clean up
    
    commit adbece83a61978739bbe07105f5d18e042e4fba6
    Author: Carl Johan Silvergrund <johan.silvergrund@hiq.se>
    Date:   Fri Oct 25 16:11:44 2019 +0200
    
        Cleanup
    
    commit 69d59d0678bf350c921358a228b6f407ee32a08e
    Author: Carl Johan Silvergrund <johan.silvergrund@hiq.se>
    Date:   Fri Oct 25 15:57:00 2019 +0200
    
        Radius on images

--- views/pages/component/image.blade.php
+++ views/pages/component/image.blade.php
@@ -1,19 +1,80 @@
 @extends('layout.master')
 
 @section('content')
+
     @markdown
-        #Image
-        Displays a simple image. If it is missing it may be replaced with a placeholder. Alt and captions can be added.
+    #Image
+    Displays a simple image. If it is missing it may be replaced with a placeholder. Alt and captions can be added.
     @endmarkdown
 
-    @doc(['slug' => 'image'])
 
-        @image([
+
+    @doc(['slug' => 'image', 'displayParams' => false])
+    <div class="grid">
+        <div class="grid-s-12 grid-md-6">
+            @markdown
+            ###Just a plain simple Image
+            @endmarkdown
+
+            @image([
             'src'=> "https://picsum.photos/300/200?image=1026",
             'alt' => "This is a image",
-            'caption' => "Hey, I am a caption"
-        ])
-        @endimage
+            'caption' => "Hey, I am a caption for an image",
+            ])
+            @endimage
+        </div>
 
+        <div class="grid-s-12 grid-md-6">
+            @markdown
+            ###Open image in modal
+            @endmarkdown
+            @image([
+            'src'=> "https://picsum.photos/300/200?image=1026",
+            'alt' => "This is a image",
+            'caption' => "Click image to open a modal with image"
+            ])
+            @endimage
+        </div>
+    </div>
     @enddoc
+
+
+
+
+    @doc(['slug' => 'image'])
+    <div class="grid">
+        <div class="grid-s-12 grid-md-6">
+
+            @markdown
+            ###Rounded corners (All corners)
+            @endmarkdown
+
+            @image([
+            'src'=> "https://picsum.photos/300/200?image=1026",
+            'alt' => "This is a image",
+            'caption' => "Image with rounded corners (default size: md)",
+            'rounded' => true
+            ])
+            @endimage
+        </div>
+        <div class="grid-s-12 grid-md-6">
+
+            @markdown
+            ###Rounded (top-left - bottom-right)
+            @endmarkdown
+
+            @image([
+            'src'=> "https://picsum.photos/300/200?image=1026",
+            'alt' => "This is a image",
+            'caption' => "Larger corner radius (size: lg) with roundedRadius",
+            'roundedTopLeft' => true,
+            'roundedBottomRight' => true,
+            'roundedRadius' => "lg"
+            ])
+            @endimage
+        </div>
+    </div>
+    @enddoc
+
+
 @stop
