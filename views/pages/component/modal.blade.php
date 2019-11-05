cede66a0072d534bfe488be2134a476e2384c8a5
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-11-05 10:32:10 +0100

    Modal

--- views/pages/component/modal.blade.php
+++ views/pages/component/modal.blade.php
@@ -2,69 +2,63 @@
 
 @section('content')
     @markdown
-        #Modal
-        Popup for important content and notifications. 
+    #Modal
+    Popup for important content and notifications.
     @endmarkdown
 
     @doc(['slug' => 'modal'])
 
-<<<<<<< HEAD
-        @button(
-            [
-                'href' => '#',
-                'isOutlined' => false,
-                'background' => 'primary',
-                'text' => 'Open Modal',
-                'icon' => ['name' => 'favorite', 'color' => 'black'],
-                'size' => 'lg',
-                'color' => 'secondary',
-                'reverseIcon' => true,
-                'floating' => true,
-                'attributeList' => ['data-open' => 'exampleModalId']
-            ]
-        )
-        @endbutton
+    @button(
+        [
+            'href' => '',
+            'isOutlined' => false,
+            'background' => 'primary',
+            'text' => 'Open Modal',
+            'icon' => ['name' =>'favorite'],
+            'size' => 'lg',
+            'color' => 'secondary',
+            'reverseIcon' => true,
+            'floating' => true,
+            'attributeList' => ['data-open' => 'exampleModalId']
+        ]
+    )
+    @endbutton
 
-        @modal(
+    @modal(
             [
                 'heading'=> "Hey, have you seen this?",
                 'isPanel' => false,
                 'id' => 'exampleModalId',
                 'overlay' => 'dark',
                 'animation' => 'scale-up',
+                'classList' => [
+                    'u-border--2',
+                    'u-rounded-top--8',
+                    'u-rounded-bottom--8'
+                ]
             ]
         )
-        sWe are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-=======
-        @modal([
-            'heading'=> "Hey, have you seen this?",
-            'isPanel' => false,
-            'id' => 'exampleModal',
-            'overlay' => 'dark',
-            'animation' => 'scale-up',
-        ])
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
->>>>>>> c4e8f756ca2032fa7fd8a1de15e234c376aeb5ed
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
+
         We are presenting the sparkling new styleguide! Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-        
-        @endmodal
+
+    @endmodal
 
     @enddoc
 @stop
