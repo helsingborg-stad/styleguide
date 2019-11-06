827c53d56818326f2883e69f9ab699708ac44acc
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-11-06 10:03:42 +0100

    Postcss-normalize-charset and browserlist

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
         require('autoprefixer')({
-            browsers: ['> 1%', 'last 4 versions']
+            overrideBrowserslist: ['> 1%', 'last 4 versions']
         }),
         require('cssnano')
     ]
