a966615f7ba1d27f5c02936bbc95e2939a167fbf
Silvergrund <johan.silvergrund@hiq.se>
2019-11-14 13:43:37 +0100

    Cards

--- views/pages/component/card.blade.php
+++ views/pages/component/card.blade.php
@@ -14,13 +14,7 @@
 
         <div class="grid-s-12 grid-md-6">
             @card([
-                'href' => 'http://styleguide.helsingborg.se/card',
-                'image' => 'https://picsum.photos/300/200?image=1077',
                 'title' => 'Plats ingalunda varit, miljoner.',
-                'content' => 'Löksås ipsum dimmhöljd björnbär regn faktor sitt del har gamla, fram faktor dimma sista precis
-                därmed annat ännu söka, från se ingalunda dag vi äng plats är. Söka denna erfarenheter flera vidsträckt fram
-                dock stora söka, genom dimma blivit enligt vemod söka nya gör annan, mot annat där enligt faktor dimma
-                rännil.'
             ])
 
             @endcard
@@ -29,14 +23,7 @@
 
         <div class="grid-s-12 grid-md-6">
             @card([
-                'href' => 'http://styleguide.helsingborg.se/card',
-                'image' => 'https://picsum.photos/300/200?image=1077',
                 'title' => 'Plats ingalunda varit, miljoner.',
-                'content' => 'Löksås ipsum dimmhöljd björnbär regn faktor sitt del har gamla, fram faktor dimma sista precis
-                därmed annat ännu söka, från se ingalunda dag vi äng plats är. Söka denna erfarenheter flera vidsträckt fram
-                dock stora söka, genom dimma blivit enligt vemod söka nya gör annan, mot annat där enligt faktor dimma
-                rännil.'
-
             ])
 
             @endcard
