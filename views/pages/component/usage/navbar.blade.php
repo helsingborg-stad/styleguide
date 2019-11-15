40b9f819e24fdd14a1f47da23045f7a2794d0ab3
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2019-11-15 10:58:40 +0100

    Hotfix: navbar usage

--- views/pages/component/usage/navbar.blade.php
+++ views/pages/component/usage/navbar.blade.php
@@ -1,9 +1,37 @@
-@section('content')
-    {!!
-        markdown('
-            #Blank
+//Navbar
+@navbar([
+    'logo' => 'https://www.url.image.png',
+    'logoPosition' => 'left',
+    'itemsPosition' => 'right',
+    'items' => [
+        'Item 1' => [
+            'href' => '/item1',
+            'list' => [
+                'Subitem 1' => [
+                    'href' => '/item1/subitem1',
+                ],
+                'Subitem 2' => [
+                    'href' => '/item1/subitem2'
+                    'list' => [
+                        'Sub-Subitem 2' => [
+                            'href' => '/item1/subitem2/sub'
+                        ]
+                    ]
+                ]
+            ],
+        ],
+        
+        'Item 2' => [
+            'href' => '/item2'
+        ],
 
-            Demo page
-        ')
-    !!}
-@stop
+        'Item 3' => [
+            'href' => '/item3'
+        ],
+
+        'item 4' => [
+            'href' => '/item4'
+        ]
+    ]
+])
+@endnavbar
