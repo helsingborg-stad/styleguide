bab390c5fe92573a6c42d37af88ce9540fb28686
Silvergrund <johan.silvergrund@hiq.se>
2019-11-21 10:16:24 +0100

    conf

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 // Path to views directory
 define('VIEWS_PATH', BASEPATH . 'views/');
 define('CACHE_PATH', BASEPATH . 'cache/');
