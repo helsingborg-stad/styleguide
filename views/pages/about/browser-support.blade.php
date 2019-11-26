971afd3f6b2e28bd645389a9f9bb27e9b2b0222a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-11-26 15:36:02 +0100

    Browser compability list

--- views/pages/about/browser-support.blade.php
+++ views/pages/about/browser-support.blade.php
@@ -6,7 +6,14 @@
     {!! markdown("
 
         # Browser support
-        This styleguide has support for modern browsers, and IE 11. We do not guarantee support for older browsers. They may however render the styleguide well.
+        This styleguide has support for modern browsers that still is maintained by the developer. We do not guarantee support for older browsers. They may however render the styleguide well with minor details not displaying correctly. The following is supported at the moment of writing this. 
+
+        - Chrome (76, 77, 78)
+        - Firefox (68, 69, 70)
+        - Safari (12, 13)
+        - Opera (63, 64, 65)
+        - Edge (16, 17, 18)
+        - Internet Explorer 11 (minor visual defects)
         
     ") !!}
 
