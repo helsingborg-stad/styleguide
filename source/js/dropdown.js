7a62ed0d8f52e327d4e3d10e878f5e7a6ef4ce7f
Silvergrund <johan.silvergrund@hiq.se>
2019-11-29 12:24:31 +0100

    Dropdown - adjust dropdown if its to close to edge of screen

--- source/js/dropdown.js
+++ source/js/dropdown.js
@@ -2,11 +2,14 @@ class Dropdown{
 
     constructor(){
         this.DROPDOWNLISTVISIBLE = 'c-dropdown__list--visible';
+        this.onResize();
     }
 
+    /**
+     * Set Valid Targets
+     */
     setValidTargets(){
         let dropdowns = document.getElementsByClassName('c-dropdown--on-click');
-
 
         for (let dropdown of dropdowns) {
 
@@ -14,24 +17,67 @@ class Dropdown{
             let dropdownButton = dropdown.getElementsByTagName('button')[0];
             let buttonLabel = dropdownButton.getElementsByClassName('c-button__label-text')[0];
             let buttonIcon = dropdownButton.getElementsByTagName('i')[0];
-            let validTargets = [dropDownList, dropdownButton, buttonLabel, buttonIcon]
-        
-            this.toggle(validTargets, dropDownList);
+            let validTargets = [dropDownList, dropdownButton, buttonLabel, buttonIcon];
+            let offsetPositionClass = (!this.isInViewport(dropDownList)) ? true : false;
+
+            this.toggle(validTargets, dropDownList, offsetPositionClass);
         }
         
     }
 
-    toggle(validTargets, dropDownList){
+    /**
+     * Toogle dropdown on and of
+     * @param validTargets
+     * @param dropDownList
+     */
+    toggle(validTargets, dropDownList, offsetPositionClass){
         document.addEventListener('click', (event) => {
-            let target = event.target; 
-            if((validTargets.includes(target))  && !dropDownList.classList.contains(this.DROPDOWNLISTVISIBLE)){
+            let target = event.target;
+
+            if (offsetPositionClass){
+                if (!dropDownList.classList.contains('c-dropdown__auto--position-right')) {
+                    dropDownList.classList.add('c-dropdown__auto--position-right');
+                }
+                console.log('YES');
+            } else {
+                if (dropDownList.classList.contains('c-dropdown__auto--position-right')) {
+                    dropDownList.classList.remove('c-dropdown__auto--position-right');
+                }
+                console.log('NO');
+            }
+
+            if ((validTargets.includes(target))  && !dropDownList.classList.contains(this.DROPDOWNLISTVISIBLE)) {
                 dropDownList.classList.add(this.DROPDOWNLISTVISIBLE);
-            }else{
+            } else {
                 dropDownList.classList.remove(this.DROPDOWNLISTVISIBLE);
             }
         })   
     }
 
+
+    /**
+     * Resize - Run check
+     */
+    onResize(){
+        const self = this;
+        window.addEventListener('resize', function(){
+            self.setValidTargets();
+        });
+    }
+
+    /**
+     * Checking Bounding Client - if dropdown is to close to screen edge
+     * @param element
+     * @returns {boolean|boolean}
+     */
+    isInViewport(element) {
+        let bounding = element.getBoundingClientRect();
+        return (
+            bounding.top >= 0 && bounding.left >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
 }
 
 export default Dropdown;
