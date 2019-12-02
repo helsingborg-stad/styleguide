72ec30906f01c0e8225975847e406278db7a10f3
Silvergrund <johan.silvergrund@hiq.se>
2019-12-02 16:45:21 +0100

    Testimonials refactor

--- views/pages/component/testimonials.blade.php
+++ views/pages/component/testimonials.blade.php
@@ -12,20 +12,20 @@
                 array(
                     'name' => 'Kendal Ozzel',
                     'title' => 'Admiral',
-                    'testimonial' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-                    'image' => 'https://picsum.photos/300/200?image=1026'
+                    'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. ',
+                    'image' => 'https://picsum.photos/200/200?image=1022'
                 ),
                 array(
                     'name' => 'Maximilian Veers',
                     'title' => 'General',
-                    'testimonial' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.',
-                    'image' => 'https://picsum.photos/300/200?image=1026'
+                    'testimonial' => 'Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
+                    'image' => 'https://picsum.photos/200/200?image=1023'
                 ),
                 array(
                     'name' => 'Shaef Corssin',
                     'title' => 'Captain',
-                    'testimonial' => 'Nobis laborum corrupti dolorem voluptatem impedit.',
-                    'image' => 'https://picsum.photos/300/200?image=1026'
+                    'testimonial' => 'Pork belly porchetta pork loin meatloaf pastrami buffalo ball tip tenderloin, tail kevin pig alcatra tri-tip bacon. ',
+                    'image' => 'https://picsum.photos/200/200?image=1024'
                 ),
             )]
         )
--- views/pages/component/usage/testimonials.blade.php
+++ views/pages/component/usage/testimonials.blade.php
@@ -4,20 +4,21 @@
         array(
             'name' => 'Kendal Ozzel',
             'title' => 'Admiral',
-            'testimonial' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            'image' => 'https://picsum.photos/300/200?image=1026'
+            'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
+            'image' => 'https://picsum.photos/200/200?image=1026'
         ),
         array(
             'name' => 'Maximilian Veers',
             'title' => 'General',
-            'testimonial' => 'Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.',
-            'image' => 'https://picsum.photos/300/200?image=1026'
+            'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
+            'image' => 'https://picsum.photos/200/200?image=1026',
+
         ),
         array(
             'name' => 'Shaef Corssin',
             'title' => 'Captain',
-            'testimonial' => 'Nobis laborum corrupti dolorem voluptatem impedit.',
-            'image' => 'https://picsum.photos/300/200?image=1026'
+            'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
+            'image' => 'https://picsum.photos/200/200?image=1026'
         ),
     )]
 )
