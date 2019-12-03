2392121aff61e42219677e1e4da9d70f1e60bc6b
Silvergrund <johan.silvergrund@hiq.se>
2019-12-03 13:43:11 +0100

    Testimonials - layout

--- views/pages/component/testimonials.blade.php
+++ views/pages/component/testimonials.blade.php
@@ -5,61 +5,58 @@
         # Testimonials
     @endmarkdown
 
-    @doc(['slug' => 'testimonials', 'displayParams' => false])
+    @doc(['slug' => 'testimonials'])
         @testimonials(
             ['perRow' => 3,
             'testimonials' => array(
                 array(
-                    'name' => 'Kendal Ozzel',
-                    'title' => 'Admiral',
-                    'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. ',
-                    'image' => 'https://picsum.photos/200/200?image=1022'
+                    'name' => 'Dalai Lama',
+                    'title' => 'Spiritual and political leader of Tibetans',
+                    'titleElement' => 'h5',
+                    'testimonial' => "Anger and hatred are signs of weakness, while compassion is a sure sign of strength. Bless this component...",
+                    'image' => 'https://picsum.photos/200/200?image=1022',
+                    'quoteColor' => 'primary'
                 ),
                 array(
-                    'name' => 'Maximilian Veers',
-                    'title' => 'General',
-                    'testimonial' => 'Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
-                    'image' => 'https://picsum.photos/200/200?image=1023'
+                    'name' => 'Jesse Owens',
+                    'title' => 'American track athlete, OS gold winner.',
+                    'titleElement' => 'h5',
+                    'testimonial' => "For a time, at least, I was the most famous person in the entire world. Great work dudes!",
+                    'image' => 'https://picsum.photos/200/200?image=1023',
+                    'quoteColor' => 'secondary'
                 ),
                 array(
-                    'name' => 'Shaef Corssin',
-                    'title' => 'Captain',
-                    'testimonial' => 'Pork belly porchetta pork loin meatloaf pastrami buffalo ball tip tenderloin, tail kevin pig alcatra tri-tip bacon. ',
-                    'image' => 'https://picsum.photos/200/200?image=1024'
+                    'name' => 'John Lennon',
+                    'title' => 'A great musician, member of the Beatles',
+                    'testimonial' => "Everything will be okay in the end. If it’s not okay, it’s not the end. Love your stuff, Peace!",
+                    'titleElement' => 'h5',
+                    'image' => 'https://picsum.photos/200/200?image=1024',
+                    'quoteColor' => 'grey'
+                ),
+                array(
+                    'name' => 'Leonardo da Vinci',
+                    'title' => 'Painter, scientist, polymath',
+                    'testimonial' => "Blinding ignorance does mislead us. Oh! Wretched mortals, open your eyes! Keep up the good work.",
+                    'image' => 'https://picsum.photos/200/200?image=1012',
+                    'imageTop' => true
+                ),
+                array(
+                    'name' => 'Alfred Hitchcock',
+                    'title' => 'Film producer, director',
+                    'testimonial' => "A lot of movies are about life, mine are like a slice of cake. Yummi styleguide!",
+                    'image' => 'https://picsum.photos/200/200?image=1013',
+                    'imageTop' => true
+                ),
+                array(
+                    'name' => 'Audrey Hepburn',
+                    'title' => 'Actress and humanitarian',
+                    'testimonial' => "Let's face it, a nice creamy chocolate cake does a lot for a lot of people; it does for me.",
+                    'image' => 'https://picsum.photos/200/200?image=1014',
+                    'imageTop' => true
                 ),
             )]
         )
         @endtestimonials
     @enddoc
-    @doc(['slug' => 'testimonials'])
-        @testimonials(
-            [
-            'perRow' => 3,
-            'testimonials' => array(
-                array(
-                    'name' => 'Ozzy Ozbourne',
-                    'title' => 'Rock star',
-                    'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. ',
-                    'image' => 'https://picsum.photos/100/50?image=1028',
-                    'avatar' => false
-                ),
-                array(
-                    'name' => 'Richie Hawtin',
-                    'title' => 'Techno legend',
-                    'testimonial' => 'Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
-                    'image' => 'https://picsum.photos/100/50?image=1029',
-                    'avatar' => false
-                ),
-                array(
-                    'name' => 'Nat King Cole',
-                    'title' => 'Jazz God',
-                    'testimonial' => 'Pork belly porchetta pork loin meatloaf pastrami buffalo ball tip tenderloin, tail kevin pig alcatra tri-tip bacon. ',
-                    'image' => 'https://picsum.photos/100/50?image=1032',
-                    'avatar' => false
-                ),
-                )
-            ]
-        )
-        @endtestimonials
-    @enddoc
+
 @stop
--- views/pages/component/usage/testimonials.blade.php
+++ views/pages/component/usage/testimonials.blade.php
@@ -1,24 +1,29 @@
 @testimonials(
-    ['perRow' => 3,
+['perRow' => 3,
     'testimonials' => array(
         array(
-            'name' => 'Kendal Ozzel',
-            'title' => 'Admiral',
-            'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
-            'image' => 'https://picsum.photos/200/200?image=1026'
+            'name' => 'Dalai Lama',
+            'title' => 'Spiritual and political leader of Tibetans',
+            'titleElement' => 'h5',
+            'testimonial' => "Anger and hatred are signs of weakness, while compassion is a sure sign of strength. Bless this component...",
+            'image' => 'https://picsum.photos/200/200?image=1022',
+            'quoteColor' => 'primary'
         ),
         array(
-            'name' => 'Maximilian Veers',
-            'title' => 'General',
-            'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
-            'image' => 'https://picsum.photos/200/200?image=1026',
-
+            'name' => 'Jesse Owens',
+            'title' => 'American track athlete, OS gold winner.',
+            'titleElement' => 'h5',
+            'testimonial' => "For a time, at least, I was the most famous person in the entire world. Great work dudes!",
+            'image' => 'https://picsum.photos/200/200?image=1023',
+            'quoteColor' => 'secondary'
         ),
         array(
-            'name' => 'Shaef Corssin',
-            'title' => 'Captain',
-            'testimonial' => 'Bacon ipsum dolor amet pork chop picanha rump turducken meatball sausage spare ribs porchetta bacon. Swine shank landjaeger, tail bacon meatloaf biltong drumstick jerky filet mignon.',
-            'image' => 'https://picsum.photos/200/200?image=1026'
+            'name' => 'John Lennon',
+            'title' => 'A great musician, member of the Beatles',
+            'testimonial' => "Everything will be okay in the end. If it’s not okay, it’s not the end. Love your stuff, Peace!",
+            'titleElement' => 'h5',
+            'image' => 'https://picsum.photos/200/200?image=1024',
+            'quoteColor' => 'primary'
         ),
     )]
 )
