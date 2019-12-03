c02ad8a2d4d06bdeea826543c7e95dc7f997bc27
Eric Rosenborg <eric.rosenborg@hiq.se>
2019-12-03 13:36:19 +0100

    Material Icon Font Package
    Add materials official npm package for icons.
    Add proper list of icons and their names.
    Add search since there are now over 900 icons.
    Fix minor breakage in menu and card where some
    of the icons have changed names or got deleted.

--- views/pages/icons.blade.php
+++ views/pages/icons.blade.php
@@ -23,12 +23,53 @@
         Can be utilized by the component <?php echo "@icon(['icon' => 'home']) @endicon"; ?>. This page represents the complete list of icons avabile. 
     @endmarkdown
 
+
     @paper(['padding' => 3])
-        @foreach(HbgStyleGuide\Helper\Icons::getJson() as $iconKey => $utf)
-            @icon(['icon' => str_replace("icon--", "", $iconKey), 'size' => 'xl'])
-            @endicon
-        @endforeach
+        <div class="grid" style="margin-bottom: 16px;" js-filter-container="5da57cccd46c6">
+        
+            @field(
+                [
+                    'label' => false,
+                    'classList' => [],
+                    'textarea' => false,
+                    'attributeList' => [
+                    'name' => 'search',
+                    'id' => '303',
+                    'placeholder' => 'Search',
+                    'type' => 'text',
+                    'js-filter-input' => '5da57cccd46c6'
+                    ]
+                ]
+            )
+            @endfield
+
+            @foreach(HbgStyleGuide\Helper\Icons::getTxt() as $iconKey => $iconName)
+                <div class="grid-md-2 " style="word-break: break-word; text-align:center;" js-filter-item="">
+                    
+                    <div class="u-margin__bottom--3">      
+                        @icon(['icon' => $iconName, 'size' => 'xl'])
+                        @endicon
+                    </div>
+
+                    <span js-filter-data="" onclick="copy(this)" style="cursor:copy;">
+                        {{$iconName}}
+                    </span>
+                    
+                </div>
+            @endforeach
+        </div>
     @endpaper
 
 </article>
 @stop
+
+<script>
+    function copy(element) {
+        navigator.clipboard.writeText(element.innerText).then(function() {
+           
+        }, function() {
+          
+        });
+    }
+    
+</script>
