de4bc4fc7320fdcf0b7043994cbe388500396299
Silvergrund <johan.silvergrund@hiq.se>
2019-12-03 15:02:50 +0100

    Blade view - parameter data

--- views/pages/component/testimonials.blade.php
+++ views/pages/component/testimonials.blade.php
@@ -10,24 +10,24 @@
             ['perRow' => 3,
             'testimonials' => array(
                 array(
-                    'name' => 'Dalai Lama',
-                    'title' => 'Spiritual and political leader of Tibetans',
+                    'name' => 'अच्छा काम अच्छा है',
+                    'title' => ' Dalai Lama, Spiritual and political leader of Tibetans',
                     'titleElement' => 'h5',
                     'testimonial' => "Anger and hatred are signs of weakness, while compassion is a sure sign of strength. Bless this component...",
                     'image' => 'https://picsum.photos/200/200?image=1022',
                     'quoteColor' => 'primary'
                 ),
                 array(
-                    'name' => 'Jesse Owens',
-                    'title' => 'American track athlete, OS gold winner.',
+                    'name' => "WOW! Can't believe it.",
+                    'title' => 'Jesse Owens, American track athlete.',
                     'titleElement' => 'h5',
                     'testimonial' => "For a time, at least, I was the most famous person in the entire world. Great work dudes!",
                     'image' => 'https://picsum.photos/200/200?image=1023',
                     'quoteColor' => 'secondary'
                 ),
                 array(
-                    'name' => 'John Lennon',
-                    'title' => 'A great musician, member of the Beatles',
+                    'name' => 'Where is my flowers?',
+                    'title' => 'John Lennon, musician and member of the Beatles',
                     'testimonial' => "Everything will be okay in the end. If it’s not okay, it’s not the end. Love your stuff, Peace!",
                     'titleElement' => 'h5',
                     'image' => 'https://picsum.photos/200/200?image=1024',
