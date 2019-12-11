2561f2b216c260f801823190d1b4d751d4fd7544
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2019-12-11 16:52:52 +0100

    Navbar - Refactor  - Add new documentation  - Add imporved documentation  - Drop old documenation

---
+++ views/pages/component/usage/navbar/alignmentLeftRight.blade.php
@@ -0,0 +1,32 @@
+@navbar([
+    'logo' => '/assets/img/logotype.svg',
+    'logoPosition' => 'left',
+    'linksPosition' => 'right',
+    'topAccent' => 'primary',
+    'activeAccent' => 'primary',
+    'items' => [
+        [
+            'id' => '123',
+            'name' => 'Level 1 1',
+            'href' => '#',
+            'list' => true
+        ],
+        [
+            'id' => '79699676',
+            'name' => 'Level 2 1',
+            'href' => '#'
+        ],
+        [
+            'id' => '959656454',
+            'name' => 'Level 3 1',
+            'href' => '#'
+        ],
+        [
+            'id' => '46346346346123',
+            'name' => 'Level 4 1',
+            'href' => '#'
+        ],
+    ]
+])
+
+@endnavbar
