8001608bb61d1c4c136ce10a29674867094c7659
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-01-09 11:31:09 +0100

    Comment & Date  - Add Comment component  - Add Date component

---
+++ views/pages/component/usage/comment/comment.blade.php
@@ -0,0 +1,48 @@
+@segment([
+    'template' => 'featured',
+    'containContent' => true,
+    'height' => 'md',
+    'width' => 'lg',
+    'card' => [
+        'isCard' => true,
+        'background' => "gray",
+        'padding' => "10"
+    ],
+    'text_alignment' => 'right',
+    'content_alignment' => [
+        'vertical' => 'center',
+        'horizontal' => 'right'
+    ],
+    'article_heading' => [
+        "variant" => "h1",
+        "element" => "h2",
+        "slot" => ""
+    ],
+    'article_body' => ""
+])
+    @comment([
+        'author' => 'Peter Svensson',
+        'text' => 'This is a comment text',
+        'icon' => 'face',
+        'date' => '2019-12-01 17:25:43'
+    ])
+    @endcomment
+
+    @comment([
+        'author' => 'Peter Olsson',
+        'text' => 'This is a reply comment text',
+        'icon' => 'face',
+        'image' => 'https://picsum.photos/70/70?image=64',
+        'date' => '2020-01-01 17:25:43',
+        'is_reply' => true
+    ])
+    @endcomment
+
+    @comment([
+        'author' => 'Peter Svensson',
+        'date' => '2020-01-09 17:25:43'
+    ])
+        This comment was sent through the slot
+    @endcomment
+
+@endsegment
