66761456cdca4d54cb8d0ce7b71173d3a8ec4c0b
Silvergrund <johan.silvergrund@hiq.se>
2020-01-21 11:40:54 +0100

    Squashed commit of the following:
    commit 29212e21447168f13a887e3db94e4a4921a9be39
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Tue Jan 21 11:40:28 2020 +0100
    
        added fileinput
    
    commit 6bc1554c02484f42c12f7e81d4441c952c72000c
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Tue Jan 21 11:37:49 2020 +0100
    
        refactor fileinput
    
    commit 12305f9f43acd894fb737cfd7ad44888ea5cd113
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Tue Jan 21 11:34:15 2020 +0100
    
        refactor fileInput usage
    
    commit ca3740a4b4ee0ca24766d13409822224f226b5e3
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Tue Jan 21 11:21:09 2020 +0100
    
        refactor name fileinput
    
    commit 35f01f0c9b71af91ff0caa68ad358d9a54ba8e9c
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Tue Jan 21 10:59:04 2020 +0100
    
        sass imports

---
+++ views/pages/component/molecules/fileinput.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.master')
+@section('content')
+    @markdown
+        #File input
+        Displays a file input.
+    @endmarkdown
+
+    @doc(['slug' => 'fileinput'])
+
+        @fileinput([
+            'classList' => ['unlist'],
+            'display' => 'area',
+            'multiple' => true,
+            'label' => 'Choose files...'
+        ])
+        @endfileinput
+
+    @enddoc
+@stop
