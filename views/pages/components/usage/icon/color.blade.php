07c2f40a20780ffb69019e5910b0136c3973615a
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-13 14:23:30 +0100

    Utilize new docuemntation file structure

---
+++ views/pages/components/usage/icon/color.blade.php
@@ -0,0 +1,8 @@
+@icon(['icon' => 'apps', 'size' => 'lg','color' => 'primary'])
+@endicon
+
+@icon(['icon' => 'apps', 'size' => 'lg', 'color' => 'secondary'])
+@endicon
+
+@icon(['icon' => 'apps', 'size' => 'lg','color' => 'default'])
+@endicon
---
+++ views/pages/components/usage/icon/name.blade.php
@@ -0,0 +1,11 @@
+@icon(['icon' => 'close', 'size' => 'lg'])
+    Icon label will be deprecated
+@endicon
+
+@icon(['icon' => 'more_vert', 'size' => 'lg'])
+    Icon label will be deprecated
+@endicon
+
+@icon(['icon' => 'apps', 'size' => 'lg'])
+    Icon label will be deprecated
+@endicon
