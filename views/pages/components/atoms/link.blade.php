ac9b1dd89660e9d8bb8c24920380b5a274a54232
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-13 14:28:29 +0100

    Utilize new doc file strucutre

--- views/pages/components/atoms/link.blade.php
+++ views/pages/components/atoms/link.blade.php
@@ -2,17 +2,10 @@
 
 @section('doc-content')
     @markdown
-        #Link
-        A simple link, for link'ish purposes.         
+        #Link    
     @endmarkdown
 
     @doc(['slug' => 'link'])
-
-        @link([
-            'href' => 'http://helsingborg.se/?variant=1'
-        ])
-        A link
-        @endbutton
 
     @enddoc
 @stop
