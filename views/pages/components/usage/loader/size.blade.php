229d15560410ccead8948bee5c7903aee1d51150
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-13 14:43:56 +0100

    Loader usage doc
    Utilize new doc structure for loaders.

---
+++ views/pages/components/usage/loader/size.blade.php
@@ -0,0 +1,17 @@
+
+<div class="grid">
+    <div class="grid-ms-4 grid-md-4 grid-lg-4">
+        @loader(['size' => 'sm'])
+        @endloader
+    </div>
+
+    <div class="grid-ms-4 grid-md-4 grid-lg-4">
+        @loader(['size' => 'md'])
+        @endloader
+    </div>
+
+    <div class="grid-ms-4 grid-md-4 grid-lg-4">
+        @loader(['size' => 'lg'])
+        @endloader
+    </div>
+</div>
