4ca139b6788affc04abede0cfb68f069f635721b
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-17 15:53:58 +0100

    Tabs usage doc
    Move usage example to new usage dir

--- views/pages/components/molecules/tabs.blade.php
+++ views/pages/components/molecules/tabs.blade.php
@@ -6,22 +6,5 @@
     @endmarkdown
 
     @doc(['slug' => 'tabs'])
-        @paper
-            @tabs(['tabs' => array(
-                array(
-                    'title' => 'Tab one',
-                    'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo autem nihil veritatis natus, nemo numquam aut veniam, accusamus sapiente illum voluptates porro ad laborum repellendus dolore maiores distinctio obcaecati mollitia.'
-                ),
-                array(
-                    'title' => 'Tab two',
-                    'content' => 'Doloribus incidunt sapiente inventore! Unde cumque eum inventore! Sit architecto, corrupti in assumenda nulla, totam perspiciatis rem libero voluptates consequuntur illum fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-                ),
-                array(
-                    'title' => 'Tab Three',
-                    'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus incidunt sapiente inventore! Unde cumque eum inventore! Sit architecto, corrupti in assumenda nulla, totam perspiciatis rem libero voluptates consequuntur illum fuga!'
-                ),
-            )])
-            @endtabs
-        @endpaper
     @enddoc
 @stop
---
+++ views/pages/components/usage/tabs/tabs.blade.php
@@ -0,0 +1,15 @@
+@tabs(['tabs' => array(
+    array(
+        'title' => 'Tab one',
+        'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo autem nihil veritatis natus, nemo numquam aut veniam, accusamus sapiente illum voluptates porro ad laborum repellendus dolore maiores distinctio obcaecati mollitia.'
+    ),
+    array(
+        'title' => 'Tab two',
+        'content' => 'Doloribus incidunt sapiente inventore! Unde cumque eum inventore! Sit architecto, corrupti in assumenda nulla, totam perspiciatis rem libero voluptates consequuntur illum fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    ),
+    array(
+        'title' => 'Tab Three',
+        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus incidunt sapiente inventore! Unde cumque eum inventore! Sit architecto, corrupti in assumenda nulla, totam perspiciatis rem libero voluptates consequuntur illum fuga!'
+    ),
+)])
+@endtabs
