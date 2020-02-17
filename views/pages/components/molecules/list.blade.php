8945552ebe19fe9e2d8325a3b24b70b12c058c48
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-17 15:04:08 +0100

    Listing usage doc
    Move example to new usage dir.

--- views/pages/components/molecules/list.blade.php
+++ views/pages/components/molecules/list.blade.php
@@ -7,46 +7,6 @@
     @endmarkdown
 
     @doc(['slug' => 'listing'])
-
-    @listing(
-        [
-        'list' => [
-                ['label' => 'This is a list label for item 1'],
-                ['label' => 'This is a list label for item 2'],
-                ['href' => 'https://url-to-item.io', 'label' => 'This is a list label for item 3 with a link'],
-                ['href' => 'https://helsingborg.se',
-                'label' => 'Hey, Look I have kiddos!',
-                'childrenElementType' => "ul",
-                'children' => [
-                    [
-                        'href' => 'http://styleguide.helsingborg.se/',
-                        'label' => 'I am sub label',
-                    ],
-                    [
-                        'label' => 'I am another sub label 1',
-                    ],
-                    [
-                        'label' => 'I am another sub label 2',
-                    ],
-                    [
-                        'label' => 'Here you go, a list in a list in a list',
-                        'children' => [
-                            ['label' => 'I am another sub sub label 1'],
-                            ['label' => 'I am another sub sub label 2'],
-                            ['label' => 'I am another sub sub label 3'],
-                            ['label' => 'I am another sub sub label 4'],
-                            ['label' => 'I am another sub sub label 5'],
-                            ['label' => 'The list can go on and on and on and on....'],
-                        ]
-                    ]
-                ]
-            ],
-        ],
-        'elementType' => "ol"
-        ]
-    )
-    @endlisting
-
     @enddoc
 @stop
 
