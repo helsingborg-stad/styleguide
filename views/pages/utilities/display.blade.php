9ed5ad8e941cc00a25a3df7e6dd30c2f2b086dd5
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-02-17 10:52:57 +0100

    add better visual example and text

--- views/pages/utilities/display.blade.php
+++ views/pages/utilities/display.blade.php
@@ -6,12 +6,47 @@
     @markdown
         #Display
     @endmarkdown
-    @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'display', 'config' => 'display']])
-        <div class="u-display--inline-block u-color__bg--info">u-display--inline-block</div>
-        <div class="u-display--inline-block u-color__bg--success">u-display--inline-block</div>
 
-        <div class="u-display--block u-color__bg--success">u-display--block</div>
-        <div class="u-display--block u-color__bg--info">u-display--block</div>
+    @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'display', 'config' => 'display']])
+
+    @typography([
+        'variant' => 'headline',
+        'classList' => [
+            'u-color__text--primary',
+            'u-margin__bottom--0',
+        ]
+    ])
+        Inline Block
+    @endtypography
+
+    @typography([])
+        Displaying inline-block allows the element to stack side to side.
+    @endtypography
+
+    <div class="u-color__bg--primary u-padding--2 u-rounded">
+        <div class="u-display--inline-block u-rounded u-padding--1 u-color__bg--info">u-display--inline-block</div>
+        <div class="u-display--inline-block u-rounded u-padding--1 u-color__bg--success">u-display--inline-block</div>
+    </div>
+
+    @typography([
+        'variant' => 'headline',
+        'classList' => [
+            'u-color__text--primary',
+            'u-margin__bottom--0',
+            'u-margin__top--2'
+        ]
+    ])
+        Block
+    @endtypography
+
+    @typography([])
+        Displaying block makes the elements take up full with which results in them stacking on top of eachother.
+    @endtypography
+
+    <div class="u-color__bg--primary u-padding--2 u-rounded">
+        <div class="u-display--block u-rounded u-padding--1 u-color__bg--info">u-display--block</div>
+        <div class="u-display--block u-rounded u-padding--1 u-color__bg--success">u-display--block</div>
+    </div>
     @endutility_doc
 
 </article>
