4402d182b8abc6c51c4b6ca79d7c66d65263bea3
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-18 12:31:56 +0100

    Pagination usage doc
    Move usage example to new usage dir.

--- views/pages/components/organisms/pagination.blade.php
+++ views/pages/components/organisms/pagination.blade.php
@@ -9,17 +9,6 @@
     @endmarkdown
 
     @doc(['slug' => 'pagination'])
-
-        @pagination([
-            'list' => [
-                ['href' => '/component/pagination?pagination=1', 'label' => 'Page 1'],
-                ['href' => '/component/pagination?pagination=2', 'label' => 'Page 2'],
-                ['href' => '/component/pagination?pagination=3', 'label' => 'Page 3'],
-            ],
-            'current' => isset($_GET['pagination']) ? $_GET['pagination'] : 1
-        ])
-        @endpagination
-
     @enddoc
 @stop
 
