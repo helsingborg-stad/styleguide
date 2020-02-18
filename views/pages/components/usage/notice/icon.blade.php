f921bb861385ccf610ed5d883a5ff6cbf649a8bb
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-18 12:06:57 +0100

    Notice usage doc
    Move and add usage examples to new usage dir.

---
+++ views/pages/components/usage/notice/icon.blade.php
@@ -0,0 +1,37 @@
+@notice([
+    'type' => 'success',
+    'message' => [
+        'text' => 'Icon color red.',
+        'size' => 'sm'
+    ],
+    'icon' => [
+        'name' => 'check',
+        'size' => 'md',
+        'color' => 'primary'
+    ]
+])
+@endnotice
+
+@notice([
+    'type' => 'success',
+    'message' => [
+        'text' => 'Icon size small',
+        'size' => 'sm'
+    ],
+    'icon' => [
+        'name' => 'apps',
+        'size' => 'md',
+        'color' => 'white',
+        'size' => 'sm'
+    ]
+])
+@endnotice
+
+@notice([
+    'type' => 'success',
+    'message' => [
+        'text' => 'Without an icon.',
+        'size' => 'sm'
+    ]
+])
+@endnotice
---
+++ views/pages/components/usage/notice/messageSize.blade.php
@@ -0,0 +1,41 @@
+@notice([
+    'type' => 'success',
+    'message' => [
+        'text' => 'This message size is set to sm.',
+        'size' => 'sm'
+    ],
+    'icon' => [
+        'name' => 'check',
+        'size' => 'md',
+        'color' => 'white'
+    ]
+])
+@endnotice
+
+@notice([
+    'type' => 'info',
+    'message' => [
+        'text' => 'This message size is set to md.',
+        'size' => 'md'
+    ],
+    'icon' => [
+        'name' => 'report',
+        'size' => 'md',
+        'color' => 'white'
+    ]
+])
+@endnotice
+
+@notice([
+    'type' => 'danger',
+    'message' => [
+        'text' => 'This message size is set to lg.',
+        'size' => 'lg'
+    ],
+    'icon' => [
+        'name' => 'forum',
+        'size' => 'lg',
+        'color' => 'black'
+    ]
+])
+@endnotice
