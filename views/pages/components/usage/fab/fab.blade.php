63410027dee000b8837a27360b4ce885b5c1de04
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-18 09:53:30 +0100

    FAB usage doc
    Move usage example to new usage dir.

---
+++ views/pages/components/usage/fab/fab.blade.php
@@ -0,0 +1,26 @@
+@fab([
+    'position' => 'bottom-left',
+    'spacing' => 'md',
+    'button' => [
+        'icon' => 'close',
+        'size' => 'lg',
+        'color' => 'secondary'
+    ]
+])
+@endfab
+
+@fab([
+    'position' => 'bottom-right',
+    'spacing' => 'md'
+])
+
+    @button([
+        'type' => 'filled',
+        'icon' => 'close',
+        'size' => 'lg',
+        'color' => 'secondary'
+        
+    ])
+    @endbutton
+
+@endfab
