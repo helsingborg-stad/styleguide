6d138db787424e652fbb9f0c0ab8b0ced0d8fb76
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-02-19 14:35:53 +0100

    add improved vertical align docs

--- views/pages/utilities/vertical-align.blade.php
+++ views/pages/utilities/vertical-align.blade.php
@@ -8,27 +8,27 @@
     @endmarkdown
 
     @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'vertical-align', 'config' => 'vertical-align']])
-        
+        <div class="u-color__bg--secondary u-margin__bottom--2 u-display--inline-block">
             <span class="u-align--baseline">baseline</span>
             <span class="u-align--top">top</span>
             <span class="u-align--middle">middle</span>
             <span class="u-align--bottom">bottom</span>
             <span class="u-align--text-top">text-top</span>
             <span class="u-align--text-bottom">text-bottom</span>
+        </div>
 
-            <table style="height: 100px;">
-                <tbody>
-                    <tr>
-                        <td class="u-align--baseline">baseline</td>
-                        <td class="u-align--top">top</td>
-                        <td class="u-align--middle">middle</td>
-                        <td class="u-align--bottom">bottom</td>
-                        <td class="u-align--text-top">text-top</td>
-                        <td class="u-align--text-bottom">text-bottom</td>
-                    </tr>
-                </tbody>
-            </table>
-     
+        <table class="u-color__bg--secondary" style="height: 100px;">
+            <tbody>
+                <tr>
+                    <td class="u-align--baseline">baseline</td>
+                    <td class="u-align--top">top</td>
+                    <td class="u-align--middle">middle</td>
+                    <td class="u-align--bottom">bottom</td>
+                    <td class="u-align--text-top">text-top</td>
+                    <td class="u-align--text-bottom">text-bottom</td>
+                </tr>
+            </tbody>
+        </table>
     @endutility_doc
 
 
