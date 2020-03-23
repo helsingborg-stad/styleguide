aa8429ba55e780562d0b479f843036b0142d6711
Alexander <alexanderbomanskoug@gmail.com>
2020-03-23 09:39:30 +0100

    Hotfix: remove background on shadow boxes in shadow example

--- views/pages/utilities/shadows.blade.php
+++ views/pages/utilities/shadows.blade.php
@@ -10,11 +10,11 @@
     
     @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'shadows', 'config' => 'box-shadow']])
         <div class="d-shadows">
-            <div class='u-box-shadow--1 u-padding--2 u-rounded u-color__bg--info' >Level 1</div>
-            <div class='u-box-shadow--2 u-padding--2 u-rounded u-color__bg--info' >Level 2</div>
-            <div class='u-box-shadow--3 u-padding--2 u-rounded u-color__bg--info' >Level 3</div>
-            <div class='u-box-shadow--4 u-padding--2 u-rounded u-color__bg--info' >Level 4</div>
-            <div class='u-box-shadow--5 u-padding--2 u-rounded u-color__bg--info' >Level 5</div>
+            <div class='u-box-shadow--1 u-padding--2 u-rounded' >Level 1</div>
+            <div class='u-box-shadow--2 u-padding--2 u-rounded' >Level 2</div>
+            <div class='u-box-shadow--3 u-padding--2 u-rounded' >Level 3</div>
+            <div class='u-box-shadow--4 u-padding--2 u-rounded' >Level 4</div>
+            <div class='u-box-shadow--5 u-padding--2 u-rounded' >Level 5</div>
         </div>
         
     @endutility_doc 
