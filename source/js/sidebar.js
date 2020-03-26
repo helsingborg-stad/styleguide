0dd71582192e3e2a492161911c5f5533add66225
Alexander <alexanderbomanskoug@gmail.com>
2020-03-26 09:53:45 +0100

    Sidebar Improvements  - add script to expand current item

--- source/js/sidebar.js
+++ source/js/sidebar.js
@@ -1,10 +1,15 @@
 export default class Sidebar {
     constructor() {
         this.ATTR = "js-sidebar";
-        this.EXPAND = "c-sidebar__item--is-expanded";
-        this.EXPANDABLE = "c-sidebar__subcontainer";
         this.ACTIVE = "item-active";
         this.TRIGGER = "js-sidebar-trigger";
+        this.TOGGLE_ITEM = "js-toggle-item";
+        this.TOGGLE_TRIGGER = "js-toggle-trigger";
+        this.EXPAND = "c-sidebar__item--is-expanded";
+        this.SUBCONTAINER = "c-sidebar__subcontainer";
+        this.ITEM = "c-sidebar__item";
+        this.TOGGLE = "c-sidebar__toggle";
+        this.COLLAPSED = ".c-sidebar--collapsed";
     }
 
     /**
@@ -19,16 +24,27 @@ export default class Sidebar {
 
             if (activeItems.length > 0) {
                 activeItems.forEach((item) => {
-                    if (item.closest('.c-sidebar__subcontainer')) {
-                        item.closest('.c-sidebar__subcontainer').classList.add(this.EXPAND);
-                        const id = item.closest('.c-sidebar__subcontainer').getAttribute('js-toggle-item')
-                        sb.querySelector(`[js-toggle-trigger="${id}"]`).setAttribute('aria-pressed', true)
+                    if (item.closest(`.${this.SUBCONTAINER}`)) {
+                        item.closest(`.${this.SUBCONTAINER}`).classList.add(this.EXPAND);
+                        const id = item.closest(`.${this.SUBCONTAINER}`).getAttribute(this.TOGGLE_ITEM)
+                        sb.querySelector(`[${this.TOGGLE_TRIGGER}="${id}"]`).setAttribute('aria-pressed', true)
                     }
+                    this.expandCurrentItem(item);
                 })
             }
             
             this.addTriggers(sb);
         }
+    }
+
+    /**
+     * Expand current item
+     * @param {Object} item The current item to expand
+     */
+    expandCurrentItem(item) {
+        const parent = item.closest(`.${this.ITEM}`);
+        parent.querySelector(`.${this.SUBCONTAINER}`).classList.add(this.EXPAND);
+        parent.querySelector(`.${this.TOGGLE}`).setAttribute('aria-pressed', true);
     }
 
     /**
@@ -40,7 +56,7 @@ export default class Sidebar {
 
         sbTriggers.forEach(btn => {
             btn.addEventListener('click', (e) => {
-                sb.classList.toggle('c-sidebar--collapsed');
+                sb.classList.toggle(`.${this.COLLAPSED}`);
             })
         });
     }
