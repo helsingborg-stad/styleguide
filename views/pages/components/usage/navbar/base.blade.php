16083fa0f02e6139a3bf8fe2a375c3eaf4bc8d6d
Alexander <alexanderbomanskoug@gmail.com>
2020-04-02 15:35:26 +0200

    Navbar Desktop Redesign  - add new styling  - add new layouts  - add new example  - add minor clean up  - remove old css file

--- views/pages/components/usage/navbar/base.blade.php
+++ views/pages/components/usage/navbar/base.blade.php
@@ -17,6 +17,25 @@
             "icon" => "people"
         ]
     ],
+    'expanded_prev' => 'Hem',
+    'expanded_current' => 'Bidrag och Underhåll',
+    'expanded_menu' => [
+        "about" => [
+            "label" => "About",
+            "href" => "#",
+            "icon" => "people"
+        ],
+        "stuff" => [
+            "label" => "Stuff",
+            "href" => "#",
+            "icon" => "people"
+        ],
+        "more" => [
+            "label" => "More",
+            "href" => "#",
+            "icon" => "people"
+        ]
+    ],
     'classList' => ['u-position--relative']
 ])
 
