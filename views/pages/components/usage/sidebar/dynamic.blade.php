f6e0c3c4a7159d578804c2b16fcc9207b240417d
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-04-15 14:23:05 +0200

    Dynamic child items
    Dynamically fetch child items in sidenav via a supplied
    endpoint.
    
    Add usage doc example.

---
+++ views/pages/components/usage/sidebar/dynamic.blade.php
@@ -0,0 +1,9 @@
+@sidebar([
+    'logo' => '/assets/img/logotype-grey-full.svg',
+    'items' => \HbgStyleGuide\Navigation::getMockedTopLevel(),
+    'showHideButton' => false,
+    'classList' => ['u-position--relative'],
+    'childItemsUrl' => '/'
+])
+
+@endsidebar
