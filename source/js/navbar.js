149536e1b772a17a989c98daa31bae22c5c77c04
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-04-22 09:57:07 +0200

    wip

--- source/js/navbar.js
+++ source/js/navbar.js
@@ -1,40 +1,99 @@
 export default class Navbar{
     constructor() {
-        this.navbarExpandedGrid = document.querySelector('.c-navbar__expanded_main[js-is-multidimensional="1"] > .c-navbar__expanded_grid');
-    }
-
     
+        this.dynamicNavBar = document.querySelector('.c-navbar[js-is-dynamic]');
 
-   static storeCurrentPageID(id) {
-        localStorage.setItem('currentPageID', id);
+        if(this.dynamicNavBar){
+            this.pageID = this.dynamicNavBar.getAttribute('data-page-id');
+            this.pageParentID = this.dynamicNavBar.getAttribute('data-page-parent-id');
+            
+            this.navbarExpandedGrid = this.dynamicNavBar.querySelector('.c-navbar__expanded_grid');
+            this.navBarGridItems = this.navbarExpandedGrid.querySelectorAll('a');
+            this.childItemsUrl = this.dynamicNavBar.getAttribute('js-child-items-url');
+            this.navbarExpandedMain = this.dynamicNavBar.querySelector('.c-navbar__expanded_main');
+            this.currentPageID = this.constructor.getPageID('currentPageID');
+    
+            this.setLinkEventHandlers();
+    
+            if(this.currentPageID) {
+                this.cleanUpNavBarContent();
+                this.populateNavBarContent();
+                this.modifyPrevButton();
+                this.setLinkEventHandlers();
+            }
+        }
+        
     }
 
-    static getCurrentPageID() {
-        localStorage.getItem('currentPageID');
+    static storePageID(id, type) {
+        localStorage.setItem(type, id);
+    }
+
+    static getPageID(type) {
+        return localStorage.getItem(type);
     }
 
     cleanUpNavBarContent() {
         this.navbarExpandedGrid.innerHTML = ' ';
     }
 
-    setLinkEventHandlers() {
-        const navBarGridItems = this.navbarExpandedGrid.querySelectorAll('a');
-        
-        navBarGridItems.forEach(item => {
-            const currentPageID = item.getAttribute('pageid');
-            
-            item.addEventListener('click', (event) => {
-                this.constructor.storeCurrentPageID(currentPageID);
-            });
+    getCurrentPageItems() {
+
+        return fetch(`${this.childItemsUrl}?parentID=${this.currentPageID}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return data;
         });
     }
 
-    setNavBarContent() {
-        const navBarContent = this.navBarContent.innerHTML;
-        const currentPageID = this.getCurrentPageID();
+    populateNavBarContent() {
+        this.getCurrentPageItems().then((currentPageItems) => {
+            
+            currentPageItems.items.forEach((item) => {
+                const anchorElement = this.constructor.createAnchorElement(item);
 
-        /* if(navBarContent) */
+                this.navbarExpandedGrid.innerHTML += anchorElement;
+                this.navbarExpandedMain.querySelector('h2').innerText = currentPageItems.title;
+            });
+        });
+    
     }
 
+    static createAnchorElement(item) {
+        return `
+            <a class="c-grid u-margin__y--4 c-grid__container__gap__col--0 c-grid__container__gap__row--0" href="${item.href}" pageid="${item.id}">
+                
+                <h2 class="c-typography c-navbar__title c-typography__variant--h2">
+                    ${item.label}
+                </h2>
+        
+                <p class="c-typography c-typography__variant--body">
+                    ${item.preview ? item.preview : 'No Preview Available'}
+                </p>
+            </a>
+        `;
+        
+    }
 
+/*     modifyPrevButton() {
+        
+    } */
+
+    setLinkEventHandlers() {
+        this.navBarGridItems.forEach(item => {
+            const currentPageID = item.getAttribute('pageid');
+            
+            item.addEventListener('click', (event) => {
+                this.constructor.storePageID(currentPageID, 'currentPageID');
+
+                if(this.currentPageID) {
+                    this.storePageID(this.currentPageID, 'previousPageID');
+                }
+            });
+        });
+
+
+    }
 }
