89fc1b3a016bd3cb293f70670d2bf088ae45edef
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-09-08 13:49:53 +0200

    Added video background to segment

--- views/pages/components/usage/slider/slider.blade.php
+++ views/pages/components/usage/slider/slider.blade.php
@@ -6,7 +6,7 @@
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-        'background_image' => 'https://i.picsum.photos/id/566/1080/720.jpg',
+        'background_image' => 'https://picsum.photos/1080/720',
         'overlay' => 'light',
         'overlay_opacity' => 'high',
     ])
@@ -41,7 +41,7 @@
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-        'background_image' => 'https://i.picsum.photos/id/876/1080/720.jpg',
+        'background_image' => 'https://picsum.photos/1080/720',
         'overlay' => 'light',
         'overlay_opacity' => 'high'
     ])
@@ -76,7 +76,8 @@
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-        'background_image' => 'https://i.picsum.photos/id/576/1080/720.jpg',
+        'background_image' => 'https://picsum.photos/1080/720',
+        'background_video' => 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4',
         'overlay' => 'light',
         'overlay_opacity' => 'high'
     ])
