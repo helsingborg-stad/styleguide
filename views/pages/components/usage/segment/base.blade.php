a5fdb5b9ed023b32b5b8b9df5ccff6c121ed2acd
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-09-30 14:13:32 +0200

    - remove outdated documentation

--- views/pages/components/usage/segment/base.blade.php
+++ views/pages/components/usage/segment/base.blade.php
@@ -2,7 +2,7 @@
     'title' => 'Base Layout',
     'sub_title' => 'This is a slightly longer title called subtitle',
     'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-    'background_image' => 'https://i.picsum.photos/id/876/1080/720.jpg',
+    'background_image' => 'https://picsum.photos/1080/720',
     'overlay' => 'light',
     'overlay_opacity' => 'high'
 ])
--- views/pages/components/usage/segment/col-right.blade.php
+++ views/pages/components/usage/segment/col-right.blade.php
@@ -5,23 +5,6 @@
     'height' => 'lg',
     'color' => 'black'
 ])
-    @slot('top')
-    @endslot
-
-    @slot('main')
-        @card([
-            'heading' => 'Heading',
-            'subHeading' => 'SubHeading',
-            'content' => 'Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components.',
-            'image' => [
-                'src' => '/assets/img/component.svg',
-                'alt' => 'ALT',
-                'backgroundColor' => 'secondary',
-                'padded' => true
-            ]
-        ])
-        @endcard
-    @endslot
 
     @slot('bottom')
         @button([
