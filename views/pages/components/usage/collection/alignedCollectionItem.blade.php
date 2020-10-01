8818cbe35f9aff01b39ec35393384c025208c0e2
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-10-01 14:08:03 +0200

    Documentation

---
+++ views/pages/components/usage/collection/alignedCollectionItem.blade.php
@@ -0,0 +1,23 @@
+@collection()
+  @collectionItem(['isAligned' => true])
+
+      @avatar(
+        [
+          'name' => "Cookie Monster",
+          'icon' => [
+              'name' => 'favorite',
+              'size' => 'xl'
+          ]
+        ]
+      )
+      @endavatar
+
+      Dolor Bibendum Inceptos Nibh
+    @endcollectionItem
+    @collectionItem(['isAligned' => true])
+      Condimentum Ullamcorper
+    @endcollectionItem
+    @collectionItem(['isAligned' => true])
+      Pharetra Ultricies Nullam Venenatis Adipiscing
+    @endcollectionItem
+@endcollection
