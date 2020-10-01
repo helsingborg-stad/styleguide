34d42974e493cb836202fad3d5a31ef3323c4132
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-10-01 11:10:04 +0200

    - added usage of container

--- views/pages/components/usage/segment/base.blade.php
+++ views/pages/components/usage/segment/base.blade.php
@@ -3,8 +3,7 @@
     'sub_title' => 'This is a slightly longer title called subtitle',
     'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
     'background_image' => 'https://picsum.photos/1080/720',
-    'overlay' => 'light',
-    'overlay_opacity' => 'high'
+    'containerColor' => 'tertiary'
 ])
 
     @slot('bottom')
