010a89fe60bf28885f4d52592e5b2a833754930b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-10-12 15:04:30 +0200

    badge docs.

--- views/pages/components/usage/date/dateBadge.blade.php
+++ views/pages/components/usage/date/dateBadge.blade.php
@@ -1,6 +1,14 @@
 @date([
     'action' => 'formatDate',
     'timestamp' => '2020-01-01 17:25:43',
-    'dateBadge' => true
+    'badge' => true
+])
+@enddate
+
+@date([
+    'action' => 'formatDate',
+    'timestamp' => ['2020-01-01 17:25:43', '2020-01-02 17:25:43'],
+    'badge' => true,
+    'badgeSize' => 'md'
 ])
 @enddate
