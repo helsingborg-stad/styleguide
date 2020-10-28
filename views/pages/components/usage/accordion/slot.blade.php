b1e646ab8728e35df933cfb854c7e1b04ae0843b
Alexander Boman Skoug <39676080+alexanderbomanskoug2@users.noreply.github.com>
2020-10-28 14:14:13 +0100

    Feature/contacts v2 (#318)
    * Updated documentation
    
    * - added square image option to card

---
+++ views/pages/components/usage/accordion/slot.blade.php
@@ -0,0 +1,45 @@
+@accordion([])
+    @accordion__item([
+        'heading' => 'First item'
+    ])
+        <div id="" class="c-collection" data-uid="5f8fe0c4ac047">
+            <!-- collection__item.blade.php -->
+            <a id="" class="c-collection__item c-collection__item--action" href="https://link.link" data-uid="5f8fe0c4ab694">
+            <div class="c-collection__content">
+            Linked item
+            </div></a> <!-- collection__item.blade.php -->
+            <div id="" class="c-collection__item" data-uid="5f8fe0c4ab97f">
+            <div class="c-collection__content">
+                Unlinked item
+            </div>
+            </div><!-- collection__item.blade.php -->
+            <div id="" class="c-collection__item" data-uid="5f8fe0c4abc86">
+            <div class="c-collection__content">
+                Unlinked item
+            </div>
+            </div>
+        </div>   
+    @endaccordion__item
+
+    @accordion__item([
+        'heading' => 'Second Item'
+    ])
+        <div id="" class="c-collection" data-uid="5f8fe0c4ac047">
+            <!-- collection__item.blade.php -->
+            <a id="" class="c-collection__item c-collection__item--action" href="https://link.link" data-uid="5f8fe0c4ab694">
+            <div class="c-collection__content">
+            Linked item
+            </div></a> <!-- collection__item.blade.php -->
+            <div id="" class="c-collection__item" data-uid="5f8fe0c4ab97f">
+            <div class="c-collection__content">
+                Unlinked item
+            </div>
+            </div><!-- collection__item.blade.php -->
+            <div id="" class="c-collection__item" data-uid="5f8fe0c4abc86">
+            <div class="c-collection__content">
+                Unlinked item
+            </div>
+            </div>
+        </div>   
+    @endaccordion__item
+@endaccordion
