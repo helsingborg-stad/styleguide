4971d5dfdb3a8cd93d979096b1bfb0a7a5c72289
Joel Bernerman <joel.bernerman@helsingborg.se>
2020-11-11 15:32:14 +0100

    More indentation fix

--- views/pages/components/usage/group/horizontal.blade.php
+++ views/pages/components/usage/group/horizontal.blade.php
@@ -1,22 +1,22 @@
 @group(['direction' => 'horizontal'])
 
-  @field([
-      'type' => 'text',
-      'attributeList' => [
-          'type' => 'text',
-          'name' => 'text',
-      ],
-      'label' => "What are you looking for?",
-      'classList' => ['u-width--50']
-  ])
-  @endfield
+    @field([
+        'type' => 'text',
+        'attributeList' => [
+            'type' => 'text',
+            'name' => 'text',
+        ],
+        'label' => "What are you looking for?",
+        'classList' => ['u-width--50']
+    ])
+    @endfield
 
-  @button([
-    'color' => 'primary',
-    'size' => 'md',
-    'text' => 'Search',
-    'background' => 'default'
-  ])
-  @endbutton
+    @button([
+        'color' => 'primary',
+        'size' => 'md',
+        'text' => 'Search',
+        'background' => 'default'
+    ])
+    @endbutton
 
 @endgroup
