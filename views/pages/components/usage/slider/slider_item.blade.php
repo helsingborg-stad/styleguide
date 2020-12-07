20c15d7ff1bf665aa42c1c9fd3e0f7ac228edda0
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-12-07 14:08:28 +0100

    added example of slider item use

--- views/pages/components/usage/slider/slider_item.blade.php
+++ views/pages/components/usage/slider/slider_item.blade.php
@@ -6,147 +6,43 @@
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-        'layout' => 'left',
-        'containerColor' => 'tertiary',
+        'layout' => 'bottom',
+        'containerColor' => 'theme',
+        'textColor' => 'white',
         'background_image' => 'https://picsum.photos/1080/720',
-        'overlay' => 'light',
-        'overlay_opacity' => 'medium',
     ])
-    
-        @slot('bottom')
-            @button([
-                'text' => 'Primary filled',
-                'color' => 'primary',
-                'type' => 'filled'
-    
-            ])
-            @endbutton
-    
-            @button([
-                'text' => 'Secondary filled',
-                'color' => 'secondary',
-                'type' => 'filled'
-    
-            ])
-            @endbutton
-    
-            @button([
-                'text' => 'Default filled',
-                'color' => 'default',
-                'type' => 'filled'
-            ])
-            @endbutton
-        @endslot
     @endslider__item
 
     @slider__item([
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-        'layout' => 'left',
-        'containerColor' => 'tertiary',
+        'layout' => 'center',
+        'containerColor' => 'none',
         'background_image' => 'https://picsum.photos/1080/720',
         'overlay' => 'light',
         'overlay_opacity' => 'medium'
     ])
-
-        @slot('bottom')
-            @button([
-                'text' => 'Primary filled',
-                'color' => 'primary',
-                'type' => 'filled'
-
-            ])
-            @endbutton
-
-            @button([
-                'text' => 'Secondary filled',
-                'color' => 'secondary',
-                'type' => 'filled'
-
-            ])
-            @endbutton
-
-            @button([
-                'text' => 'Default filled',
-                'color' => 'default',
-                'type' => 'filled'
-            ])
-            @endbutton
-        @endslot
     @endslider__item
 
     @slider__item([
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-        'layout' => 'left',
-        'containerColor' => 'tertiary',
+        'layout' => 'bottom',
+        'containerColor' => 'white',
+        'textColor' => 'theme',
         'background_image' => 'https://picsum.photos/1080/720',
         'background_video' => 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4',
-        'overlay' => 'light',
-        'overlay_opacity' => 'medium'
     ])
-
-        @slot('bottom')
-            @button([
-                'text' => 'Primary filled',
-                'color' => 'primary',
-                'type' => 'filled'
-
-            ])
-            @endbutton
-
-            @button([
-                'text' => 'Secondary filled',
-                'color' => 'secondary',
-                'type' => 'filled'
-
-            ])
-            @endbutton
-
-            @button([
-                'text' => 'Default filled',
-                'color' => 'default',
-                'type' => 'filled'
-            ])
-            @endbutton
-        @endslot
     @endslider__item
 
     @slider__item([
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-        'containerColor' => 'tertiary',
-        'layout' => 'left',
-        'overlay' => 'light',
-        'overlay_opacity' => 'medium'
+        'containerColor' => 'white',
+        'layout' => 'center',
     ])
-
-        @slot('bottom')
-            @button([
-                'text' => 'Primary filled',
-                'color' => 'primary',
-                'type' => 'filled'
-
-            ])
-            @endbutton
-
-            @button([
-                'text' => 'Secondary filled',
-                'color' => 'secondary',
-                'type' => 'filled'
-
-            ])
-            @endbutton
-
-            @button([
-                'text' => 'Default filled',
-                'color' => 'default',
-                'type' => 'filled'
-            ])
-            @endbutton
-        @endslot
     @endslider__item
 @endslider
