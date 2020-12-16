8cc7409f41e3bb3a97f6c3ac99492324c7851dfc
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-12-16 17:43:33 +0100

    Improved implementation of mobile and desktop iamges

--- views/pages/components/usage/slider/slider_item.blade.php
+++ views/pages/components/usage/slider/slider_item.blade.php
@@ -9,7 +9,8 @@
         'layout' => 'bottom',
         'containerColor' => 'theme',
         'textColor' => 'white',
-        'background_image' => 'https://picsum.photos/1080/720',
+        'mobile_image' => 'https://picsum.photos/720/720',
+        'desktop_image' => 'https://picsum.photos/1080/720',
     ])
     @endslider__item
 
@@ -19,7 +20,8 @@
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'layout' => 'center',
         'containerColor' => 'none',
-        'background_image' => 'https://picsum.photos/1080/720',
+        'mobile_image' => 'https://picsum.photos/720/720',
+        'desktop_image' => 'https://picsum.photos/1080/720',
         'overlay' => 'light',
         'overlay_opacity' => 'medium'
     ])
@@ -32,7 +34,8 @@
         'layout' => 'bottom',
         'containerColor' => 'white',
         'textColor' => 'theme',
-        'background_image' => 'https://picsum.photos/1080/720',
+        'mobile_image' => 'https://picsum.photos/720/720',
+        'desktop_image' => 'https://picsum.photos/1080/720',
         'background_video' => 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4',
     ])
     @endslider__item
