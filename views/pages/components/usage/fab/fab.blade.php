35ac0e0fa8d47ab15c265408805d9cc3a83b4b8b
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2021-06-15 10:14:56 +0200

    Add fab panel (#380)
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- views/pages/components/usage/fab/fab.blade.php
+++ views/pages/components/usage/fab/fab.blade.php
@@ -1,31 +1,22 @@
 @fab([
-    'position' => 'bottom-left',
-    'spacing' => 'md',
-    'button' => [
-        'icon' => 'close',
-        'size' => 'lg',
-        'color' => 'secondary'
-    ],
-    'classList' => ['d-fab__left','u-position--static']
-])
-@endfab
-
-@fab([
     'position' => 'bottom-right',
-    'spacing' => 'md',
-    'classList' => [
-        'u-position--static'
-    ]
+    'heading' => "Vestibulum id ligula porta felis euismod semper",
+    'button' => [
+        'icon' => 'apps',
+        'size' => 'md',
+        'color' => 'primary',
+        'text' => 'Open menu',
+        'reversePositions' => true,
+    ],
+    'classList' => []
 ])
 
-    @button([
-        'type' => 'filled',
-        'icon' => 'close',
-        'size' => 'lg',
-        'text' => 'Right',
-        'color' => 'primary'
-        
+    @nav([
+        'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
+        'direction' => 'vertical',
+        'includeToggle' => false,
+        'classList' => ['c-nav--tiles']
     ])
-    @endbutton
+    @endnav
 
 @endfab
