8c35e1040b2c8218b3c014c4f3332d8ae01422ce
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-06-16 09:56:59 +0200

    Make icons follow button sizes

---
+++ views/pages/components/usage/button/sizewithicon.blade.php
@@ -0,0 +1,29 @@
+@button([
+'color' => 'secondary',
+'size' => 'sm',
+'text' => 'Size sm',
+'background' => 'default',
+'icon' => 'home',
+'reversePositions' => 'true'
+])
+@endbutton
+
+@button([
+'color' => 'secondary',
+'size' => 'md',
+'text' => 'Size md',
+'background' => 'default',
+'icon' => 'home',
+'reversePositions' => 'true'
+])
+@endbutton
+
+@button([
+'color' => 'secondary',
+'size' => 'lg',
+'text' => 'Size lg',
+'background' => 'default',
+'icon' => 'home',
+'reversePositions' => 'true'
+])
+@endbutton
