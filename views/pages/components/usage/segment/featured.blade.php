00d0a79f5042d2c16b9c794b91590ab27ffcb621
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-06-21 13:54:20 +0200

    removed featured view and minor css fixes

--- views/pages/components/usage/segment/featured.blade.php
+++
@@ -1,9 +0,0 @@
-@segment([
-    'layout'            => 'featured',
-    'background'        => 'https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I',
-    'textColor'         => 'dark',
-    'overlay'           => 'light',
-    'reverseColumns'    => true
-])
-
-@endsegment
