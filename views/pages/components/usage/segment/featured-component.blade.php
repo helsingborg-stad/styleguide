5b8be3828624c81d49960040b35d6372af6f6374
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-06-21 10:33:19 +0200

    updated documentation

---
+++ views/pages/components/usage/segment/featured-component.blade.php
@@ -0,0 +1,23 @@
+@segment([
+    'layout'            => 'featured',
+    'background'        => 'secondary',
+    'textColor'         => 'dark',
+    'overlay'           => 'light',
+    'reverseColumns'    => true
+])
+
+    @notice([
+        'type' => 'success',
+        'message' => [
+            'text' => 'Tellus Sem Lorem Malesuada Ipsum',
+            'size' => 'sm'
+        ],
+        'icon' => [
+            'name' => 'check',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
+    
+@endsegment
