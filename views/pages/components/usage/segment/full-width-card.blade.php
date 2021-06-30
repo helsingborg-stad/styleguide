93bc1caf8d8079e24ae46573615462178d560c7e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-06-30 10:04:19 +0200

    Further development on segments
    Added slot compontents on both full-width and split

---
+++ views/pages/components/usage/segment/full-width-card.blade.php
@@ -0,0 +1,56 @@
+@segment([
+    'layout'            => 'full-width',
+    'image'             => 'https://i.picsum.photos/id/308/1536/1024.jpg?hmac=sENdBVMjJ5k40eSlDyPh8CZKbXjOm1S73hukmgfyMKQ',
+    'background'        => 'primary',
+    'textColor'         => 'light',
+    'overlay'           => 'dark',
+    'reverseColumns'    => true
+])
+
+    <div class="o-grid">
+
+        <div class="o-grid-4@lg">
+            @card([
+                'heading' => 'Heading',
+                'subHeading' => 'SubHeading', 
+                'content' => 'Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components.',
+                'image' => [
+                    'src' => 'https://picsum.photos/400/300?a',
+                    'alt' => 'ALT', 
+                    'backgroundColor' => 'secondary'
+                ]
+            ])
+            @endcard
+        </div>
+
+        <div class="o-grid-4@lg">
+            @card([
+                'heading' => 'Heading',
+                'subHeading' => 'SubHeading', 
+                'content' => 'Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components.',
+                'image' => [
+                    'src' => 'https://picsum.photos/400/300?b',
+                    'alt' => 'ALT', 
+                    'backgroundColor' => 'secondary'
+                ]
+            ])
+            @endcard
+        </div>
+
+        <div class="o-grid-4@lg">
+            @card([
+                'heading' => 'Heading',
+                'subHeading' => 'SubHeading', 
+                'content' => 'Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components.',
+                'image' => [
+                    'src' => 'https://picsum.photos/400/300?c',
+                    'alt' => 'ALT', 
+                    'backgroundColor' => 'secondary'
+                ]
+            ])
+            @endcard
+        </div>
+
+    </div>
+
+@endsegment
--- views/pages/components/usage/segment/reversed.blade.php
+++ views/pages/components/usage/segment/reversed.blade.php
@@ -1,6 +1,6 @@
 @segment([
     'layout'        => 'split',
-    'image'         => 'https://picsum.photos/1080/720',
+    'image'         => 'https://picsum.photos/1080/720?d',
     'height'        => 'full-screen',
     'textColor'     => 'dark',
     'textAlignment' => 'bottom',
