e4868ae2f0cc2b52ffc649edb4b2d667073393c1
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-08-20 09:38:21 +0200

    - add hero style to docuementation

--- views/pages/components/usage/slider/slider_item.blade.php
+++ views/pages/components/usage/slider/slider_item.blade.php
@@ -6,6 +6,19 @@
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
+        'layout' => 'center',
+        'containerColor' => 'transparent',
+        'textColor' => 'white',
+        'mobile_image' => 'https://picsum.photos/720/720',
+        'desktop_image' => 'https://picsum.photos/1080/720',
+        'heroStyle' => true
+    ])
+    @endslider__item
+
+    @slider__item([
+        'title' => 'Base Layout',
+        'sub_title' => 'This is a slightly longer title called subtitle',
+        'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'layout' => 'bottom',
         'containerColor' => 'theme',
         'textColor' => 'white',
