fba36a52dbcfb7611d4b88c6259b1eb4fa2680ed
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-09-07 13:49:34 +0200

    - add new styling for new layout

--- views/pages/components/organisms/testimonials.blade.php
+++ views/pages/components/organisms/testimonials.blade.php
@@ -7,31 +7,26 @@
 
     @doc(['slug' => 'testimonials'])
         @testimonials(
-            ['perRow' => 4,
+            ['perRow' => 1,
             'testimonials' => array(
                 array(
-                    'name' => 'Alfred Nilsson',
+                    'name' => 'David Conradi',
                     'title' => ' Matematik- och idrottslärare',
-                    'testimonial' => "Anger and hatred are signs of weakness, while compassion is a sure sign of strength. Bless this component...",
+                    'testimonial' => 'Det bästa med Filbornaskolan är bredden på programutbudet samt att all personal är engagerad och arbetar mot samma mål. Våra elever ska gå vidare från Filbornaskolan med så mycket kunskap och goda erfarenheter som möjligt.',
                     'image' => 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
-                ),
+                )
+            )]
+        )
+        @endtestimonials
+
+        @testimonials(
+            ['perRow' => 1,
+            'testimonials' => array(
                 array(
-                    'name' => "Eric Rosenborg",
-                    'title' => 'Lärare i engelska och danska',
-                    'testimonial' => "For a time, at least, I was the most famous person in the entire world. Great work dudes!",
-                    'image' => 'https://cdn.vox-cdn.com/thumbor/I04LgBg2MHGcFb1F2Jc7pchQWbI=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22795858/1195397540.jpg',
-                ),
-                array(
-                    'name' => 'Johan Rehborg',
-                    'title' => 'Lärare i svenska som andraspråk',
-                    'testimonial' => "Everything will be okay in the end. If it’s not okay, it’s not the end. Love your stuff, Peace!",
+                    'name' => 'Mr Long Name Senior af Helsingborg',
+                    'title' => ' Matematik- och idrottslärare',
+                    'testimonial' => 'Det bästa med Filbornaskolan är bredden på programutbudet samt att all personal är engagerad och arbetar mot samma mål. Våra elever ska gå vidare från Filbornaskolan med så mycket kunskap och goda erfarenheter som möjligt.',
                     'image' => 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
-                ),
-                array(
-                    'name' => 'Johan Rehborg',
-                    'title' => 'Lärare i svenska som andraspråk',
-                    'testimonial' => "Everything will be okay in the end. If it’s not okay, it’s not the end. Love your stuff, Peace!",
-                    'image' => 'https://cdn.vox-cdn.com/thumbor/I04LgBg2MHGcFb1F2Jc7pchQWbI=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22795858/1195397540.jpg',
                 )
             )]
         )
