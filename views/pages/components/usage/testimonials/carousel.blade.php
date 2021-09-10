ad1c3ffa6d04814e72b5acdfecc00004fec8ee7e
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-09-10 13:32:51 +0200

    - add more examples in documentation

--- views/pages/components/usage/testimonials/carousel.blade.php
+++ views/pages/components/usage/testimonials/carousel.blade.php
@@ -52,3 +52,47 @@
     )
 ])
 @endtestimonials
+
+@testimonials([
+    'isCarousel' => true,
+    'testimonials' => array(
+        array(
+            'name' => 'David Conradi 1st',
+            'title' => ' Matematik- och idrottslärare',
+            'testimonial' => 'Det bästa med Filbornaskolan är bredden på programutbudet samt att all personal är engagerad och arbetar mot samma mål. Våra elever ska gå vidare från Filbornaskolan med så mycket kunskap och goda erfarenheter som möjligt.',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
+        ),
+        array(
+            'name' => 'David Conradi 2nd',
+            'title' => ' Matematik- och idrottslärare',
+            'testimonial' => '"Det är så himla roligt att undervisa ungdomar och att lära av dem. Elever som anstränger sig lyckas och att se glädjen i deras ögon när det sker, är det bästa som finns. De ögonblicken lever jag länge på."',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
+        ),
+        array(
+            'name' => 'David Conradi 3nd',
+            'title' => ' Matematik- och idrottslärare',
+            'testimonial' => '"Det är så himla roligt att undervisa ungdomar och att lära av dem. Elever som anstränger sig lyckas och att se glädjen i deras ögon när det sker, är det bästa som finns. De ögonblicken lever jag länge på."',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
+        )
+    )
+])
+@endtestimonials
+
+@testimonials([
+    'isCarousel' => true,
+    'testimonials' => array(
+        array(
+            'name' => 'David Conradi 1st',
+            'title' => ' Matematik- och idrottslärare',
+            'testimonial' => 'Det bästa med Filbornaskolan är bredden på programutbudet samt att all personal är engagerad och arbetar mot samma mål. Våra elever ska gå vidare från Filbornaskolan med så mycket kunskap och goda erfarenheter som möjligt.',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
+        ),
+        array(
+            'name' => 'David Conradi 2nd',
+            'title' => ' Matematik- och idrottslärare',
+            'testimonial' => '"Det är så himla roligt att undervisa ungdomar och att lära av dem. Elever som anstränger sig lyckas och att se glädjen i deras ögon när det sker, är det bästa som finns. De ögonblicken lever jag länge på."',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
+        )
+    )
+])
+@endtestimonials
