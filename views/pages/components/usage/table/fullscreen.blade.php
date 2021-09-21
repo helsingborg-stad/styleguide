4cb3cfbf8b6445b01370833c8c44cd1a1f059608
Eric Rosenborg <eric.rosenborg@hiq.se>
2021-09-21 13:51:26 +0200

    Add missing documentation, add more examples

---
+++ views/pages/components/usage/table/fullscreen.blade.php
@@ -0,0 +1,18 @@
+@table([
+    'title'         => "Title",
+    'headings'      => ['Heading', 'Heading', 'Heading', 'Heading', 'Heading'],   
+    'fullscreen'    => true, 
+    'list'          => [
+        ['columns' => ['Value', '1', 'svejsan', 'urlis', 'urlis']],
+        ['columns' => ['Value', '10', 'svejsan', 'urlis', 'urlis']],
+        ['columns' => ['Value', '10', 'svejsan', 'urlis', 'urlis']],
+        ['columns' => ['Value', '10', 'svejsan', 'urlis', 'urlis']],
+        ['columns' => ['Value', '10', 'svejsan', 'urlis', 'urlis']],
+        ['columns' => ['Value', '10', 'svejsan', 'urlis', 'urlis']],
+        ['columns' => ['Value', '10', 'svejsan', 'urlis', 'urlis']],
+        ['columns' => ['Value', '10', 'svejsan', 'urlis', 'urlis']],
+        ['columns' => ['Value', '10', 'svejsan', 'urlis', 'urlis']],
+
+    ],
+])
+@endtable
---
+++ views/pages/components/usage/table/multidimensional.blade.php
@@ -0,0 +1,20 @@
+@table([
+    'title'         => "Title",
+    'headings'      => ['Dim 1', 'Dim 1', 'Dim 1', 'Dim 1', 'Dim 1', 'Dim 1', 'Dim 1', 'Dim 1', 'Dim 1', 'Dim 1', 'Dim 1'],    
+    'isMultidimensional' => false,
+    'pagination' => 2,
+    'caption' => '* This is a table, it looks good, yes.',
+    'list'          => [
+        ['columns' => ['Another dimension 2', '1', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+        ['columns' => ['Another dimension 2', '10', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+        ['columns' => ['Another dimension 2', '10', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+        ['columns' => ['Another dimension 2', '10', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+        ['columns' => ['Another dimension 2', '10', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+        ['columns' => ['Another dimension 2', '10', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+        ['columns' => ['Another dimension 2', '10', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+        ['columns' => ['Another dimension 2', '10', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+        ['columns' => ['Another dimension 2', '10', 'svejsan', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis', 'urlis']],
+
+    ],
+])
+@endtable
