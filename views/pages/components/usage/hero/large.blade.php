b8ca4c6e9ccca13c02ba0a2ee133bbc20a59d14c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-09-22 15:08:22 +0200

    Add hero component

---
+++ views/pages/components/usage/hero/large.blade.php
@@ -0,0 +1,9 @@
+@hero([
+    "image" => "https://picsum.photos/1500/1000?image=1026",
+    "size" => "large",
+    "overlay" => "neutral",
+    "title" => "Donec ullamcorper nulla non metus auctor fringilla.",
+    "byline" => "This is a byline",
+    "paragraph" => "Aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam."
+])
+@endhero
