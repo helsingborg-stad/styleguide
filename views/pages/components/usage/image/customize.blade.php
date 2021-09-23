64ce7478734b4656fcc1753ccab74dd07cf4ef95
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-09-23 11:31:39 +0200

    - improve documentation of image

---
+++ views/pages/components/usage/image/customize.blade.php
@@ -0,0 +1,9 @@
+@image([
+    'src'                   => false,
+    'alt'                   => "This is a image",
+    'caption'               => "Hey, I am a caption for an image",
+    'placeholderText'       => 'Broken image source',
+    'placeholderIcon'       => 'block',
+    'placeholderIconSize'   => 'lg'
+])
+@endimage
