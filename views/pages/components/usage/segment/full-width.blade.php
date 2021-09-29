dbb7f2535ed21f58959f4c8823da89c0380bd191
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-09-29 11:51:25 +0200

    Add ability to align full width segment

--- views/pages/components/usage/segment/full-width.blade.php
+++ views/pages/components/usage/segment/full-width.blade.php
@@ -3,6 +3,7 @@
     'image'             => 'https://i.picsum.photos/id/342/2896/1944.jpg?hmac=_2cYDHi2iG1XY53gvXOrhrEWIP5R5OJlP7ySYYCA0QA',
     'background'        => 'primary',
     'textColor'         => 'light',
-    'overlay'           => 'dark'
+    'overlay'           => 'dark',
+    'textAlignment'     => 'center',
 ])
 @endsegment
