1440c8708c48677658340c7f2a8ebcc273a41a32
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-10-13 13:27:16 +0200

    More examples in documentation

--- views/pages/components/usage/button/toggle.blade.php
+++ views/pages/components/usage/button/toggle.blade.php
@@ -1,37 +1,20 @@
-@button([
-    'icon' => 'format_align_center',
-    'toggle' => true,
-    'size' => 'md'
+@typography([
+    "variant" => "h3"
 ])
-@endbutton
+    Basic buttons
+@endtypography
 
 @button([
-    'icon' => 'format_align_center',
+    'icon' => 'format_align_left',
     'toggle' => true,
     'size' => 'md',
     'color' => 'primary',
+    'style' => 'basic'
 ])
 @endbutton
 
 @button([
     'icon' => 'format_align_center',
-    'toggle' => true,
-    'size' => 'md',
-    'color' => 'secondary',
-])
-@endbutton
-
-@button([
-    'icon' => 'format_align_right',
-    'toggle' => true,
-    'size' => 'md',
-    'color' => 'secondary',
-    'style' => 'text'
-])
-@endbutton
-
-@button([
-    'icon' => 'format_align_right',
     'toggle' => true,
     'size' => 'md',
     'color' => 'secondary',
@@ -43,6 +26,29 @@
     'icon' => 'format_align_right',
     'toggle' => true,
     'size' => 'md',
+    'style' => 'basic'
+])
+@endbutton
+
+@typography([
+    "variant" => "h3"
+])
+    Filled buttons
+@endtypography
+
+@button([
+    'icon' => 'format_align_left',
+    'toggle' => true,
+    'size' => 'md',
+    'color' => 'primary',
+    'style' => 'filled'
+])
+@endbutton
+
+@button([
+    'icon' => 'format_align_center',
+    'toggle' => true,
+    'size' => 'md',
     'color' => 'secondary',
     'style' => 'filled'
 ])
@@ -52,7 +58,39 @@
     'icon' => 'format_align_right',
     'toggle' => true,
     'size' => 'md',
+    'style' => 'filled'
+])
+@endbutton
+
+
+@typography([
+    "variant" => "h3"
+])
+    Outline buttons
+@endtypography
+
+@button([
+    'icon' => 'format_align_left',
+    'toggle' => true,
+    'size' => 'md',
+    'color' => 'primary',
+    'style' => 'outlined'
+])
+@endbutton
+
+@button([
+    'icon' => 'format_align_center',
+    'toggle' => true,
+    'size' => 'md',
     'color' => 'secondary',
-    'style' => 'outline'
+    'style' => 'outlined'
+])
+@endbutton
+
+@button([
+    'icon' => 'format_align_right',
+    'toggle' => true,
+    'size' => 'md',
+    'style' => 'outlined'
 ])
 @endbutton
