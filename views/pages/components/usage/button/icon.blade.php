8e4d8d5e8d6788166f5e6ae0315707e4d5ae61e8
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2021-10-14 09:51:23 +0200

    Feature/3.0/buttoncolors (#424)
    * Update colors on buttons & fix default
    
    * Update colors & animations
    
    * Fix issues & change color on disabeld.
    
    * Fixes issue where some styling persisted on focus
    
    * More examples in documentation
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- views/pages/components/usage/button/icon.blade.php
+++ views/pages/components/usage/button/icon.blade.php
@@ -1,4 +1,79 @@
 @button([
+    'style' => 'basic',
+    'icon' => 'close',
+    'size' => 'md',
+    'color' => 'primary',
+])
+@endbutton
+
+@button([
+    'style' => 'filled',
+    'icon' => 'close',
+    'size' => 'md',
+    'color' => 'primary',
+])
+@endbutton
+
+@button([
+    'style' => 'outlined',
+    'icon' => 'close',
+    'size' => 'md',
+    'color' => 'primary',
+])
+@endbutton
+
+@button([
+    'style' => 'basic',
+    'icon' => 'close',
+    'size' => 'md',
+    'color' => 'secondary',
+])
+@endbutton
+
+@button([
+    'style' => 'filled',
+    'icon' => 'close',
+    'size' => 'md',
+    'color' => 'secondary',
+])
+@endbutton
+
+@button([
+    'style' => 'outlined',
+    'icon' => 'close',
+    'size' => 'md',
+    'color' => 'secondary',
+])
+@endbutton
+
+@button([
+    'style' => 'basic',
+    'icon' => 'close',
+    'size' => 'md'
+])
+@endbutton
+
+@button([
+    'style' => 'filled',
+    'icon' => 'close',
+    'size' => 'md'
+])
+@endbutton
+
+@button([
+    'style' => 'outlined',
+    'icon' => 'close',
+    'size' => 'md'
+])
+@endbutton
+
+@typography([
+    "variant" => "h3"
+])
+    Button with icon only - sizes
+@endtypography
+
+@button([
     'style' => 'filled',
     'icon' => 'close',
     'size' => 'sm'
@@ -18,3 +93,46 @@
     'size' => 'lg'
 ])
 @endbutton
+
+@button([
+    'style' => 'outlined',
+    'icon' => 'close',
+    'size' => 'sm'
+])
+@endbutton
+
+@button([
+    'style' => 'outlined',
+    'icon' => 'close',
+    'size' => 'md'
+])
+@endbutton
+
+@button([
+    'style' => 'outlined',
+    'icon' => 'close',
+    'size' => 'lg'
+])
+@endbutton
+
+@button([
+    'style' => 'basic',
+    'icon' => 'close',
+    'size' => 'sm'
+])
+@endbutton
+
+@button([
+    'style' => 'basic',
+    'icon' => 'close',
+    'size' => 'md'
+])
+@endbutton
+
+@button([
+    'style' => 'basic',
+    'icon' => 'close',
+    'size' => 'lg'
+])
+@endbutton
+
--- views/pages/components/usage/button/toggle.blade.php
+++ views/pages/components/usage/button/toggle.blade.php
@@ -1,6 +1,96 @@
+@typography([
+    "variant" => "h3"
+])
+    Basic buttons
+@endtypography
+
+@button([
+    'icon' => 'format_align_left',
+    'toggle' => true,
+    'size' => 'md',
+    'color' => 'primary',
+    'style' => 'basic'
+])
+@endbutton
+
 @button([
     'icon' => 'format_align_center',
     'toggle' => true,
-    'size' => 'md'
+    'size' => 'md',
+    'color' => 'secondary',
+    'style' => 'basic'
+])
+@endbutton
+
+@button([
+    'icon' => 'format_align_right',
+    'toggle' => true,
+    'size' => 'md',
+    'style' => 'basic'
+])
+@endbutton
+
+@typography([
+    "variant" => "h3"
+])
+    Filled buttons
+@endtypography
+
+@button([
+    'icon' => 'format_align_left',
+    'toggle' => true,
+    'size' => 'md',
+    'color' => 'primary',
+    'style' => 'filled'
+])
+@endbutton
+
+@button([
+    'icon' => 'format_align_center',
+    'toggle' => true,
+    'size' => 'md',
+    'color' => 'secondary',
+    'style' => 'filled'
+])
+@endbutton
+
+@button([
+    'icon' => 'format_align_right',
+    'toggle' => true,
+    'size' => 'md',
+    'style' => 'filled'
+])
+@endbutton
+
+
+@typography([
+    "variant" => "h3"
+])
+    Outline buttons
+@endtypography
+
+@button([
+    'icon' => 'format_align_left',
+    'toggle' => true,
+    'size' => 'md',
+    'color' => 'primary',
+    'style' => 'outlined'
+])
+@endbutton
+
+@button([
+    'icon' => 'format_align_center',
+    'toggle' => true,
+    'size' => 'md',
+    'color' => 'secondary',
+    'style' => 'outlined'
+])
+@endbutton
+
+@button([
+    'icon' => 'format_align_right',
+    'toggle' => true,
+    'size' => 'md',
+    'style' => 'outlined'
 ])
 @endbutton
