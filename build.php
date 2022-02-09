fb9b35a161d1a39afb781539da03f6522de49266
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-02-09 15:09:08 +0100

    feat: update npm packages, update material icons, refactor build scripts

--- build.php
+++ build.php
@@ -7,9 +7,9 @@ if (php_sapi_name() !== 'cli') {
 
 // Any command needed to run and build plugin assets when newly cheched out of repo.
 $buildCommands = [
-    'npm install --no-progress',
+    'npm ci --no-progress --no-audit',
     'npm run build',
-    'composer install --prefer-dist --no-progress --no-suggest'
+    'composer install --prefer-dist --no-progress'
 ];
 
 // Files and directories not suitable for prod to be removed.
