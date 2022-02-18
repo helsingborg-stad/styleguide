aabf87a14969888adaa943f1ed1976d994370c5b
Freya <752642+faejr@users.noreply.github.com>
2022-02-18 15:22:00 +0100

    feat: setup base block

---
+++ views/pages/components/organisms/block.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.containers.doc')
+
+@section('doc-content')
+    @markdown
+    #Blocks
+
+    Blocks are surfaces that display content and actions on a single topic. <br />
+
+    They should be easy to scan for relevant and actionable information. Elements, like text and images, <br />
+    should be placed on them in a way that clearly indicates hierarchy.
+    @endmarkdown
+
+
+    @doc(['slug' => 'block'])
+    @enddoc
+@stop
