63d740279e57e9bea0df39a1f8d37d11d2c6559b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-02-22 15:47:58 +0100

    Add new ”objects” page in styleguide

---
+++ views/pages/objects/grid.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.containers.doc')
+
+@section('doc-content')
+    @markdown
+        #Grid
+        Grid is a object that allows you to create a grid of elements. Please note that we support, .grid and .o-grid syntax. We are moving away from .grid class to the better scoped .o-grid naming. 
+
+    @endmarkdown
+@stop
