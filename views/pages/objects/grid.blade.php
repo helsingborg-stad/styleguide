4f57f04911786d5dcd55004f561de599133ac470
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-02-22 16:14:41 +0100

    Adds a very basic version of grid docs

--- views/pages/objects/grid.blade.php
+++ views/pages/objects/grid.blade.php
@@ -3,7 +3,35 @@
 @section('doc-content')
     @markdown
         #Grid
-        Grid is a object that allows you to create a grid of elements. Please note that we support, .grid and .o-grid syntax. We are moving away from .grid class to the better scoped .o-grid naming. 
-
+        Grid is a object that allows you to create a grid of elements. Please note that we support, .grid and .o-grid syntax. We are moving away from .grid class to the better scoped .o-grid. 
     @endmarkdown
+
+    @paper(['padding' => '3'])
+      @typography(['variant' => 'h3', 'element' => 'h3'])
+        Regular grid
+      @endtypography
+
+      <div class="d-code__toggle c-code__toggle">
+        @button([
+            'text' => 'HTML',
+            'color' => 'default',
+            'type' => 'basic',
+            'size' => 'md',
+            'icon' => 'code'
+        ])
+        @endbutton
+      </div>
+
+      @code(['language' => 'html', 'content' => "", 'classList' => ['d-code']])
+        {{\HbgStyleGuide\Helper\ParseString::tidyHtml('
+          <div class="o-grid">
+              <div class="o-grid-12@sm o-grid-6@md o-grid-4@lg">Column</div>
+              <div class="o-grid-12@sm o-grid-6@md o-grid-4@lg">Column</div>
+              <div class="o-grid-12@sm o-grid-6@md o-grid-4@lg">Column</div>
+              <div class="o-grid-12@sm o-grid-6@md o-grid-4@lg">Column</div>
+          </div>
+        ')}}
+      @endcode
+
+    @endpaper
 @stop
