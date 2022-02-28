63fb4bbdc2d21d71803092d3ab24dc3b3de64595
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-02-28 11:08:13 +0100

    Add view for gutterless grids.

---
+++ views/pages/components/usage/block/gutter.blade.php
@@ -0,0 +1,67 @@
+<div class="o-grid o-grid--no-gutter">
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+</div>
