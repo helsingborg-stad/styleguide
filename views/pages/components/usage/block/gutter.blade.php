30c80ea6cf0a9ee5be299bfed741f49d429ab56b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-02-28 13:21:30 +0100

    Add lorem ipsum to square and nogutter square

--- views/pages/components/usage/block/gutter.blade.php
+++ views/pages/components/usage/block/gutter.blade.php
@@ -1,8 +1,8 @@
 <div class="o-grid o-grid--no-gutter">
     <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
         @block([
-            'heading' => 'Heading',
-            'meta' => 'Meta',
+            'heading' => 'Purus Tellus',
+            'meta' => 'Parturient Ultricies',
             'filled' => true,
             'image' => [
                 'src' => 'https://picsum.photos/680/510?image=88',
@@ -15,8 +15,8 @@
     </div>
     <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
         @block([
-            'heading' => 'Heading',
-            'meta' => 'Meta',
+            'heading' => 'Donec id elit non mi porta gravida at eget metus',
+            'meta' => 'Nibh Sit',
             'filled' => true,
             'image' => [
                 'src' => 'https://picsum.photos/680/510?image=87',
@@ -28,8 +28,8 @@
     </div>
     <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
         @block([
-            'heading' => 'Heading',
-            'meta' => 'Meta',
+            'heading' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'meta' => 'Commodo Fringilla',
             'filled' => true,
             'image' => [
                 'src' => 'https://picsum.photos/680/510?image=32',
@@ -41,8 +41,8 @@
     </div>
     <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
         @block([
-            'heading' => 'Heading',
-            'meta' => 'Meta',
+            'heading' => 'Maecenas sed diam eget risus varius blandit sit amet non magna',
+            'meta' => 'Ullamcorper Euismod Pharetra',
             'filled' => true,
             'image' => [
                 'src' => 'https://picsum.photos/680/510?image=33',
@@ -54,8 +54,8 @@
     </div>
     <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
         @block([
-            'heading' => 'Heading',
-            'meta' => 'Meta',
+            'heading' => 'Pharetra Etiam',
+            'meta' => 'Amet',
             'filled' => true,
             'image' => [
                 'src' => 'https://picsum.photos/680/510?image=34',
