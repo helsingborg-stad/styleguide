22acc6a5471afd3d0c5a3b67deed56d3c0acfc59
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-02-28 10:52:34 +0100

    Create more examples

---
+++ views/pages/components/usage/block/tall.blade.php
@@ -0,0 +1,72 @@
+<div class="o-grid">
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'ratio' => '12:16',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'ratio' => '12:16',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'ratio' => '12:16',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'ratio' => '12:16',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-6@sm o-grid-3@md">
+        @block([
+            'heading' => 'Heading',
+            'ratio' => '12:16',
+            'meta' => 'Meta',
+            'filled' => true,
+            'image' => [
+                'src' => '/assets/img/1038-900x600.jpg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+</div>
