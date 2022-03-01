49711f1dba98009ef849f7c8024fccff0ba2a53c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-01 15:17:51 +0100

    Adds a example without images in styleguide

---
+++ views/pages/components/usage/block/noimg.blade.php
@@ -0,0 +1,48 @@
+<div class="o-grid">
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Nibh Dolor Bibendum Vehicula Amet',
+            'ratio' => '12:16',
+            'meta' => 'Ultricies',
+            'filled' => true,
+            'link' => '#',
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Nulla vitae elit libero, a pharetra augue',
+            'ratio' => '12:16',
+            'meta' => 'Malesuada Parturient Dolor',
+            'filled' => true
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
+            'ratio' => '12:16',
+            'meta' => 'Porta Ultricies',
+            'filled' => true
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Maecenas faucibus mollis interdum',
+            'ratio' => '12:16',
+            'meta' => 'Nibh',
+            'filled' => true
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Curabitur blandit tempus porttitor',
+            'ratio' => '12:16',
+            'meta' => 'Ornare Malesuada',
+            'filled' => false
+        ])
+        @endblock
+    </div>
+</div>
