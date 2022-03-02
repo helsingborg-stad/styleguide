f5a62400b54ec0212f6bb9db7c5a572d895f2cc2
Freya <752642+faejr@users.noreply.github.com>
2022-03-02 10:14:25 +0100

    Feature/block component (#459)
    * feat: setup base block
    
    * initial block styling
    
    * fix: use base value
    
    * fix: rename section
    
    * fix: border radius in municipio
    
    * Remove gid component from blocks documentation
    
    * Create more examples
    
    * Add view for gutterless grids.
    
    * Add linked blocks
    
    * Display as block, to fix linked cards
    
    * fix: text overlay gradient
    
    * Add hover animation to linked block
    
    * Remove linked block
    
    * fix: More appropriate grid sizes.
    
    * fix: 1:1 -> 4:3
    
    * fix: border radius when filled
    
    * Make docs more dynamic with alternating images
    
    * Fix grid sizes, add lorem ipsum to tall examples
    
    * Add lorem ipsum to square and nogutter square
    
    * Add more padding to top of body, in order to align gradient to content better.
    
    * Handle overflowing content
    
    * Gutterless, fix radius issue on :after elements
    
    * feat: setup base block
    
    * initial block styling
    
    * fix: use base value
    
    * fix: rename section
    
    * Remove gid component from blocks documentation
    
    * fix: border radius in municipio
    
    * Create more examples
    
    * Add view for gutterless grids.
    
    * Add linked blocks
    
    * Display as block, to fix linked cards
    
    * Add hover animation to linked block
    
    * fix: text overlay gradient
    
    * Remove linked block
    
    * fix: More appropriate grid sizes.
    
    * fix: 1:1 -> 4:3
    
    * fix: border radius when filled
    
    * Make docs more dynamic with alternating images
    
    * Fix grid sizes, add lorem ipsum to tall examples
    
    * Add lorem ipsum to square and nogutter square
    
    * Add more padding to top of body, in order to align gradient to content better.
    
    * Handle overflowing content
    
    * Gutterless, fix radius issue on :after elements
    
    * Adds a example without images in styleguide
    
    * Example with array of meta’s
    
    * Simplify styling
    
    * Add icons json to ignore
    
    * Remove build of icons.json
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

---
+++ views/pages/components/usage/block/gutter.blade.php
@@ -0,0 +1,68 @@
+<div class="o-grid o-grid--no-gutter">
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Purus Tellus',
+            'meta' => 'Parturient Ultricies',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/510?image=88',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ],
+            'link' => '#',
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Donec id elit non mi porta gravida at eget metus',
+            'meta' => 'Nibh Sit',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/510?image=87',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'meta' => 'Commodo Fringilla',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/510?image=32',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Maecenas sed diam eget risus varius blandit sit amet non magna',
+            'meta' => 'Ullamcorper Euismod Pharetra',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/510?image=33',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Pharetra Etiam',
+            'meta' => 'Amet',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/510?image=34',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+</div>
---
+++ views/pages/components/usage/block/tall.blade.php
@@ -0,0 +1,73 @@
+<div class="o-grid">
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Nibh Dolor Bibendum Vehicula Amet',
+            'ratio' => '12:16',
+            'meta' => 'Ultricies',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/1000?image=15',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ],
+            'link' => '#',
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Nulla vitae elit libero, a pharetra augue',
+            'ratio' => '12:16',
+            'meta' => 'Malesuada Parturient Dolor',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/1000?image=16',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
+            'ratio' => '12:16',
+            'meta' => 'Porta Ultricies',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/1000?image=17',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Maecenas faucibus mollis interdum',
+            'ratio' => '12:16',
+            'meta' => 'Nibh',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/1000?image=18',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg">
+        @block([
+            'heading' => 'Curabitur blandit tempus porttitor',
+            'ratio' => '12:16',
+            'meta' => 'Ornare Malesuada',
+            'filled' => true,
+            'image' => [
+                'src' => 'https://picsum.photos/680/1000?image=19',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+            ]
+        ])
+        @endblock
+    </div>
+</div>
