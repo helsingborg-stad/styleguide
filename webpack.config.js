b228e6d7f2c1609e71a28848826be8e984f68921
Freya <752642+faejr@users.noreply.github.com>
2022-03-11 16:55:55 +0100

    feat: build sass using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const path = require('path');
 const glob = require('glob');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScripts = require('webpack-remove-empty-scripts');
 
 module.exports = {
-
     /**
      * Entry files - Add more entries if needed.
      */
     entry: {
+        'styleguide-css': ['./source/sass/main.scss'],
         'styleguide-js': glob.sync('./source/js/**/*.js'),
-        'buildcss': './build/index.js'
+        buildcss: './build/index.js',
     },
     mode: 'production',
 
+    //assets/dist/css/styleguide-css.min.css
     watchOptions: {
         poll: 100,
         ignored: /node_modules/,
@@ -45,12 +49,12 @@ module.exports = {
                                 {
                                     useBuiltIns: 'entry',
                                     corejs: 3,
-                                }
-                            ]
+                                },
+                            ],
                         ],
                         plugins: [
-                            ['@babel/plugin-transform-runtime', { corejs: 3, regenerator: true }]
-                        ]
+                            ['@babel/plugin-transform-runtime', { corejs: 3, regenerator: true }],
+                        ],
                     },
                 },
             },
@@ -69,12 +73,56 @@ module.exports = {
                     },
                 ],
             },
+
+            /**
+             * Styles
+             */
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                            url: false,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [autoprefixer, require('postcss-object-fit-images')],
+                            },
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {},
+                    },
+                ],
+            },
         ],
     },
 
     /**
      * Plugins
      */
-    plugins: [],
+    plugins: [
+        /**
+         * Fix CSS entry chunks generating js file
+         */
+         new RemoveEmptyScripts(),
+
+        /**
+         * Output CSS files
+         */
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].min.css',
+        }),
+    ],
     // ...
 };
