095294b21750494869ebc0f54c2d35038b62b328
Freya <752642+faejr@users.noreply.github.com>
2022-06-30 15:41:09 +0200

    fix: update design (#509)
    * fix: update design
    
    * fix typo
    
    * fix: make featured stand out more

--- views/pages/components/usage/product/product.blade.php
+++ views/pages/components/usage/product/product.blade.php
@@ -10,20 +10,20 @@
                 'padded' => true
             ],
             'prices' => [
-                ['amount' => '249', 'currency' => 'kr', 'frequency' => 'mon'],
-                ['amount' => '399', 'currency' => 'kr', 'frequency' => 'yr', 'backgroundColor' => 'secondary']
+                ['amount' => '249', 'currency' => ':-', 'frequency' => 'mon']
             ],
             'bulletPoints' => [
                 ['label' => 'Lorem'],
                 ['label' => 'Ipsum']
             ],
-            'button' => ['type' => 'filled', 'color' => 'primary', 'text' => 'Buy', 'href' => '#']
+            'button' => ['type' => 'filled', 'text' => 'Buy', 'href' => '#']
         ])
         @endproduct
     </div>
     <div class="o-grid-4@md">
         @product([
             'heading' => 'Heading 2',
+            'backgroundColor' => 'secondary',
             'label' => 'Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components.',
             'image' => [
                 'src' => '/assets/img/component.svg',
@@ -33,14 +33,36 @@
             ],
             'meta' => 'Everything in heading 1 is included',
             'prices' => [
-                ['amount' => '349', 'currency' => 'kr', 'frequency' => 'mon'],
-                ['amount' => '499', 'currency' => 'kr', 'frequency' => 'yr']
+                ['amount' => '349', 'currency' => ':-', 'frequency' => 'mon']
             ],
             'bulletPoints' => [
-                ['label' => 'Lorem'],
+                ['label' => 'Lorem', 'href' => '#'],
                 ['label' => 'Ipsum']
             ],
-            'button' => ['type' => 'filled', 'color' => 'primary', 'text' => 'Buy', 'href' => '#']
+            'button' => ['type' => 'filled', 'text' => 'Buy', 'href' => '#']
+        ])
+        @endproduct
+    </div>
+     <div class="o-grid-4@md">
+        @product([
+            'heading' => 'Heading 3',
+            'backgroundColor' => 'custom',
+            'label' => 'Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components.',
+            'image' => [
+                'src' => '/assets/img/component.svg',
+                'alt' => 'ALT', 
+                'backgroundColor' => 'secondary',
+                'padded' => true
+            ],
+            'meta' => 'Everything in heading 2 is included',
+            'prices' => [
+                ['amount' => '349', 'currency' => ':-', 'frequency' => 'mon']
+            ],
+            'bulletPoints' => [
+                ['label' => 'Lorem', 'href' => '#'],
+                ['label' => 'Ipsum']
+            ],
+            'button' => ['type' => 'filled', 'text' => 'Buy', 'href' => '#']
         ])
         @endproduct
     </div>
