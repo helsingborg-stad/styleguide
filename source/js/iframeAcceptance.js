bbd05b56cccbacbf9a4994f5ddd048b755236de9
niclas.norin <niclas.norin@klingit.team>
2022-08-18 09:27:39 +0200

    removed logs and added localstorage again

--- source/js/iframeAcceptance.js
+++ source/js/iframeAcceptance.js
@@ -6,13 +6,13 @@ class IframeAcceptance {
             buttonText: 'Button Text'
         };
 
-        localStorage.clear();
         const accepted = localStorage.getItem('iframeAccepted') === 'accepted';
 
         accepted ? revealIframes() : suppressIframes();
 
         function markup(width, height, title, content, buttonText) {
-            return `<div data-iframe-container style="height: ${height}; width:100%">
+            return `
+            <div data-iframe-container style="height: ${height}; width:100%">
                 <div class="u-level-top u-position--absolute u-align-middle u-padding__x--3 u-padding__y--3 u-flex-direction--column u-overflow--auto" data-suppressed-iframe style="width:100%;height:${height};backdrop-filter:blur(30px);display:flex;">
                     <h4 class="c-typography c-typography__variant--h2">Informationen i den här rutan hämtas från en extern leverantör</h4>
                     <p class="c-typography u-padding__bottom--4 c-typography__variant--p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In libero metus, bibendum id dui non, sollicitudin venenatis orci. In venenatis mi mattis, consectetur ipsum sit amet, porta orci.</p> 
@@ -28,15 +28,13 @@ class IframeAcceptance {
         }
 
         function revealIframes() {
-            console.log("reveal");
             document.querySelectorAll('[data-suppressed-iframe]').forEach(item => {
                 item.classList.add('u-display--none');
             });
         }
         
         function onClicklHandler() {
-            console.log("click");
-            //localStorage.setItem('iframeAccepted', 'accepted');
+            localStorage.setItem('iframeAccepted', 'accepted');
             revealIframes();
         }
         
