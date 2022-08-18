403c5258613cf78ec578cad93d66c6176b7cae56
niclas.norin <niclas.norin@klingit.team>
2022-08-18 09:33:03 +0200

    Done

--- source/js/iframeAcceptance.js
+++ source/js/iframeAcceptance.js
@@ -11,20 +11,19 @@ class IframeAcceptance {
         accepted ? revealIframes() : suppressIframes();
 
         function markup(width, height, title, content, buttonText) {
-            return `
-            <div data-iframe-container style="height: ${height}; width:100%">
-                <div class="u-level-top u-position--absolute u-align-middle u-padding__x--3 u-padding__y--3 u-flex-direction--column u-overflow--auto" data-suppressed-iframe style="width:100%;height:${height};backdrop-filter:blur(30px);display:flex;">
-                    <h4 class="c-typography c-typography__variant--h2">Informationen i den här rutan hämtas från en extern leverantör</h4>
-                    <p class="c-typography u-padding__bottom--4 c-typography__variant--p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In libero metus, bibendum id dui non, sollicitudin venenatis orci. In venenatis mi mattis, consectetur ipsum sit amet, porta orci.</p> 
-                    <button class="c-button c-button__filled c-button__filled--primary c-button--md" target="_top" type="button" aria-pressed="false" style="height:0;padding: calc(var(--base, 8px) * 3) calc(var(--base, 8px) * 4)" js-suppressed-iframe-button>   
-                        <span class="c-button__label">
-                            <span class="c-button__label-text">
-                               Visa informationen
-                            </span>
-                        </span>
-                    </button>  
-                </div>
-            </div>`
+            return `<div data-iframe-container style="height: ${height}; width:100%">
+                        <div class="u-level-top u-position--absolute u-align-middle u-padding__x--3 u-padding__y--3 u-flex-direction--column u-overflow--auto" data-suppressed-iframe style="width:100%;height:${height};backdrop-filter:blur(30px);display:flex;">
+                            <h4 class="c-typography c-typography__variant--h2">Informationen i den här rutan hämtas från en extern leverantör</h4>
+                            <p class="c-typography u-padding__bottom--4 c-typography__variant--p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In libero metus, bibendum id dui non, sollicitudin venenatis orci. In venenatis mi mattis, consectetur ipsum sit amet, porta orci.</p> 
+                            <button class="c-button c-button__filled c-button__filled--primary c-button--md" target="_top" type="button" aria-pressed="false" style="height:0;padding: calc(var(--base, 8px) * 3) calc(var(--base, 8px) * 4)" js-suppressed-iframe-button>   
+                                <span class="c-button__label">
+                                    <span class="c-button__label-text">
+                                        Visa informationen
+                                    </span>
+                                </span>
+                            </button>  
+                        </div>
+                    </div>`
         }
 
         function revealIframes() {
