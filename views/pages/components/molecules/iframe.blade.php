6ecb5d18fe9cbf051146c36e21e691dd96fd91c4
Anna <anna@thehappyhipster.se>
2022-09-14 06:00:33 +0200

    iframe component usage documentation

---
+++ views/pages/components/molecules/iframe.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.containers.doc')
+
+@section('doc-content')
+    @markdown
+        #Iframe
+        The iframe HTML element is used for embedding another HTML page into the current one.
+		
+        
+    @endmarkdown
+
+    @doc(['slug' => 'iframe'])
+    @enddoc
+@stop
+
