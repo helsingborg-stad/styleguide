310bbc2b0cf0c9e0f25283fab84f2d75528ef6e9
NiclasNorin <niclas.norin@frontwalker.se>
2022-09-23 15:08:15 +0200

    Simplify n labels

--- source/js/iframeAcceptance.js
+++ source/js/iframeAcceptance.js
@@ -1,14 +1,14 @@
 let acceptedSuppliers = JSON.parse(localStorage.getItem('acceptedSuppliers')) ?? [];
 
-const template = ({ titleText, infoText, buttonText }) => (`
+const template = ({ title, info, button }) => (`
     <div class="js-suppressed-iframe-wrapper" style="position:relative;">
         <div class="js-suppressed-iframe-prompt" style="position:absolute; left:0; top:0; width:100%; height:100%; z-index:1; background-color: white; display: flex; align-items: center;">
             <div style="max-width: 600px; width: 100%; margin: auto; padding: 0 24px;">
-            <h4 class="c-typography c-typography__variant--h2">${titleText}</h4>
-            <p class="c-typography u-padding__bottom--4 c-typography__variant--p">${infoText}</p> 
+            <h4 class="c-typography c-typography__variant--h2">${title}</h4>
+            <p class="c-typography u-padding__bottom--4 c-typography__variant--p">${info}</p> 
                 <button class="js-suppressed-iframe-button c-button c-button__filled c-button__filled--primary c-button--md" target="_top" type="button" aria-pressed="false" style="">   
                     <span class="c-button__label">
-                        <span class="c-button__label-text">${buttonText}</span>
+                        <span class="c-button__label-text">${button}</span>
                     </span>
                 </button>  
             </div>
@@ -44,30 +44,29 @@ const suppressIframes = () => {
     [...document.querySelectorAll('.js-suppressed-iframe')]
     .forEach(iframe => {
 
-            const dataAttribute = JSON.parse(iframe.getAttribute('options') ?? '{}');
+            let lang = JSON.parse(iframe.getAttribute('options') ?? '{}');
             
             const option = () => {
-                console.log("körs");
-                if (iframe.getAttribute('data-supplier') && iframe.getAttribute('data-policy')) {
-                   let options = dataAttribute.knownSupplierLabels;
-                   console.log(options);
-                    const supplier = options.infoText.replace('{SUPPLIER_WEBSITE}', iframe.getAttribute('data-supplier')).replace('{SUPPLIER_POLICY}', iframe.getAttribute('data-policy'));
-                    options.infoText = supplier;
-                    return options;
-                } else {
-                    return dataAttribute.unknownSupplierLabels;
+                if (iframe.getAttribute('data-supplier-name') && iframe.getAttribute('data-supplier-policy')) {
+
+                    lang.knownLabels.info = lang.knownLabels.info.replace('{SUPPLIER_WEBSITE}', iframe.getAttribute('data-supplier-name')).replace('{SUPPLIER_POLICY}', iframe.getAttribute('data-supplier-policy'));
+
+                    return lang.knownLabels;
+
+                } else { 
+                    return lang.unknownLabels;
                 }
             }
 
-            const {titleText, infoText, buttonText } = option(); 
+            const {title, info, button } = option(); 
             const div = document.createElement('div');
-            div.insertAdjacentHTML('beforeend', template({ titleText, infoText, buttonText }));
+            div.insertAdjacentHTML('beforeend', template({ title, info, button } ));
             const wrapper = div.querySelector("*");
-            const button = wrapper.querySelector('.js-suppressed-iframe-button');
+            const buttonEl = wrapper.querySelector('.js-suppressed-iframe-button');
             iframe.parentNode.insertBefore(wrapper, iframe);
             wrapper.appendChild(iframe);
-            button.params = {iframe: iframe};
-            button.addEventListener('click', () => {
+            buttonEl.params = {iframe: iframe};
+            buttonEl.addEventListener('click', () => {
                 onClicklHandler(iframe);
             });
             div.remove();
