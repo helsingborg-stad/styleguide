702bf6f11aadf909257e179483bd6427b22f3e74
NiclasNorin <niclas.norin@frontwalker.se>
2022-09-30 12:04:45 +0200

    Optimization small fixes

--- source/js/iframeAcceptance.js
+++ source/js/iframeAcceptance.js
@@ -17,23 +17,25 @@ const template = ({ title, info, button }) => (`
 `)
 
 const revealIframes = () => {
-    [...document.querySelectorAll('.js-suppressed-iframe-prompt')]
+    [...document.querySelectorAll('.js-suppressed-iframe-wrapper')]
         .forEach(item => {
-            const iframe = item.nextElementSibling;
+            const iframe = item.querySelector('iframe');
             const iframeUrl = new URL(iframe.getAttribute('data-src'));
             if(acceptedSuppliers.includes(iframeUrl.host)) {
                 iframe.setAttribute('src', iframe.getAttribute('data-src'));
-                item.classList.add('u-display--none');
+                item.classList.remove('js-suppressed-iframe-wrapper');
+                item.style.position = 'static';
+                item.querySelector('.js-suppressed-iframe-prompt').classList.add('u-display--none');
             }
         }); 
 }
 
 const onClicklHandler = (iframe) => {
     const iframeUrl = new URL(iframe.getAttribute('data-src'));
-
     if (!acceptedSuppliers.includes(iframeUrl.host) && iframeUrl.host !== "https" && iframeUrl.host !== "http") {
         acceptedSuppliers.push(iframeUrl.host);
-    }
+    } 
+    
     localStorage.setItem('acceptedSuppliers', JSON.stringify(acceptedSuppliers));
     
     revealIframes();
@@ -43,11 +45,10 @@ const suppressIframes = () => {
   
     [...document.querySelectorAll('.js-suppressed-iframe')]
     .forEach(iframe => {
-        
             let lang = JSON.parse(iframe.getAttribute('options') ?? '{}');
+
             const option = () => {
                 if (iframe.getAttribute('data-supplier-name') && iframe.getAttribute('data-supplier-policy')) {
-
                     lang.knownLabels.info = lang.knownLabels.info.replace('{SUPPLIER_WEBSITE}', iframe.getAttribute('data-supplier-name')).replace('{SUPPLIER_POLICY}', iframe.getAttribute('data-supplier-policy'));
 
                     return lang.knownLabels;
@@ -56,7 +57,6 @@ const suppressIframes = () => {
                     return lang.unknownLabels;
                 }
             }
-
             const {title, info, button } = option(); 
             const div = document.createElement('div');
             div.insertAdjacentHTML('beforeend', template({ title, info, button } ));
@@ -78,7 +78,7 @@ export default () => addEventListener('DOMContentLoaded', () => {
             const iframeUrl = new URL(iframe.getAttribute('data-src'));
             if (acceptedSuppliers.includes(iframeUrl.host)) {
                 iframe.setAttribute('src', iframe.getAttribute('data-src'));
-                iframe.classList.remove('js-suppressed-iframe');
+                iframe.classList.remove('js-suppressed-iframe'); 
             }
         })
     }
