5bb2aaaf76c2c6cc143a8590bf9382c1bfaea3bb
NiclasNorin <niclas.norin@frontwalker.se>
2022-10-04 16:56:54 +0200

    Moved markup

--- source/js/iframeAcceptance.js
+++ source/js/iframeAcceptance.js
@@ -1,19 +1,7 @@
 let acceptedSuppliers = JSON.parse(localStorage.getItem('acceptedSuppliers')) ?? [];
 
 const template = ({ title, info, button }) => (`
-    <div class="js-suppressed-iframe-wrapper">
-        <div class="js-suppressed-iframe-prompt" style="position:absolute; left:0; top:0; width:100%; height:100%; z-index:1; display: flex; align-items: center; overflow:auto;">
-            <div style="max-width: 600px; width: 100%; margin: auto; padding: 0 24px;">
-            <h4 class="c-typography c-typography__variant--h2">${title}</h4>
-            <p class="c-typography u-padding__bottom--4 c-typography__variant--p">${info}</p> 
-                <button class="js-suppressed-iframe-button c-button c-button__filled c-button__filled--primary c-button--md" target="_top" type="button" aria-pressed="false" style="">   
-                    <span class="c-button__label">
-                        <span class="c-button__label-text">${button}</span>
-                    </span>
-                </button>  
-            </div>
-        </div>
-    </div>
+   
 `)
 
 const revealIframes = () => {
@@ -31,7 +19,7 @@ const revealIframes = () => {
 }
 
 const onClicklHandler = (iframe) => {
-    const iframeUrl = new URL(iframe.getAttribute('data-src'));
+    const iframeUrl = new URL(iframe.querySelector('iframe').getAttribute('data-src'));
     if (!acceptedSuppliers.includes(iframeUrl.host) && iframeUrl.host !== "https" && iframeUrl.host !== "http") {
         acceptedSuppliers.push(iframeUrl.host);
     } 
@@ -43,9 +31,10 @@ const onClicklHandler = (iframe) => {
 
 const suppressIframes = () => {
   
-    [...document.querySelectorAll('.js-suppressed-iframe')]
+    [...document.querySelectorAll('.js-suppressed-iframe-wrapper')]
     .forEach(iframe => {
             let lang = JSON.parse(iframe.getAttribute('options') ?? '{}');
+            console.log(iframe);
 
             const option = () => {
                 if (iframe.getAttribute('data-supplier-name') && iframe.getAttribute('data-supplier-policy')) {
@@ -57,28 +46,25 @@ const suppressIframes = () => {
                     return lang.unknownLabels;
                 }
             }
-            const {title, info, button } = option(); 
-            const div = document.createElement('div');
-            div.insertAdjacentHTML('beforeend', template({ title, info, button } ));
-            const wrapper = div.querySelector("*");
-            const buttonEl = wrapper.querySelector('.js-suppressed-iframe-button');
-            iframe.parentNode.insertBefore(wrapper, iframe);
-            wrapper.appendChild(iframe);
+ 
+            console.log(iframe.querySelector('[js-suppressed-iframe-button]'));
+            const buttonEl = iframe.querySelector('[js-suppressed-iframe-button]');
             buttonEl.params = {iframe: iframe};
             buttonEl.addEventListener('click', () => {
                 onClicklHandler(iframe);
             });
-            div.remove();
+           
         });
 }
 
 export default () => addEventListener('DOMContentLoaded', () => {
     if(acceptedSuppliers.length > 0) {
-        [...document.querySelectorAll('.js-suppressed-iframe')].forEach(iframe => {
-            const iframeUrl = new URL(iframe.getAttribute('data-src'));
+        [...document.querySelectorAll('.js-suppressed-iframe-wrapper')].forEach(iframe => {
+            const iframeUrl = new URL(iframe.querySelector('iframe').getAttribute('data-src'));
             if (acceptedSuppliers.includes(iframeUrl.host)) {
-                iframe.setAttribute('src', iframe.getAttribute('data-src'));
-                iframe.classList.remove('js-suppressed-iframe'); 
+                iframe.querySelector('iframe').setAttribute('src', iframe.querySelector('iframe').getAttribute('data-src'));
+                iframe.querySelector('.js-suppressed-iframe-prompt').style.display = 'none';
+                iframe.classList.remove('js-suppressed-iframe-wrapper'); 
             }
         })
     }
