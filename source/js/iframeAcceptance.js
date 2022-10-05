71a7f8c4ea3fb6b204a934834ba77639570f65e4
NiclasNorin <niclas.norin@frontwalker.se>
2022-10-05 16:04:33 +0200

    Cleanup

--- source/js/iframeAcceptance.js
+++ source/js/iframeAcceptance.js
@@ -29,27 +29,6 @@ const suppressIframes = () => {
   
     [...document.querySelectorAll('.js-suppressed-iframe-wrapper')]
     .forEach(iframeWrapper => {
-            const iframe = iframeWrapper.querySelector('iframe');
-            let lang = JSON.parse(iframe.getAttribute('options') ?? '{}');
-            //console.log(iframe);
-
-        if (iframe.getAttribute('data-supplier-name') && iframe.getAttribute('data-supplier-policy')) {
-            console.log(iframe);
-            console.log(lang);
-            console.log(iframeWrapper.querySelector('h3'));
-        }
-
-            const option = () => {
-                if (iframeWrapper.getAttribute('data-supplier-name') && iframe.getAttribute('data-supplier-policy')) {
-                    lang.knownLabels.info = lang.knownLabels.info.replace('{SUPPLIER_WEBSITE}', iframe.getAttribute('data-supplier-name')).replace('{SUPPLIER_POLICY}', iframeWrapper.getAttribute('data-supplier-policy'));
-
-                    return lang.knownLabels;
-
-                } else { 
-                    return lang.unknownLabels;
-                }
-            }
-
             const buttonEl = iframeWrapper.querySelector('[js-suppressed-iframe-button]');
             buttonEl.params = {iframe: iframeWrapper};
             buttonEl.addEventListener('click', () => {
@@ -62,9 +41,10 @@ const suppressIframes = () => {
 export default () => addEventListener('DOMContentLoaded', () => {
     if(acceptedSuppliers.length > 0) {
         [...document.querySelectorAll('.js-suppressed-iframe-wrapper')].forEach(iframeWrapper => {
-            const iframeUrl = new URL(iframeWrapper.querySelector('iframe').getAttribute('data-src'));
+            const iframe = iframeWrapper.querySelector('iframe');
+            const iframeUrl = new URL(iframe.getAttribute('data-src'));
             if (acceptedSuppliers.includes(iframeUrl.host)) {
-                iframeWrapper.querySelector('iframe').setAttribute('src', iframeWrapper.querySelector('iframe').getAttribute('data-src'));
+                iframe.setAttribute('src', iframeWrapper.querySelector('iframe').getAttribute('data-src'));
                 iframeWrapper.querySelector('.js-suppressed-iframe-prompt').style.display = 'none';
                 iframeWrapper.classList.remove('js-suppressed-iframe-wrapper'); 
             }
