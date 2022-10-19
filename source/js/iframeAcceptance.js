46bbec41b87e97ba528b3541e58669d4f73e1e06
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2022-10-19 14:18:08 +0200

    Feature/iframe video ratio (#566)
    * Initial commit video ratio iframe
    
    * Cleanup
    
    * Removed log

--- source/js/iframeAcceptance.js
+++ source/js/iframeAcceptance.js
@@ -5,12 +5,16 @@ const revealIframes = () => {
         .forEach(iframeWrapper => {
             const iframe = iframeWrapper.querySelector('iframe');
             const iframeUrl = new URL(iframe.getAttribute('data-src'));
+            const iframeWrapperParent = iframeWrapper.parentNode;
             if(acceptedSuppliers.includes(iframeUrl.host)) {
+                if(iframeWrapperParent.classList.contains('embed__ratio--1-1')) {
+                    iframeWrapperParent.classList.replace('embed__ratio--1-1', 'embed__ratio--16-9');
+                 }
                 iframe.setAttribute('src', iframe.getAttribute('data-src'));
                 iframeWrapper.classList.remove('js-suppressed-iframe-wrapper');
                 iframeWrapper.style.position = 'static';
                 iframeWrapper.querySelector('.js-suppressed-iframe-prompt').classList.add('u-display--none');
-            }
+            } 
         }); 
 }
 
@@ -38,11 +42,21 @@ const suppressIframes = () => {
 }
 
 export default () => addEventListener('DOMContentLoaded', () => {
-    if (acceptedSuppliers.length > 0 && document.querySelectorAll('.js-suppressed-iframe-wrapper').length > 0 ) {
+    if (window.innerHeight > window.innerWidth) {
+        [...document.querySelectorAll('.embed__ratio--16-9')].forEach(embed => {
+            embed.classList.replace('embed__ratio--16-9', 'embed__ratio--1-1');
+        });
+    }
+    if (acceptedSuppliers.length > 0 && 
+        document.querySelectorAll('.js-suppressed-iframe-wrapper').length > 0 ) {
         [...document.querySelectorAll('.js-suppressed-iframe-wrapper')].forEach(iframeWrapper => {
             const iframe = iframeWrapper.querySelector('iframe');
             const iframeUrl = new URL(iframe.getAttribute('data-src'));
+            const iframeWrapperParent = iframeWrapper.parentNode;
             if (acceptedSuppliers.includes(iframeUrl.host)) {
+                 if (iframeWrapperParent.classList.contains('embed__ratio--1-1')) {
+                    iframeWrapperParent.classList.replace('embed__ratio--1-1', 'embed__ratio--16-9');
+                } 
                 iframe.setAttribute('src', iframeWrapper.querySelector('iframe').getAttribute('data-src'));
                 iframeWrapper.querySelector('.js-suppressed-iframe-prompt').style.display = 'none';
                 iframeWrapper.classList.remove('js-suppressed-iframe-wrapper'); 
