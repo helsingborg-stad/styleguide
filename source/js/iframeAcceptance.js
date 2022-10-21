2c5ce65caf297883e8a0d2055130dcf4fa101faa
NiclasNorin <niclas.norin@frontwalker.se>
2022-10-21 10:47:55 +0200

    More funcs moved button outside

--- source/js/iframeAcceptance.js
+++ source/js/iframeAcceptance.js
@@ -1,25 +1,35 @@
 let acceptedSuppliers = JSON.parse(localStorage.getItem('acceptedSuppliers')) ?? [];
 
+const revealIframe = (iframeWrapper) => {
+    const template = iframeWrapper.querySelector('template');
+    const iframeContent = iframeWrapper.querySelector('.c-acceptance__content');
+    iframeWrapper.querySelector('.js-suppressed-iframe-prompt').classList.add('u-display--none');
+    iframeWrapper.classList.remove('js-suppressed-iframe');
+    //iframeWrapper.style.position = 'static';
+    let clone = template.content.cloneNode(true);
+    iframeContent.appendChild(clone);
+    const iframe = iframeContent.querySelector('iframe');
+    if (iframeWrapper.classList.contains('c-acceptance--video')) {
+        embedVideo(iframe, iframeWrapper);
+    } else {
+        iframe.setAttribute('src', iframe.getAttribute('data-src'));
+    }
+}
+
+const embedVideo = (iframe, iframeWrapper) => {
+    console.log("hej");
+    const embed = iframeWrapper.parentNode;
+    console.log(embed);
+}
+
+
 const revealIframes = () => {
     [...document.querySelectorAll('.js-suppressed-iframe')]
         .forEach(iframeWrapper => {
             const iframeUrl = new URL(iframeWrapper.getAttribute('data-src'));
 
            if(acceptedSuppliers.includes(iframeUrl.host)) {
-                const template = iframeWrapper.querySelector('template');
-                const iframeContent = iframeWrapper.querySelector('.c-acceptance__content');
-                iframeWrapper.querySelector('.js-suppressed-iframe-prompt').classList.add('u-display--none');
-                iframeWrapper.classList.remove('js-suppressed-iframe');
-                //iframeWrapper.style.position = 'static';
-                let clone = template.content.cloneNode(true);
-                iframeContent.appendChild(clone);
-                const iframe = iframeContent.querySelector('iframe');
-                if(iframeWrapper.classList.contains('c-acceptance--video')) {
-                    embedVideo(iframe);
-                } else {
-                    iframe.setAttribute('src', iframe.getAttribute('data-src'));
-                }
-                
+                revealIframe(iframeWrapper);
             }  
         }); 
 }
@@ -38,6 +48,7 @@ const onClicklHandler = (iframeWrapper) => {
 const suppressIframes = () => {
     [...document.querySelectorAll('.js-suppressed-iframe')]
     .forEach(iframeWrapper => {
+            iframeWrapper.querySelector('.js-suppressed-iframe-prompt').style.display = 'flex';
             const buttonEl = iframeWrapper.querySelector('[js-suppressed-iframe-button]');
             buttonEl.params = {iframe: iframeWrapper};
             buttonEl.addEventListener('click', () => {
@@ -47,22 +58,16 @@ const suppressIframes = () => {
         });
 }
 
-const embedVideo = (iframe) => {
-    console.log("hej");
-}
-
 export default () => addEventListener('DOMContentLoaded', () => {
-    /* if (acceptedSuppliers.length > 0 && document.querySelectorAll('.js-suppressed-iframe').length > 0 ) {
+    if (acceptedSuppliers.length > 0 && document.querySelectorAll('.js-suppressed-iframe').length > 0 ) {
         [...document.querySelectorAll('.js-suppressed-iframe')].forEach(iframeWrapper => {
-            const iframe = iframeWrapper.querySelector('iframe');
             const iframeUrl = new URL(iframeWrapper.getAttribute('data-src'));
+            console.log(iframeUrl);
             if (acceptedSuppliers.includes(iframeUrl.host)) {
-                iframe.setAttribute('src', iframeWrapper.getAttribute('data-src'));
-                iframeWrapper.querySelector('.js-suppressed-iframe-prompt').style.display = 'none';
-                iframeWrapper.classList.remove('js-suppressed-iframe'); 
+               revealIframe(iframeWrapper);
             }
         })
-    } */
+    }
     document.querySelectorAll('.js-suppressed-iframe').length > 0 ? suppressIframes() : '';
 });
 
