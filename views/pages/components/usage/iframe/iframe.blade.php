f9ccf0121726e96cf8f2d0c1ffb6db16e1d74003
Anna <anna@thehappyhipster.se>
2022-11-09 10:16:50 +0100

    Added description to acceptance component docs (#591)
    * Added description to acceptance component docs
    
    * Fixed mismatched quotation mark

--- views/pages/components/usage/iframe/iframe.blade.php
+++ views/pages/components/usage/iframe/iframe.blade.php
@@ -1,8 +1,7 @@
 @iframe([
     'src' => 'https://helsingborg.se',
-	'title' => 'Startsidan på helsingborg.se",
+	'title' => 'Startsidan på helsingborg.se',
 	'width' => 800,
 	'height' => 600,
-	'options' => '{"knownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n en annan {SUPPLIER_WEBSITE}. Genom att forts\u00e4tta godk\u00e4nner du <a href=\"{SUPPLIER_POLICY}\">GDPR och Sekretesspolicyn<\/a>","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."},"unknownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n en annan webbplats. Genom att forts\u00e4tta godk\u00e4nner du GDPR och Sekretesspolicyn","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."}}'
 ])
 @endiframe
