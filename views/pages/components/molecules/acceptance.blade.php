02f959c281abc54ca60e2f6b6e0cfffe610f7154
Anna Johansson <anna@thehappyhipster.se>
2022-11-09 08:06:54 +0100

    Added description to acceptance component docs

--- views/pages/components/molecules/acceptance.blade.php
+++ views/pages/components/molecules/acceptance.blade.php
@@ -3,6 +3,11 @@
 @section('doc-content')
     @markdown
         #Acceptance
+		A component that can wrap any other content to prevent it from being rendered on page load. 
+		
+		Wrapped content will instead be printed inside a ```<template>``` tag and a message will be shown to the user that lets them know that there are blocked content that they need to accept to by clicking a button included with the message.
+
+		#
     @endmarkdown
 
     @doc(['slug' => 'acceptance'])
