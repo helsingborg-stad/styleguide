a1047267f8a809b7f7d7075f716c2d20317380f6
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-11-14 16:43:05 +0100

    Acceptance map style

--- views/pages/components/usage/acceptance/video.blade.php
+++ views/pages/components/usage/acceptance/video.blade.php
@@ -1,13 +1,9 @@
 <div style="position: relative;">
 @acceptance([
-    "labels"    => (object) [
-        'title'     => "Title",
-        'info'      => "This is the body of acceptance it can include {{name}}.",
-        'button'    => "Hey, i'm okay with that!"
-    ],
+    "labels"    => '{"knownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n {SUPPLIER_WEBSITE}. Genom att forts\u00e4tta godk\u00e4nner du <a href=\"{SUPPLIER_POLICY}\">GDPR och Sekretesspolicyn<\/a>.","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."},"unknownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n en annan webbplats ({SUPPLIER_WEBSITE}). Genom att forts\u00e4tta godk\u00e4nner du GDPR och Sekretesspolicyn.","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."}}',
     "modifier"  => 'video',
     "height"    => '500',
-    "src"       => 'https://helsingborg.se',
+    "src"       => 'https://www.youtube.com/watch?v=axiYo61XRRw',
     "policy"    => 'https://helsingborg.se/policy',
     "host"      => 'helsingborg.se',
     "name"      => 'Helsingborg Stad',
