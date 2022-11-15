35bcd32fb7fc67b3abb905574234eaf27f676719
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-11-15 15:26:58 +0100

    Add labels to documentation

--- views/pages/components/usage/iframe/video.blade.php
+++ views/pages/components/usage/iframe/video.blade.php
@@ -3,5 +3,6 @@
 	'title' => 'A video from youtube',
 	'width' => 800,
 	'height' => 600,
+	'labels' => '{"knownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n {SUPPLIER_WEBSITE}. Genom att forts\u00e4tta godk\u00e4nner du <a href=\"{SUPPLIER_POLICY}\">GDPR och Sekretesspolicyn<\/a>.","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."},"unknownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n en annan webbplats ({SUPPLIER_WEBSITE}). Genom att forts\u00e4tta godk\u00e4nner du GDPR och Sekretesspolicyn.","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."}}',
 ])
 @endiframe
