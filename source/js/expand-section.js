3243e5a3b75291654e189d5298a1a1f53ef8d7e2
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2022-11-22 16:21:14 +0100

    Navigate to correct element when opening and closing (#606)
    * Navigate to correct element when opening and closing
    
    * test
    
    * Bounce and variable
    
    * Documentation

--- source/js/expand-section.js
+++ source/js/expand-section.js
@@ -6,21 +6,79 @@ const EXPANDED = 'aria-expanded';
 const CONTROLS = 'aria-controls';
 const HIDDEN = 'aria-hidden';
 
+const setMarginEventListener = () => {
+    window.addEventListener('resize', debounce(setMarginVariable, 2000));
+}
+
+const setMarginVariable = (root, header) => {
+    root.style.setProperty('--c-accordion-scroll-margin', (header.offsetHeight + 20) + 'px');
+}
+
+/**
+ * The debounce function takes a function and a delay as arguments, and returns a function that will
+ * call the passed function after the delay has passed.
+ * @param func - the function that will be called
+ * @param delay - the amount of time to wait before running the function
+ * @returns A function that takes two arguments, root and header.
+ */
+const debounce = (func, delay) => {
+    let timer;
+    let root = document.querySelector(':root');
+    const header = document.querySelector('#site-header');
+
+    func(root, header);
+
+    return () => {
+        timer ? clearTimeout(timer) : '';
+        timer = setTimeout(() => {
+            func(root, header);
+        }, delay);
+    }
+}
+
+
 /**
  * Adds toggle event listeners to expandable elements
  */
 const expandSection = () => {
     const buttons = document.querySelectorAll(BUTTON);
+    let i = 0;
+    let prev = false;
+    
+    if (document.querySelector('#site-header').classList.contains('c-header--sticky')) {
+        setMarginEventListener();
+    }
 
     buttons.forEach((button) => {
-        button.addEventListener('click', function (e) {
-            e.preventDefault();
+        button.setAttribute('js-accordion-button', i);
+        i++;
 
+        button.addEventListener('click', function (e) {
             const expanded = button.getAttribute(EXPANDED) === 'true';
             toggleButton(button, expanded);
+            prev = handleAnchor(button, prev, expanded, e);
         });
     });
 };
+
+const handleAnchor = (button, prev, expanded, e) => {
+    if (prev && !expanded) {
+        if (parseInt(button.getAttribute('js-accordion-button')) > parseInt(prev.getAttribute('js-accordion-button'))) {
+
+        } else {
+            e.preventDefault();
+        }
+    } else {
+        e.preventDefault();
+    }
+
+    if (!expanded) {
+        return button;
+
+    } else {
+        return false;
+    }
+}
 
 /**
  * Toggles a button's and its siblings "pressed" state
