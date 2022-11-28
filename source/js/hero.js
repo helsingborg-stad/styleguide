8702be043e07bc95498e226c05c532e3ace93729
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-28 13:49:01 +0100

    Remove console.log

--- source/js/hero.js
+++ source/js/hero.js
@@ -2,14 +2,12 @@ import VideoControls from './helpers/video';
 class Hero {
     constructor() {
         this.heroVideos = document.querySelectorAll('.c-hero--video');
-        
+
         this.heroVideos.length && this.handleVideoPause();
     }
 
     handleVideoPause() {
-        console.log(this.heroVideos);
         this.heroVideos.forEach(heroVideo => {
-            console.log(heroVideo);
             const video = new VideoControls(heroVideo);
         });
     }
