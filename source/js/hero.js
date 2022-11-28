0cc9ab422ab813d8c911364a9f39e45d680c7e7b
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-28 09:46:57 +0100

    Video init push

---
+++ source/js/hero.js
@@ -0,0 +1,17 @@
+class Hero {
+    constructor() {
+        this.videoItemClasses = ['c-hero--video'];
+        this.pause = 'js-pause-video';
+        this.play = 'js-play-video';
+
+        this.handleVideoPause();
+    }
+
+    handleVideoPause() {
+       this.videoItemClasses.forEach(videoItemClass => {
+        
+       })
+    }
+}
+
+export default Hero;
