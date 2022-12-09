2cc374272f98093052bad8a0c6026cc7fb8aca10
NiclasNorin <niclas.norin@frontwalker.se>
2022-12-09 15:46:35 +0100

    Fixes v2

--- source/js/tooltip.js
+++ source/js/tooltip.js
@@ -4,8 +4,8 @@ constructor(){
 }
 
     setDebounce() {
-        
         const containers = document.querySelectorAll('.c-tooltip__container');
+
         if(containers.length > 0) {
             window.addEventListener('resize', this.debounce(containers, 1000));
         } else { return false;};
@@ -45,24 +45,33 @@ constructor(){
 
         if (position.right > document.documentElement.clientWidth) {
             this.overflowRight(parent, position);
-        }
+
+        } 
 
         if (position.left < 0) {
-            this.overflowLeft(parent);
+            this.overflowLeft(parent, position);
         }
     }
 
     overflowRight(parent, position) {
         if(parent.classList.contains('c-tooltip--right')) {
-            this.handleClasses(parent, ['c-tooltip--right'], 'c-tooltip--left');
+            if(position.left - position.width > 0) {
+                this.handleClasses(parent, ['c-tooltip--right'], 'c-tooltip--left');
+            } else {
+                this.handleClasses(parent, ['c-tooltip--right'], 'c-tooltip--bottom');
+            }
         } else {
             this.handleClasses(parent, ['c-tooltip--overflow-left'], 'c-tooltip--overflow-right');
         }
     }
 
-    overflowLeft(parent) {
+    overflowLeft(parent, position) {
         if(parent.classList.contains('c-tooltip--left')) {
-            this.handleClasses(parent, ['c-tooltip--left'], 'c-tooltip--right');
+            if(document.documentElement.clientWidth - position.width > 0) {
+                this.handleClasses(parent, ['c-tooltip--left'], 'c-tooltip--right');
+            } else {
+                this.handleClasses(parent, ['c-tooltip--left'], 'c-tooltip--bottom');
+            }
         }
         else {
             this.handleClasses(parent, ['c-tooltip--overflow-right'], 'c-tooltip--overflow-left');
@@ -70,7 +79,7 @@ constructor(){
     }
 
     overflowBoth(parent) {
-        this.handleClasses(parent, ['c-tooltip--overflow-left', 'c-tooltip--overflow-left'], 'c-tooltip--full-width')
+        this.handleClasses(parent, ['c-tooltip--overflow-left', 'c-tooltip--overflow-right'], 'c-tooltip--full-width')
     }
 
     resetDirection(parent, position, originalDirection) {
