77354526cc56edc9c42e2c3a222d3823a540feac
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-12-23 10:49:05 +0000

    fix: automate composer install in setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 if [ -d "source/library" ]; then
     cd ./source/library/ || git pull origin master
     echo Library repository already installed in source/library. You might want to add this in your git-client-ui.
+    cd ../..
 else
     if ! (git clone "git@github.com:helsingborg-stad/component-library.git" "source/library") then
         git clone "https://github.com/helsingborg-stad/component-library.git" "source/library"
     fi
 fi
+cd ./source/library/ && composer install
