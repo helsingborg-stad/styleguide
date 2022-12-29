a2487ab4698dea785a724ba0bbda5114e12298da
NiclasNorin <niclas.norin@frontwalker.se>
2022-12-29 11:25:43 +0100

    uses utilities for more possibilities and less code

--- views/pages/components/usage/slider/slider_item.blade.php
+++ views/pages/components/usage/slider/slider_item.blade.php
@@ -57,7 +57,7 @@
         'title' => 'Base Layout',
         'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
-        'containerColor' => 'white',
+        'containerColor' => 'darkest',
         'layout' => 'center',
     ])
     @endslider__item
