397d0b10eeb22b556837d9d1bd3f3e7473ce1484
NiclasNorin <niclas.norin@frontwalker.se>
2022-12-30 15:29:50 +0100

    List mixin documentation

---
+++ views/pages/mixins/list.blade.php
@@ -0,0 +1,46 @@
+@extends('layout.containers.doc')
+
+@section('doc-content')
+    @markdown
+        #List
+        The list mixin lets you change the visuals of a list.
+    @endmarkdown
+
+    @paper(['padding' => '3'])
+      @typography(['variant' => 'h3', 'element' => 'h3'])
+        Unlist
+      @endtypography
+
+        <ul class="mixins-example-list">
+            <li>
+                This list item has no visual list styling.
+            </li>
+        </ul>
+
+      @code(['language' => 'html', 'content' => "", 'classList' => ['d-code', 'u-margin__top--3']])
+        {{\HbgStyleGuide\Helper\ParseString::tidyHtml('
+            <ul class="example-list">
+                <li>
+                    This list item has no visual list styling.
+                </li>
+            </ul>
+        ')}}
+      @endcode
+      @code(['language' => 'scss', 'content' => "", 'classList' => ['d-code']])
+        .mixins-example-list {
+            {{'@'}}include unlist()
+        }
+      @endcode
+
+    @endpaper
+@stop
+
+<style>
+    .mixins-example-list   {
+        list-style-type: none;
+    }
+    .mixins-example-list li::before, .mixins-example-list::before {
+        display: none;
+    }
+    
+</style>
