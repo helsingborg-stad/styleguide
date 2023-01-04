779147307c9fc17b89359ec055d78db4670882e2
Thor Brink <thor.brink@helsingborg.se>
2023-01-04 12:05:05 +0100

    Add slider with cards for reference

---
+++ views/pages/components/usage/slider/slider_with_cards.blade.php
@@ -0,0 +1,31 @@
+@slider([
+    'showStepper' => false,
+    'autoSlide' => false,
+    'attributeList' => [
+        'data-slides-per-page' => 4
+    ],
+])
+
+    @for ($i = 0; $i < 4; $i++)
+    
+        @slider__item([
+            'layout' => 'center',
+            'textColor' => 'black',
+            'heroStyle' => false,
+            'classList' => ['c-slider__item--post']
+        ])
+
+            @card([
+                'link' => 'https://www.helsingborg.se',
+                'image' => ['src' => "https://picsum.photos/id/$i/267", 'alt' => 'ALT'],
+                'heading' => "Card #$i",
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan pharetra porta. Nullam id est suscipit velit tincidunt ultrices a non mi.',
+                'classList' => ['u-color__text--info'],
+            ])
+            @endcard
+
+        @endslider__item
+
+    @endfor
+
+@endslider
