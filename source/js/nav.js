196fa331abe6be12d90f725108a43b94f0c46702
NiclasNorin <niclas.norin@frontwalker.se>
2023-01-27 10:05:54 +0100

    Fixing js crashing

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,20 +1,32 @@
 class Nav {
     constructor() {
-        this.setListeners();
+        this.hasMainMenu();
+    }
+
+    hasMainMenu() {
+        document.querySelector('#main-menu') && this.setListeners();
     }
 
     setListeners() {
         const menu = document.querySelector('#main-menu');
         const mainItems = menu.querySelectorAll('.c-nav--depth-0 > .c-nav__item');
 
-        menu && this.clickListeners(menu, mainItems);
+        mainItems && this.clickListeners(menu, mainItems);
     }
 
     clickListeners(menu, mainItems) {
         const menuItems = menu.querySelectorAll('.c-nav__item');
 
+        mainItems.forEach(mainItem => {
+            mainItem.addEventListener('click', (e) => {
+                e.stopPropagation();
+                this.openChildren(mainItem);
+            })
+        });
+
+
         menuItems.forEach(menuItem => {
-            if(menuItem.querySelector('.c-nav')) {
+            if (menuItem.querySelector('.c-nav')) {
                 menuItem.querySelector('a') && this.handleLinks(menuItem);
                 menuItem.addEventListener('click', (e) => {
                     e.stopPropagation();
@@ -27,12 +39,22 @@ class Nav {
         });
     }
 
+    openChildren(mainItem) {
+        const currentItem = mainItem.querySelector('.is-current');
+        if (currentItem) {
+            const depth = currentItem.hasAttribute('depth') ? currentItem.getAttribute('depth') : false;
+            for (let i = 0; i < depth - 1; i++) {
+                currentItem.closest(`[depth="${i + 1}"]`).classList.add('is-active');
+            }
+        }
+    }
+
     handleClickVisible(menuItem, mainItems) {
         mainItems.forEach(item => {
-            if(!item.contains(menuItem)) {
-            item.classList.remove('is-active');
+            if (!item.contains(menuItem)) {
+                item.classList.remove('is-active');
                 item.querySelectorAll('.c-nav__item').forEach(childItem => {
-                    childItem.classList.remove('is-active');
+                    // childItem.classList.remove('is-active');
                 })
             }
         })
@@ -45,7 +67,7 @@ class Nav {
 
     handleLinks(menuItem) {
         const link = menuItem.querySelector('a');
-        if(link.querySelector('template')) {
+        if (link.querySelector('template')) {
             let temp = link.querySelector('template');
             let clone = temp.content.cloneNode(true);
             link.appendChild(clone);
