57b5f8c08b08d805c8c9dd87cd8247830c6651c7
Thor Brink <thor.brink@helsingborg.se>
2023-01-31 13:45:22 +0100

    Add opacity modifier to preloader (#689)
    * Add opacity modifier to preloader
    
    * Refactor preloader styles

--- views/pages/utilities/pre-loader.blade.php
+++ views/pages/utilities/pre-loader.blade.php
@@ -6,6 +6,7 @@
     @markdown
         #Pre-loader
     @endmarkdown
+
     @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'pre-loader', 'config' => 'pre-loader']])
         @typography([
         ])
@@ -16,6 +17,22 @@
         <div class="u-preloader" style="background-color:blue;width:200px;height:200px;"></div>
 
     @endutility_doc
+    
+    @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'pre-loader', 'config' => 'opacity']])
 
+        <div class="u-display--flex u-flex--gridgap">
+            <div class="u-preloader u-preloader__opacity--1" style="background-color:blue;width:200px;height:40px;"></div>
+            <div class="u-preloader u-preloader__opacity--2" style="background-color:blue;width:200px;height:40px;"></div>
+            <div class="u-preloader u-preloader__opacity--3" style="background-color:blue;width:200px;height:40px;"></div>
+            <div class="u-preloader u-preloader__opacity--4" style="background-color:blue;width:200px;height:40px;"></div>
+            <div class="u-preloader u-preloader__opacity--5" style="background-color:blue;width:200px;height:40px;"></div>
+            <div class="u-preloader u-preloader__opacity--6" style="background-color:blue;width:200px;height:40px;"></div>
+            <div class="u-preloader u-preloader__opacity--7" style="background-color:blue;width:200px;height:40px;"></div>
+            <div class="u-preloader u-preloader__opacity--8" style="background-color:blue;width:200px;height:40px;"></div>
+            <div class="u-preloader u-preloader__opacity--9" style="background-color:blue;width:200px;height:40px;"></div>
+        </div>
+
+    @endutility_doc
+    
 </article>
 @stop
