4bc900080f94fbd6b02bd49af5a74acf7b5d5020
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2023-03-02 13:06:40 +0100

    Base for siteselector component (#702)
    * Base for siteselector component
    
    * Disable all js
    
    * Simplify js-functionality for nav menus
    
    * Simplify toggling as mutch as possible
    
    * Add close siblings function
    
    * Clarify what selectors we are using
    
    * Only toggle button on vertical menus.
    
    * Fix height overflow in sitenav
    
    * Styling of siteselector
    
    * Add hover & active states.
    
    * Define all color variables
    
    Danger color for visibility right now.
    
    * Rename vars
    
    * Fix hover interaction
    
    ---------
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

---
+++ views/pages/components/usage/siteselector/many.blade.php
@@ -0,0 +1,12 @@
+<div style="min-height: 500px;">
+  @siteselector([
+    'items' => [
+      ['label' => 'Google', 'href' => 'https://google.com'],
+      ['label' => 'This site', 'href' => 'https://' . $_SERVER['HTTP_HOST']],
+      ['label' => 'Facebook', 'href' => 'https://facebok.com'],
+      ['label' => 'Twitter', 'href' => 'https://twitter.com']
+    ],
+    'maxItems' => 2
+  ])
+  @endsiteselector
+</div>
