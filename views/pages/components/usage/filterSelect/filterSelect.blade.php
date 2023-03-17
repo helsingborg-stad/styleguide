ffe529253070c5d55d332ea90a665f5511c2c5c3
NiclasNorin <niclas.norin@frontwalker.se>
2023-03-17 14:00:04 +0100

    added helper and longer array to docs

--- views/pages/components/usage/filterSelect/filterSelect.blade.php
+++ views/pages/components/usage/filterSelect/filterSelect.blade.php
@@ -1,15 +1,5 @@
-@php 
-$options = [];
-    for ($i = 0; i <= 100; $i++) {
-        $option = [
-            'label' => 'Option' . $i;
-            'value' => 'option-' . $i;
-        ];
-    $options[] = $option;
-    }
-@endphp
 @filterSelect([
-    'options' => $options,
+    'options' => HbgStyleGuide\Helper\MockupArrays::getMockupArray(['label', 'value'], 31),
     'attributeList' => ['style' => 'height: 400px']
 ])
 @endfilterSelect
