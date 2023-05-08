28b03354edad18d736bd84c08f4ffd946864bb57
Thor Brink <thor.brink@helsingborg.se>
2023-05-08 12:47:29 +0200

    Misc. slider bugs on smaller screens (#762)
    * fix: slide moving more than 1 per page on small screens
    
    * fix: slider item content flickering in safari/ios-safari

--- source/js/slider.ts
+++ source/js/slider.ts
@@ -72,6 +72,7 @@ export default class Slider {
             breakpoints: {
                 896: {
                     perPage: 1,
+                    perMove: 1,
                 }
             }
         });
