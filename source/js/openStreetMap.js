6c4dbced74ffaef115e253d02d7f91105a5ea055
NiclasNorin <niclas.norin@frontwalker.se>
2023-05-12 15:02:13 +0200

    pale map fix

--- source/js/openStreetMap.js
+++ source/js/openStreetMap.js
@@ -164,8 +164,8 @@ class OpenStreetMap {
             case 'pale':
                 return {
                     attribution:
-                        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributor',
-                    url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+                        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+                    url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
                 };
             case 'default':
                 return {
