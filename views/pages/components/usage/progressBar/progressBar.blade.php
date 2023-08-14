98b9d014f53ee9e167d17f115a9a5ae794617ac7
NiclasNorin <niclas.norin@frontwalker.se>
2023-08-14 17:07:58 +0200

    Feat: Progressbar test 2 docs

---
+++ views/pages/components/usage/progressBar/progressBar.blade.php
@@ -0,0 +1,11 @@
+@progressBar([
+    'value' => 25,
+])
+@endprogressBar
+
+@progressBar([
+    'value' => 75,
+    'isCancelled' => true,
+    'classList' => ['u-margin__top--3']
+])
+@endprogressBar
