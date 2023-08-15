1da95e88bf3419fd5aec1d26b4d793842b4dfbab
NiclasNorin <niclas.norin@frontwalker.se>
2023-08-15 10:33:05 +0200

    Feat: Anchor menu description

--- views/pages/components/organisms/anchorMenu.blade.php
+++ views/pages/components/organisms/anchorMenu.blade.php
@@ -3,6 +3,9 @@
 @section('doc-content')
     @markdown
         #Anchor menu
+        
+        The anchor menu keeps track of its position and display to the user which part of the page is active. <br />
+        The items in the menu will only show when the there is a matching element on the page.
     @endmarkdown
 
     @doc(['slug' => 'anchorMenu'])
