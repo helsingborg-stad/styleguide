a1517441dd0df47441bfd063e3823b8fecd9266d
Niclas Norin <niclas.norin@helsingborg.se>
2023-08-28 11:29:59 +0200

    Reset: Resetting tests

--- source/js/stretch.js
+++ source/js/stretch.js
@@ -1,8 +1,5 @@
 export default () => {
-    document.addEventListener('DOMContentLoaded', () => {
-        const body = document.querySelector("body");
-        const scrollbar = window.innerWidth - document.documentElement.clientWidth;
-        console.log("inner:", window.innerWidth,"client:", document.documentElement.clientWidth);
-        body.setAttribute("style", `--scrollbar: ${scrollbar}px`);
-    })
+    const body = document.querySelector("body");
+    const scrollbar = window.innerWidth - document.documentElement.clientWidth;
+    body.setAttribute("style", `--scrollbar: ${scrollbar}px`);
 }
