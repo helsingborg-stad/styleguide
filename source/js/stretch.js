d071985f3d2231369381e4427b28f8582a181fe9
Niclas Norin <niclas.norin@helsingborg.se>
2023-08-28 09:51:58 +0200

    Test: Domcontent loaded

--- source/js/stretch.js
+++ source/js/stretch.js
@@ -1,7 +1,8 @@
 export default () => {
-    const body = document.querySelector("body");
-    const scrollbar = window.innerWidth - document.documentElement.clientWidth;
-    console.log(window.innerWidth, document.documentElement.clientWidth);
-    console.log(body.getBoundingClientRect());
-    body.setAttribute("style", `--scrollbar: ${scrollbar}px`);
+    document.addEventListener('DOMContentLoaded', () => {
+        const body = document.querySelector("body");
+        const scrollbar = window.innerWidth - document.documentElement.clientWidth;
+        console.log(window.innerWidth, document.documentElement.clientWidth);
+        body.setAttribute("style", `--scrollbar: ${scrollbar}px`);
+    })
 }
