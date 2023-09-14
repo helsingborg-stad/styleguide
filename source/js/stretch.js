da1578a54362b3b977e744f7ae32a52ad432ccb6
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2023-09-14 16:36:52 +0200

    Fix: Scrollbar --var faulty (#831)
    Co-authored-by: Niclas Norin <niclas.norin@helsingborg.se>

--- source/js/stretch.js
+++
@@ -1,5 +0,0 @@
-export default () => {
-    const body = document.querySelector("body");
-    const scrollbar = window.innerWidth - document.documentElement.clientWidth;
-    body.setAttribute("style", `--scrollbar: ${scrollbar}px`);
-}
