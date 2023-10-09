5f635522d1a791234c257509845fb77ddf470e59
gexen2 <51493362+gexen2@users.noreply.github.com>
2023-10-09 12:04:24 +0200

    Revert "Bugfix: Single select dropdown doesn't close on selection"

--- source/js/select/Select.ts
+++ source/js/select/Select.ts
@@ -46,7 +46,6 @@ export class Select {
 		this.selectElement.addEventListener('change', () => this.disableMultiSelectOptionsWhenMaxSelectionsReached())
 		this.selectElement.addEventListener('change', () => this.updatePlaceholderText());
 		this.selectElement.addEventListener('change', () => this.updateClearButtonVisibilityState());
-		this.selectElement.addEventListener('change', () => this.closeSingleSelectDropdown());
 		this.actionOverlayElement.addEventListener('keydown', (event) => this.openDropdownOnSpacebar(event));
 		this.clearButton?.addEventListener('click', () => this.setSingleSelectValue(null));
 		this.element.addEventListener('classListChange', () => this.updateDropdownAriaStateOnTopElementClassListChange());
@@ -64,15 +63,6 @@ export class Select {
 	updateDropdownAriaStateOnTopElementClassListChange(): void {
 		const isOpen = this.element.classList.contains('is-open');
 		this.dropDownElement.setAttribute('aria-hidden', Boolean(!isOpen).toString());
-	}
-
-	closeSingleSelectDropdown() : void {
-		if(!this.isMultiSelect()) {
-			const element = this.element.classList;
-			if(element.contains('is-open')) {
-				element.remove('is-open');
-			}
-		}
 	}
 
 	selectOptionOnElementClick(optionElement: HTMLElement) {
