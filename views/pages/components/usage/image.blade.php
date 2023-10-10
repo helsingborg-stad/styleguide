20fd49afb34da596e6c5e27a9f7a3e6e533d02b2
NiclasNorin <niclasn@outlook.com>
2023-10-10 11:39:39 +0200

    Feat: Image photographer

--- views/pages/components/usage/image.blade.php
+++ views/pages/components/usage/image.blade.php
@@ -3,9 +3,6 @@
         'src'=> "https://your-url.se",
         'alt' => "This is an image",
         'caption' => "Your caption text",
-        'roundedTopLeft' => true,
-        'roundedBottomRight' => true,
-        'roundedRadius' => "md"
     ]
 )
 @endimage
