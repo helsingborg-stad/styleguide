dabebd0da4d20c6d877088e5806573d26aca361a
Niclas Norin <niclas.norin@helsingborg.se>
2023-10-12 16:23:28 +0200

    Fix: Adds mobile navigation for styleguide docs

--- views/layout/containers/home.blade.php
+++ views/layout/containers/home.blade.php
@@ -3,6 +3,18 @@
 @section('content')
     <div class="container--home">
         @yield('hero')
+
+        @sidebar([
+            'logo' => '/assets/img/logotype-grey-full.svg',
+            'items' => $sideNavigation,
+            'classList' => ['l-docs--sidebar', 'c-sidebar--fixed', 'u-display--none@md', 'u-display--none@lg', 'u-display--none@xl'],
+            'attributeList' => [
+                'js-toggle-item'    => 'js-mobile-sidebar',
+                'js-toggle-class'   => 'c-sidebar--collapsed'
+            ],
+        ])
+
+        @endsidebar
         
         <section>
             <article class="article">
