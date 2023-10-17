8316ccfbbd4028f98e43a88e775d00616e4ea635
Niclas Norin <niclas.norin@helsingborg.se>
2023-10-17 10:17:20 +0200

    Fix: Correct labels for acceptance docs

--- views/pages/components/usage/acceptance/iframe.blade.php
+++ views/pages/components/usage/acceptance/iframe.blade.php
@@ -1,5 +1,16 @@
 @acceptance([
-    "labels"    => '{"knownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n {SUPPLIER_WEBSITE}. Genom att forts\u00e4tta godk\u00e4nner du <a href=\"{SUPPLIER_POLICY}\">GDPR och Sekretesspolicyn<\/a>.","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."},"unknownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n en annan webbplats ({SUPPLIER_WEBSITE}). Genom att forts\u00e4tta godk\u00e4nner du GDPR och Sekretesspolicyn.","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."}}',
+    'labels' => [
+		'knownLabels' => (object) [
+			'title' => 'This is the title of a known website ({SUPPLIER_WEBSITE}). You can find their policy here: {SUPPLIER_POLICY}.',
+			'info' => 'This is the information that is shown describing what accepting means.',
+			'button' => 'accept'
+		],
+		'unknownLabels' => (object) [
+			'title' => 'This is the content of a unknown website ({SUPPLIER_WEBSITE}).',
+			'info' => 'This is the information that is shown describing what accepting means.',
+			'button' => 'accept'
+		]
+	],
     "height"    => '500',
     "src"       => 'https://google.com',
     "policy"    => 'https://google.com/policy',
