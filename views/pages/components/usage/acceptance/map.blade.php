d20936b621190d91776d0bd815bb4e06268dd51c
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2023-10-19 14:36:08 +0200

    docs: iframe, script and acceptance component (#855)

--- views/pages/components/usage/acceptance/map.blade.php
+++ views/pages/components/usage/acceptance/map.blade.php
@@ -1,10 +1,18 @@
 @acceptance([
-    "labels"    => '{"knownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n {SUPPLIER_WEBSITE}. Genom att forts\u00e4tta godk\u00e4nner du <a href=\"{SUPPLIER_POLICY}\">GDPR och Sekretesspolicyn<\/a>.","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."},"unknownLabels":{"title":"Vi beh\u00f6ver ditt godk\u00e4nnande f\u00f6r att ta dig vidare","info":"Den h\u00e4r delen av hemsidan visar inneh\u00e5ll fr\u00e5n en annan webbplats ({SUPPLIER_WEBSITE}). Genom att forts\u00e4tta godk\u00e4nner du GDPR och Sekretesspolicyn.","button":"Jag f\u00f6rst\u00e5r, forts\u00e4tt."}}',
+    'labels' => [
+		'knownLabels' => [
+			'title' => 'This is the title of a known website ({SUPPLIER_WEBSITE}). You can find their policy here: {SUPPLIER_POLICY}.',
+			'info' => 'This is the information that is shown describing what accepting means.',
+			'button' => 'accept'
+		],
+		'unknownLabels' => [
+			'title' => 'This is the content of a unknown website ({SUPPLIER_WEBSITE}).',
+			'info' => 'This is the information that is shown describing what accepting means.',
+			'button' => 'accept'
+		]
+	],
     "height"    => '500',
-    "src"       => 'https://helsingborg.maps.arcgis.com/apps/Embed/index.html?webmap=5e2a37ce1fe649f78889205f484caafb&amp;extent=12.7047,56.0333,12.73,56.041&amp;home=true&amp;zoom=true&amp;scale=true&amp;search=true&amp;searchextent=false&amp;basemap_gallery=true&amp;disable_scroll=false&amp;theme=light',
-    "policy"    => 'https://google.com/policy',
-    "host"      => 'google.com',
-    "name"      => 'Google',
+    "src"       => ['https://helsingborg.maps.arcgis.com/apps/Embed/index.html?webmap=5e2a37ce1fe649f78889205f484caafb&amp;extent=12.7047,56.0333,12.73,56.041&amp;home=true&amp;zoom=true&amp;scale=true&amp;search=true&amp;searchextent=false&amp;basemap_gallery=true&amp;disable_scroll=false&amp;theme=light'],
 ])
     Hey! This should be a map!
 @endacceptance
