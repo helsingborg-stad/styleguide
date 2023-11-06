912daf6632c016be3acea466cde22fc55e6fd73f
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2023-11-06 10:32:53 +0100

    Chore: Adding paper documentation (#851)

---
+++ views/pages/components/usage/paper/paper.blade.php
@@ -0,0 +1,20 @@
+@paper([
+    'classList' => ['u-margin__bottom--4']
+])
+    Content wrapped with the paper component
+@endpaper
+
+@paper([
+    'classList' => ['u-margin__bottom--4'],
+    'padding' => '4'
+])
+    Content wrapped with the paper component. Padding parameter set to "4"
+@endpaper
+
+
+@paper([
+    'classList' => ['u-margin__bottom--4'],
+    'transparent' => true
+])
+    Content wrapped with the paper component. Transparent parameter set to "true" removing shadow and background.
+@endpaper
