2df553181f440719be2609661552527d606beddf
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-03-22 10:54:02 +0100

    fix: add missing ,

--- views/pages/components/usage/quote/quote.blade.php
+++ views/pages/components/usage/quote/quote.blade.php
@@ -1,6 +1,6 @@
 @quote([
     'content' => '<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p> <p>Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>',
-    'footer' => '<p><b>Lorem Ipsum</b><br>Dolor</p>'
+    'footer' => '<p><b>Lorem Ipsum</b><br>Dolor</p>',
     'image' => (object) [
         'src' => 'https://picsum.photos/id/1028/300/200',
         'alt' => 'Placeholder'
