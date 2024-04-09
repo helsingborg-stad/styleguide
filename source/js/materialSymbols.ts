0701aa90bca5122bedfae0db1af49cb91dd4aeec
NiclasNorin <niclasn@outlook.com>
2024-04-09 10:38:31 +0200

    feat: only display icons if font exists

--- source/js/materialSymbols.ts
+++ source/js/materialSymbols.ts
@@ -1,8 +1,26 @@
 export function initializeMaterialSymbols() {
-    document.querySelectorAll('[material-symbol]').forEach((icon) => {
-        const iconName = icon.getAttribute('material-symbol');
-        if (iconName) {
-            icon.innerHTML = iconName;
+    fontHasBeenLoaded().then((isLoaded) => {
+        if (isLoaded) {
+            document.querySelectorAll('[material-symbol]').forEach((icon) => {
+                const iconName = icon.getAttribute('material-symbol');
+                if (iconName) {
+                    icon.innerHTML = iconName;
+                }
+            });
         }
+    });
+}
+
+function fontHasBeenLoaded(): Promise<boolean> {
+    return new Promise(resolve => {
+        document.fonts.ready.then(fontFaces => {
+            let fontLoaded = false;
+            fontFaces.forEach(fontFace => {
+                if (fontFace.family === 'Material Symbols Outlined') {
+                    fontLoaded = true;
+                }
+            });
+            resolve(fontLoaded);
+        });
     });
 }
