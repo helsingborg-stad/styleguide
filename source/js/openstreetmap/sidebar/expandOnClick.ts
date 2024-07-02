1065f6fe84a0c5477054fb82a69a5e133483126f
NiclasNorin <niclasn@outlook.com>
2024-07-02 11:34:18 +0200

    feat: handle clicking expand icon correctly

--- source/js/openstreetmap/sidebar/expandOnClick.ts
+++ source/js/openstreetmap/sidebar/expandOnClick.ts
@@ -1,18 +1,22 @@
-import { invalidateSize } from "../map/mapHelpers";
 import L, { Layer, Map as LeafletMap, Marker, MarkerClusterGroup } from 'leaflet';
+import { invalidateSize } from "../map/mapHelpers";
 
 class ExpandOnClick {
+    expandButton: HTMLElement | null;
     constructor(private container: HTMLElement, private map: LeafletMap, private baseClass: string) {
+        this.expandButton = this.container.querySelector(`.${this.baseClass}__expand-icon`);
 
+        if (this.expandButton) {
+            this.setupExpandClick();
+        }
     }
 
-    setupExpandClick() {
-        const expand = this.container.querySelector(`.${this.baseClass}__expand-icon`);
-
-        expand?.addEventListener('click', () => {
+    private setupExpandClick() {
+        (this.expandButton as HTMLElement).addEventListener('click', () => {            
+            this.container.classList.toggle('is-expanded');
             invalidateSize(this.map);
         });
     }
 }
-
+                
 export default ExpandOnClick;
