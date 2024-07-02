281dd5ddc78195b5287a76064dc40e409ffdf7f9
NiclasNorin <niclasn@outlook.com>
2024-07-02 11:36:03 +0200

    chore: zoom to marker settings (will be removed)

--- source/js/openstreetmap/map/zoomToMarker.ts
+++ source/js/openstreetmap/map/zoomToMarker.ts
@@ -1,13 +1,17 @@
 import L, { Layer, Map as LeafletMap, Marker, MarkerClusterGroup } from 'leaflet';
 
-export function zoomToMarker(marker: Marker | undefined) {
+export function zoomToMarker(marker: Marker | undefined, map: LeafletMap | undefined = undefined, markers: MarkerClusterGroup | undefined = undefined) {
     if (marker && (marker as any).__parent) {
-        const cluster = (marker as any).__parent;
-        let hasMoreThanOnePin = true;
 
-        if (hasMoreThanOnePin) {
-            cluster.zoomToBounds();
-        } 
+        if (map && markers) {
+            markers.zoomToShowLayer(marker, function () {
+                console.log("runs");
+                map.flyTo(marker.getLatLng(), 16, {
+                    animate: true,
+                    duration: 1
+                });
+            });
+        }
         
         setTimeout(function () {
             marker.openPopup();
