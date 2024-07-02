223e2a36f69423b424b178c05b634e06eda432c6
NiclasNorin <niclasn@outlook.com>
2024-07-02 11:33:08 +0200

    feat: handle spinner and when to show sidebar

--- source/js/openstreetmap/sidebar/showIfNotEmpty.ts
+++ source/js/openstreetmap/sidebar/showIfNotEmpty.ts
@@ -1,29 +1,31 @@
 class ShowIfNotEmpty {
     constructor(private container: HTMLElement, private baseClass: string) {
         const sidebar = this.container.querySelector(`.${this.baseClass}__sidebar`);
+        const expandIcon = this.container.querySelector(`.${this.baseClass}__expand-icon`);
 
-        if (sidebar) {
-            this.checkForInnerBlocks(sidebar as HTMLElement);
-            this.listenForEndpointPosts(sidebar as HTMLElement);
+        if (sidebar && expandIcon) {
+            this.checkForInnerBlocks(sidebar as HTMLElement, expandIcon as HTMLElement);
+            this.listenForEndpointPosts(sidebar as HTMLElement, expandIcon as HTMLElement);
         }
     }
 
-    private checkForInnerBlocks(sidebar: HTMLElement): void {
+    private checkForInnerBlocks(sidebar: HTMLElement, expandIcon: HTMLElement): void {
         const innerBlocks = sidebar.querySelector(`.${this.baseClass}__inner-blocks`);
 
         if (innerBlocks && !innerBlocks.querySelector('innerblocks')) {
-            this.removeDisplayNoneUtility(sidebar);
+            this.removeDisplayNoneUtility(sidebar, expandIcon);
         }
     }
 
-    private listenForEndpointPosts(sidebar: HTMLElement): void {
-        this.container.addEventListener('postAdded', () => {
-            this.removeDisplayNoneUtility(sidebar);
+    private listenForEndpointPosts(sidebar: HTMLElement, expandIcon: HTMLElement): void {
+        this.container.addEventListener('fetchingPosts', () => {
+            this.removeDisplayNoneUtility(sidebar, expandIcon);
         });
     }
 
-    private removeDisplayNoneUtility(sidebar: HTMLElement): void {
+    private removeDisplayNoneUtility(sidebar: HTMLElement, expandIcon: HTMLElement): void {
         sidebar.classList.remove('u-display--none');
+        expandIcon.classList.remove('u-display--none');
     }
 }
 
