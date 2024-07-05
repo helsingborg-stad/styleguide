3a3bab8b9c29d5bd177290e8fa5a561492edb57a
Niclas <niclasn@outlook.com>
2024-07-05 15:07:57 +0200

    feat: expand for mobile

--- source/js/openstreetmap/sidebar/expandOnClick.ts
+++ source/js/openstreetmap/sidebar/expandOnClick.ts
@@ -2,19 +2,23 @@ import L, { Layer, Map as LeafletMap, Marker, MarkerClusterGroup } from 'leaflet
 import { invalidateSize } from "../map/mapHelpers";
 
 class ExpandOnClick {
-    expandButton: HTMLElement | null;
     constructor(private container: HTMLElement, private map: LeafletMap, private baseClass: string) {
-        this.expandButton = this.container.querySelector(`.${this.baseClass}__expand-icon`);
+        const expandButtonDesktop = this.container.querySelector(`.${this.baseClass}__expand-icon-desktop`);
+        const expandButtonMobile = this.container.querySelector(`.${this.baseClass}__expand-icon-mobile`);
 
-        if (this.expandButton) {
-            this.setupExpandClick();
+        console.log(expandButtonDesktop, expandButtonMobile)
+
+        if (expandButtonDesktop && expandButtonMobile) {
+            this.setupExpandClick(expandButtonDesktop, expandButtonMobile);
         }
     }
 
-    private setupExpandClick() {
-        (this.expandButton as HTMLElement).addEventListener('click', () => {            
-            this.container.classList.toggle('is-expanded');
-            invalidateSize(this.map);
+    private setupExpandClick(expandButtonDesktop: Element, expandButtonMobile: Element) {
+        [expandButtonDesktop, expandButtonMobile].forEach(element => {
+            element.addEventListener('click', () => {    
+                this.container.classList.toggle('is-expanded');
+                invalidateSize(this.map);
+            });
         });
     }
 }
