d2dc874526e3b1ac77076cdba1bc9e0e930d16be
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-05 11:20:14 +0200

    fix: add more cases to comments.

--- views/pages/components/usage/comment/comment.blade.php
+++ views/pages/components/usage/comment/comment.blade.php
@@ -1,10 +1,36 @@
+<?php 
+    $labels = [
+        'dateLabels' => [
+            'year' => 'år',
+            'month' => 'månad',
+            'week' => 'vecka',
+            'day' => 'dag',
+            'hour' => 'timme',
+            'minute' => 'minut',
+            'second' => 'sekund'
+            ],
+        'dateLabelsPlural' => [
+            'year' => 'år',
+            'month' => 'månader',
+            'week' => 'veckor',
+            'day' => 'dagar',
+            'hour' => 'timmar',
+            'minute' => 'minuter',
+            'second' => 'sekund'
+        ]
+    ];
+?>
+
+
 
 @comment([
     'author' => 'Peter Svensson',
     'author_url' => '#',
     'text' => 'This is a comment text',
     'icon' => 'face',
-    'date' => '2019-12-01 17:25:43'
+    'date' => '2019-12-01 17:25:43',
+    'dateLabels' => $labels['dateLabels'],
+    'dateLabelsPlural' => $labels['dateLabelsPlural']
 ])
     @slot('actions')
         @button([
@@ -33,33 +59,43 @@
     'author' => 'Peter Olsson',
     'text' => 'This is a reply comment text',
     'icon' => 'face',
-    'author_image' => 'https://picsum.photos/id/64/70/70',
+    'author_image' => 'https://picsum.photos/id/338/70/70',
     'date' => '2020-01-01 17:25:43',
-    'is_reply' => true
+    'is_reply' => true,
+    'dateLabels' => $labels['dateLabels'],
+    'dateLabelsPlural' => $labels['dateLabelsPlural']
+])
+@endcomment
+
+@comment([
+    'author' => 'Peter Olsson',
+    'text' => 'This is a reply comment text, it was written now.',
+    'icon' => 'face',
+    'author_image' => 'https://picsum.photos/id/342/70/70',
+    'date' => date("Y-m-d H:i:s"),
+    'is_reply' => true,
+    'dateLabels' => $labels['dateLabels'],
+    'dateLabelsPlural' => $labels['dateLabelsPlural']
+])
+@endcomment
+
+@comment([
+    'author' => 'Peter Olsson',
+    'text' => 'This is a reply comment text, it was written five hours ago.',
+    'icon' => 'face',
+    'author_image' => 'https://picsum.photos/id/64/70/70',
+    'date' => date("Y-m-d H:i:s", strtotime('-5 hours')),
+    'is_reply' => true,
+    'dateLabels' => $labels['dateLabels'],
+    'dateLabelsPlural' => $labels['dateLabelsPlural']
 ])
 @endcomment
 
 @comment([
     'author' => 'Peter Svensson',
     'date' => '2020-01-09 17:25:43',
-    'dateLabels' => [
-      'year' => 'år',
-      'month' => 'månad',
-      'week' => 'vecka',
-      'day' => 'dag',
-      'hour' => 'timme',
-      'minute' => 'minut',
-      'second' => 'sekund'
-    ],
-    'dateLabelsPlural' => [
-      'year' => 'år',
-      'month' => 'månader',
-      'week' => 'veckor',
-      'day' => 'dagar',
-      'hour' => 'timmar',
-      'minute' => 'minuter',
-      'second' => 'sekund'
-    ]
+    'dateLabels' => $labels['dateLabels'],
+    'dateLabelsPlural' => $labels['dateLabelsPlural']
 ])
-    This comment was sent through the slot
+    This comment <u>content</u> was sent through the slot.
 @endcomment
