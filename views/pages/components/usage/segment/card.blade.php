3057c3a1f89732762353de3603c7a17df431386a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-25 11:11:56 +0200

    fix: adapt to fully support c-image component to act as a segment image.

---
+++ views/pages/components/usage/segment/card.blade.php
@@ -0,0 +1,46 @@
+<div class="o-grid">
+    <div class="o-grid-12">
+        @segment([
+            'title'             => 'Fusce Amet Parturient Etiam',
+            'content'           => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            'layout'            => 'card',
+            'image'             => 'https://picsum.photos/id/342/2896/1944',
+            'containerAware'   => true
+        ])
+        @endsegment
+    </div>
+    <div class="u-padding__y--4"></div>
+    <div class="o-grid-4@md">
+        @segment([
+            'title'             => 'Fusce Amet Parturient Etiam',
+            'content'           => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            'layout'            => 'card',
+            'image'             => 'https://picsum.photos/id/342/2896/1944',
+            'textColor'         => 'dark',
+            'textAlignment'     => 'center'
+        ])
+        @endsegment
+    </div>
+    <div class="o-grid-4@md">
+        @segment([
+            'title'             => 'Fusce Amet Parturient Etiam',
+            'content'           => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            'layout'            => 'card',
+            'image'             => 'https://picsum.photos/id/342/2896/1944',
+            'textColor'         => 'dark',
+            'textAlignment'     => 'center'
+        ])
+        @endsegment
+    </div>
+    <div class="o-grid-4@md">
+        @segment([
+            'title'             => 'Fusce Amet Parturient Etiam',
+            'content'           => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            'layout'            => 'card',
+            'image'             => 'https://picsum.photos/id/342/2896/1944',
+            'textColor'         => 'dark',
+            'textAlignment'     => 'center'
+        ])
+        @endsegment
+    </div>
+</div>
