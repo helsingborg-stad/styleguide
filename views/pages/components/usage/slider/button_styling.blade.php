b8f03c5628d703332c5e6ac1a2369a7c9fd570db
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-30 09:30:58 +0200

    fix: update documentation to reflect new params in slider item.

--- views/pages/components/usage/slider/button_styling.blade.php
+++ views/pages/components/usage/slider/button_styling.blade.php
@@ -4,23 +4,20 @@
     'arrowButtons' => array("color" => "secondary", "style" => "outlined")
 ])
     @slider__item([
+        'image' => 'https://picsum.photos/1080/720',
         'title' => 'Base Layout',
-        'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'layout' => 'center',
-        'containerColor' => 'darkest',
-        'textColor' => 'white',
-
+        'containerColor' => 'darkest'
     ])
     @endslider__item
 
     @slider__item([
         'title' => 'Base Layout',
-        'sub_title' => 'This is a slightly longer title called subtitle',
+        'image' => 'https://picsum.photos/1080/720',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'layout' => 'bottom',
-        'containerColor' => 'darkest',
-        'textColor' => 'white',
+        'containerColor' => 'darkest'
     ])
     @endslider__item
 @endslider
--- views/pages/components/usage/slider/repeat_slide.blade.php
+++ views/pages/components/usage/slider/repeat_slide.blade.php
@@ -5,66 +5,52 @@
 ])
     @slider__item([
         'title' => 'Base Layout',
-        'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'layout' => 'center',
         'containerColor' => 'none',
         'overlay' => 'dark',
-        'textColor' => 'white',
         'textAlignment' => 'center',
-        'mobile_image' => 'https://picsum.photos/720/720',
-        'desktop_image' => 'https://picsum.photos/1080/720',
+        'image' => 'https://picsum.photos/1080/720',
         'heroStyle' => true
     ])
     @endslider__item
 
     @slider__item([
         'title' => 'Base Layout',
-        'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'layout' => 'bottom',
         'overlay' => 'light',
         'containerColor' => 'theme',
-        'textColor' => 'white',
-        'mobile_image' => 'https://picsum.photos/720/720',
-        'desktop_image' => 'https://picsum.photos/1080/720',
+        'image' => 'https://picsum.photos/1080/720',
     ])
     @endslider__item
 
     @slider__item([
         'title' => 'Base Layout',
-        'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'layout' => 'center',
         'containerColor' => 'none',
-        'mobile_image' => 'https://picsum.photos/720/720',
-        'desktop_image' => 'https://picsum.photos/1080/720',
+        'image' => 'https://picsum.photos/1080/720',
         'overlay' => 'light',
-        'overlay_opacity' => 'medium'
     ])
     @endslider__item
 
     @slider__item([
-        'title' => 'Base Layout',
-        'sub_title' => 'This is a slightly longer title called subtitle',
+        'title' => 'Base Layout',   
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'layout' => 'bottom',
         'containerColor' => 'white',
-        'textColor' => 'theme',
-        'mobile_image' => 'https://picsum.photos/720/720',
-        'desktop_image' => 'https://picsum.photos/1080/720',
-        'background_video' => 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        'image' => 'https://picsum.photos/1080/720',
+        'video' => 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
     ])
     @endslider__item
 
     @slider__item([
         'title' => 'Base Layout',
-        'sub_title' => 'This is a slightly longer title called subtitle',
         'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
         'containerColor' => 'white',
         'layout' => 'center',
-        'mobile_image' => 'https://picsum.photos/720/720',
-        'desktop_image' => 'https://picsum.photos/1080/720',
+        'image' => 'https://picsum.photos/1080/720',
     ])
     @endslider__item
 @endslider
