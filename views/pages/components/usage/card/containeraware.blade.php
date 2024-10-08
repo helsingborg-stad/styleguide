a41c49b3ceac52f1058502e6178ead458e8257e7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-10-08 09:06:24 +0200

    fix: container awareness adjustment on cards.

--- views/pages/components/usage/card/containeraware.blade.php
+++ views/pages/components/usage/card/containeraware.blade.php
@@ -1,15 +1,21 @@
+<div style="border: 2px solid; padding: 20px;  width: 70%; resize: horizontal; overflow: auto;">
+    @card([
+        'collapsible' => false,
+        'heading' => 'Heading',
+        'subHeading' => 'SubHeading', 
+        'content' => 'Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components. Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components. Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components. Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components.',
+        'image' => ['src' => 'https://www.w3schools.com/w3css/img_lights.jpg', 'alt' => 'ALT'],
+        'buttons' => [
+            ['type' => 'filled', 'color' => 'primary', 'text' => 'Lets go!'],
+            ['type' => 'filled', 'color' => 'primary', 'text' => 'Lets go!']
+        ],
+        'containerAware' => true
 
-@card([
-    'collapsible' => false,
-    'heading' => 'Heading',
-    'subHeading' => 'SubHeading', 
-    'content' => 'Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components. Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components. Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components. Atoms are the fundemental building blocks. They are rarely used just by them self but mostly used to build more advanced components.',
-    'image' => ['src' => 'https://www.w3schools.com/w3css/img_lights.jpg', 'alt' => 'ALT'],
-    'buttons' => [
-        ['type' => 'filled', 'color' => 'primary', 'text' => 'Lets go!'],
-        ['type' => 'filled', 'color' => 'primary', 'text' => 'Lets go!']
-    ],
-    'containerAware' => true
-
+    ])
+    @endcard
+</div>
+@typography([
+    'variant' => 'meta'
 ])
-@endcard
+    Drag the container above, to see the card's container aware feature.
+@endtypography
