52bc0ca5cba18b467c44886f483a999d2c232399
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-10-09 19:36:58 +0200

    fix: when a segment of type card is missing an image, make content with 100%

--- views/pages/components/usage/segment/card.blade.php
+++ views/pages/components/usage/segment/card.blade.php
@@ -11,6 +11,17 @@
         @endsegment
     </div>
     <div class="u-padding__y--4"></div>
+    <div class="o-grid-12">
+        @segment([
+            'title'             => 'Fusce Amet Parturient Etiam (no image)',
+            'content'           => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            'layout'            => 'card',
+            'containerAware'    => true,
+            'link'              => 'https://helsingborg.se'
+        ])
+        @endsegment
+    </div>
+    <div class="u-padding__y--4"></div>
     <div class="o-grid-4@md">
         @segment([
             'title'             => 'Fusce Amet Parturient Etiam',
@@ -38,11 +49,11 @@
             'title'             => 'Linked: Fusce Amet Parturient Etiam',
             'content'           => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
             'layout'            => 'card',
-            'image'             => 'https://picsum.photos/id/342/2896/1944',
             'textColor'         => 'dark',
             'textAlignment'     => 'center',
             'link'              => 'https://helsingborg.se',
-            'containerAware'    => true
+            'containerAware'    => true,
+            'hasPlaceholder'    => true
         ])
         @endsegment
     </div>
