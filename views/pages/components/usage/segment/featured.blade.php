ce8f5279d021254827e36acd56d0b570494d2df6
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-10-10 13:52:16 +0200

    fix: featured segment image position.

---
+++ views/pages/components/usage/segment/featured.blade.php
@@ -0,0 +1,38 @@
+@segment([
+    'title'         => 'Fusce Amet Parturient Etiam',
+    'content'       => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+    'layout'        => 'featured',
+    'image'         => 'https://picsum.photos/1080/720?e',
+    'textColor'     => 'dark',
+    'textAlignment' => 'center',
+    'classList' => [
+        'u-margin__top--4'
+    ]
+])
+@endsegment
+
+@segment([
+    'title'         => 'Fusce Amet Parturient Etiam',
+    'content'       => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+    'layout'        => 'featured',
+    'image'         => 'https://picsum.photos/1080/720?e',
+    'textColor'     => 'dark',
+    'textAlignment' => 'center',
+    'classList' => [
+        'u-margin__top--4'
+    ]
+])
+@endsegment
+
+@segment([
+    'title'         => 'Fusce Amet Parturient Etiam',
+    'content'       => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+    'layout'        => 'featured',
+    'image'         => 'https://picsum.photos/1080/720?e',
+    'textColor'     => 'dark',
+    'textAlignment' => 'center',
+    'classList' => [
+        'u-margin__top--4'
+    ]
+])
+@endsegment
