7edd4c3e2a06c3f69ea24ab89cdc75d8745ee583
Niclas <niclasn@outlook.com>
2024-11-29 15:53:31 +0100

    fix: corrected brand width

--- source/js/brand.ts
+++ source/js/brand.ts
@@ -1,31 +1,25 @@
-class Brand {
-    constructor(
-        private brand: HTMLElement,
-        private svg: SVGSVGElement,
-        private container: HTMLElement
-    ) {
-        this.setCorrectedViewBox();
-    }
-
-    private setCorrectedViewBox(): void {
-        const height = this.getCorrectedViewBoxHeight();
-        this.svg.setAttribute('viewBox', `0 0 ${height} 100`);
-    }
-
-    private getCorrectedViewBoxHeight(): number {
-        const containerRect = this.container.getBoundingClientRect();
-
-        return containerRect.width / (containerRect.height / 100);
-    }
-}
-
 export function initializeBrand(): void {
     document.querySelectorAll('.c-brand').forEach((brand) => {
         const svg = brand.querySelector('svg');
+        const brandText = brand.querySelector('.c-brand__text');
+        const brandFigure = brand.querySelector('figure');
         const container = brand.querySelector('.c-brand__container');
 
-        if (svg && container) {
-            new Brand(brand as HTMLElement, svg as SVGSVGElement, container as HTMLElement);
+        
+        
+        if (svg && brandText && brandFigure && container) {
+            const svgRect = svg.getBoundingClientRect();
+            const figureRect = brandFigure.getBoundingClientRect();
+            const textRect = brandText.getBoundingClientRect();
+
+            const svgScale = svg.viewBox.baseVal.width / svgRect.width;
+
+            const figureWidth = figureRect.width * svgScale;
+            const textWidth = textRect.width * svgScale;
+
+            const totalWidth = figureWidth + textWidth + 24;
+            
+            svg.setAttribute('viewBox', `0 0 ${totalWidth} 100`);
         }
     });
 }
