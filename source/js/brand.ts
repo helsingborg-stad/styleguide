88658c626ca7985df5880331bfe2096d552e917b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-11-29 16:48:56 +0100

    fix: allow undefined figure

--- source/js/brand.ts
+++ source/js/brand.ts
@@ -1,35 +1,34 @@
 class BrandViewBoxManager {
     private svg: SVGElement;
     private textElement: HTMLElement;
-    private figureElement: HTMLElement;
+    private figureElement: HTMLElement | null;
     private container: HTMLElement;
 
     constructor(private brand: HTMLElement) {
         this.container = this.brand.querySelector<HTMLElement>('.c-brand__container')!;
         this.svg = this.brand.querySelector<SVGElement>('.c-brand__viewbox')!;
         this.textElement = this.brand.querySelector<HTMLElement>('.c-brand__text')!;
-        this.figureElement = this.brand.querySelector<HTMLElement>('.c-brand__logotype')!;
+        this.figureElement = this.brand.querySelector<HTMLElement>('.c-brand__logotype'); // Optional
+
         this.updateViewBox();
     }
 
     private updateViewBox(): void {
-        // Get computed styles for the figure and text elements
-        const figureStyles = window.getComputedStyle(this.figureElement);
         const textStyles = window.getComputedStyle(this.textElement);
-
-        // Parse computed widths
-        const figureWidth = parseFloat(figureStyles.getPropertyValue('width'));
+        const containerStyles = window.getComputedStyle(this.container);
         const textWidth = parseFloat(textStyles.getPropertyValue('width'));
-        const gap = parseFloat(window.getComputedStyle(this.container).getPropertyValue('gap'));
+        const gap = parseFloat(containerStyles.getPropertyValue('gap'));
 
-        // Calculate the total width
-        const totalWidth = Math.ceil(figureWidth + textWidth + gap);
+        // Calculate figure width if the element exists
+        const figureWidth = this.figureElement 
+            ? parseFloat(window.getComputedStyle(this.figureElement).getPropertyValue('width')) 
+            : 0;
+
+        // Calculate the total width of the SVG
+        const totalWidth = !figureWidth ? Math.ceil(textWidth) : Math.ceil(figureWidth + textWidth + gap);
 
         // Update the viewBox of the SVG
-        this.svg.setAttribute(
-            'viewBox', 
-            `0 0 ${totalWidth} 100`
-        );
+        this.svg.setAttribute('viewBox', `0 0 ${totalWidth} 100`);
     }
 }
 
