f34d19360862314819b6bcb5eb004f5a00007a98
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-01-09 10:34:49 +0100

    feat: more examples of each type.

--- views/pages/components/usage/notice/actions.blade.php
+++ views/pages/components/usage/notice/actions.blade.php
@@ -1,48 +1,50 @@
-@notice([
-    'type' => 'info',
-    'message' => [
-        'text' => 'This message size is set to sm.',
-    ],
-    'action' => [
-        'text' => 'Go to home',
-        'url' => '#'
-    ],
-    'dismissable' => true,
-    'icon' => [
-        'name' => 'check',
-        'size' => 'md',
-        'color' => 'white'
-    ]
-])
-@endnotice
+@foreach (['info', 'success', 'danger', 'warning'] as $type)
+    @notice([
+        'type' => $type,
+        'message' => [
+            'text' => 'This message has an action button and may be dismissable.',
+        ],
+        'action' => [
+            'text' => 'Go to home',
+            'url' => '#'
+        ],
+        'dismissable' => true,
+        'icon' => [
+            'name' => 'check',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
 
-@notice([
-    'type' => 'info',
-    'message' => [
-        'text' => 'This message size is set to sm.',
-    ],
-    'dismissable' => true,
-    'icon' => [
-        'name' => 'check',
-        'size' => 'md',
-        'color' => 'white'
-    ]
-])
-@endnotice
+    @notice([
+        'type' => $type,
+        'message' => [
+            'text' => 'This message is simply dismissable.',
+        ],
+        'dismissable' => true,
+        'icon' => [
+            'name' => 'check',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
 
-@notice([
-    'type' => 'info',
-    'message' => [
-        'text' => 'This message size is set to sm.',
-    ],
-    'action' => [
-        'text' => 'Undo',
-        'url' => '#'
-    ],
-    'icon' => [
-        'name' => 'check',
-        'size' => 'md',
-        'color' => 'white'
-    ]
-])
-@endnotice
+    @notice([
+        'type' => $type,
+        'message' => [
+            'text' => 'This has a button to do a task but no dismiss action.',
+        ],
+        'action' => [
+            'text' => 'Undo',
+            'url' => '#'
+        ],
+        'icon' => [
+            'name' => 'check',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
+@endforeach
