6c930a40f839ac52d870ebb360d4d71e66249855
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-01-09 08:25:59 +0100

    feat: add examples for dismissable and actions in notices.

---
+++ views/pages/components/usage/notice/actions.blade.php
@@ -0,0 +1,48 @@
+@notice([
+    'type' => 'info',
+    'message' => [
+        'text' => 'This message size is set to sm.',
+    ],
+    'action' => [
+        'text' => 'Undo',
+        'url' => '#'
+    ],
+    'dismissable' => true,
+    'icon' => [
+        'name' => 'check',
+        'size' => 'md',
+        'color' => 'white'
+    ]
+])
+@endnotice
+
+@notice([
+    'type' => 'info',
+    'message' => [
+        'text' => 'This message size is set to sm.',
+    ],
+    'dismissable' => true,
+    'icon' => [
+        'name' => 'check',
+        'size' => 'md',
+        'color' => 'white'
+    ]
+])
+@endnotice
+
+@notice([
+    'type' => 'info',
+    'message' => [
+        'text' => 'This message size is set to sm.',
+    ],
+    'action' => [
+        'text' => 'Undo',
+        'url' => '#'
+    ],
+    'icon' => [
+        'name' => 'check',
+        'size' => 'md',
+        'color' => 'white'
+    ]
+])
+@endnotice
