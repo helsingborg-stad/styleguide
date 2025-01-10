f4017b48f98118f15515e0ce6c41ab0fcc5225d1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-01-10 11:29:59 +0100

    Toast example.

--- views/pages/components/usage/toast/basic.blade.php
+++ views/pages/components/usage/toast/basic.blade.php
@@ -1,1 +1,52 @@
 Here are examples!
+
+@toast(['position' => 'top-right'])
+    @notice([
+        'type' => 'info',
+        'message' => [
+            'text' => 'This message has an action button and may be dismissed.',
+        ],
+        'action' => [
+            'text' => 'Go to home',
+            'url' => '#'
+        ],
+        'dismissable' => 'immediate',
+        'icon' => [
+            'name' => 'check',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
+
+    @notice([
+        'type' => 'info',
+        'message' => [
+            'text' => 'This message may be dismissed.',
+        ],
+        'dismissable' => 'immediate',
+        'icon' => [
+            'name' => 'check',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
+
+    @notice([
+        'type' => 'info',
+        'message' => [
+            'text' => 'This has a button to do a task but no dismiss action.',
+        ],
+        'action' => [
+            'text' => 'Undo',
+            'url' => '#'
+        ],
+        'icon' => [
+            'name' => 'check',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
+@endtoast
