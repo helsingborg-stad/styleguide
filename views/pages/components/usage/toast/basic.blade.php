f33483f1245486e35bda6179501f95b070ec4ef1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-01-10 13:39:25 +0100

    fix: timeouts and examples.

--- views/pages/components/usage/toast/basic.blade.php
+++ views/pages/components/usage/toast/basic.blade.php
@@ -7,7 +7,7 @@ Here are examples!
         @toast__item([
             'type' => 'info',
             'message' => [
-                'text' => 'This message has an action button and may be dismissed.',
+                'text' => 'This message has an action button and may be dismissed, it will not dissapear automatically.',
             ],
             'action' => [
                 'text' => 'Go to home',
@@ -25,7 +25,7 @@ Here are examples!
         @toast__item([
             'type' => 'info',
             'message' => [
-                'text' => 'This message may be dismissed.',
+                'text' => 'This message may be dismissed, it will not dissapear automatically.',
             ],
             'dismissable' => 'immediate',
             'icon' => [
@@ -39,11 +39,25 @@ Here are examples!
         @toast__item([
             'type' => 'info',
             'message' => [
-                'text' => 'This has a button to do a task but no dismiss action.',
+                'text' => 'This has a button to do a task but no dismiss action and will have double the time for hidden.',
             ],
             'action' => [
                 'text' => 'Undo',
                 'url' => '#'
+            ],
+            'icon' => [
+                'name' => 'check',
+                'size' => 'md',
+                'color' => 'white'
+            ]
+        ])
+        @endtoast__item
+
+
+        @toast__item([
+            'type' => 'info',
+            'message' => [
+                'text' => 'This neither has a button, or a dismiss and will be hidden first.',
             ],
             'icon' => [
                 'name' => 'check',
