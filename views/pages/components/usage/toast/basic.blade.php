2eb85932250f69a790f5fd872aa3951907142af3
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-01-10 12:53:41 +0100

    Do not autohide items that has a dismiss action.

--- views/pages/components/usage/toast/basic.blade.php
+++ views/pages/components/usage/toast/basic.blade.php
@@ -1,52 +1,57 @@
 Here are examples!
 
-@toast(['position' => 'top-right'])
-    @notice([
-        'type' => 'info',
-        'message' => [
-            'text' => 'This message has an action button and may be dismissed.',
-        ],
-        'action' => [
-            'text' => 'Go to home',
-            'url' => '#'
-        ],
-        'dismissable' => 'immediate',
-        'icon' => [
-            'name' => 'check',
-            'size' => 'md',
-            'color' => 'white'
-        ]
-    ])
-    @endnotice
 
-    @notice([
-        'type' => 'info',
-        'message' => [
-            'text' => 'This message may be dismissed.',
-        ],
-        'dismissable' => 'immediate',
-        'icon' => [
-            'name' => 'check',
-            'size' => 'md',
-            'color' => 'white'
-        ]
-    ])
-    @endnotice
+<div style="height: 400px; overflow: auto; background-color: rgba(0,0,0,.1); position: relative;">
+    @toast(['position' => 'top-right', 'attributeList' => ['style' => 'position: absolute;']])
+       
+        @toast__item([
+            'type' => 'info',
+            'message' => [
+                'text' => 'This message has an action button and may be dismissed.',
+            ],
+            'action' => [
+                'text' => 'Go to home',
+                'url' => '#'
+            ],
+            'dismissable' => 'immediate',
+            'icon' => [
+                'name' => 'check',
+                'size' => 'md',
+                'color' => 'white'
+            ]
+        ])
+        @endtoast__item
 
-    @notice([
-        'type' => 'info',
-        'message' => [
-            'text' => 'This has a button to do a task but no dismiss action.',
-        ],
-        'action' => [
-            'text' => 'Undo',
-            'url' => '#'
-        ],
-        'icon' => [
-            'name' => 'check',
-            'size' => 'md',
-            'color' => 'white'
-        ]
-    ])
-    @endnotice
-@endtoast
+        @toast__item([
+            'type' => 'info',
+            'message' => [
+                'text' => 'This message may be dismissed.',
+            ],
+            'dismissable' => 'immediate',
+            'icon' => [
+                'name' => 'check',
+                'size' => 'md',
+                'color' => 'white'
+            ]
+        ])
+        @endtoast__item
+
+        @toast__item([
+            'type' => 'info',
+            'message' => [
+                'text' => 'This has a button to do a task but no dismiss action.',
+            ],
+            'action' => [
+                'text' => 'Undo',
+                'url' => '#'
+            ],
+            'icon' => [
+                'name' => 'check',
+                'size' => 'md',
+                'color' => 'white'
+            ]
+        ])
+        @endtoast__item
+    @endtoast
+
+</div>
