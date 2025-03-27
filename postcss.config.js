a5001bf140b4e9a77d0d99a2fae752e60a604b2b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-03-27 14:25:05 +0100

    fix: remove autprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')({
-            overrideBrowserslist: ['> 1%', 'last 4 versions']
-        }),
         require('cssnano')
     ]
 };
