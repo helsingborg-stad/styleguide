0b3eb34837d5dd718e64165ad14d2fee5833f129
NiclasNorin <niclasn@outlook.com>
2025-04-02 16:13:25 +0200

    docs: iconSection + iconSection__item

--- views/pages/components/usage/iconSection/iconSection.blade.php
+++
@@ -1,9 +0,0 @@
-@iconSection([
-    'icon' => [
-        'icon' => 'location_on',
-        'size' => 'md'
-    ]
-])
-    <h2>Location</h2>
-    <p>1234 Street Name, City Name, Country Name</p>
-@endiconSection
