647e0842136bca0eb15c4925bad0a318f238f2f0
NiclasNorin <niclasn@outlook.com>
2025-08-13 10:48:43 +0200

    refactor: adding classlist to max file size to show user error

--- source/js/form/fileinput/maxFileSize.ts
+++ source/js/form/fileinput/maxFileSize.ts
@@ -13,9 +13,11 @@ export function MaxFileSize(
     const maxFileSizeBytes = parseFloat(maxFileSize) * 1024 * 1024;
 
     controller.onFileAdded((file) => {
+        dropzone.classList.remove('file-size-error');
         if (file.size > maxFileSizeBytes) {
             controller.removeFileFromList(file);
             noticeHandler.showNotice([file]);
+            dropzone.classList.add('file-size-error');
         }
     });
 }
