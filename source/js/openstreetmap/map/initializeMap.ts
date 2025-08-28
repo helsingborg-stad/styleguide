9e242b3156e554d857fd97b8d28e25b773df9c88
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-08-28 10:30:36 +0200

    fix: use our own version of leaflet markercluster

--- source/js/openstreetmap/map/initializeMap.ts
+++ source/js/openstreetmap/map/initializeMap.ts
@@ -1,76 +1,28 @@
-import L, { Map as LeafletMap } from 'leaflet';
-import 'leaflet.markercluster';
-
-// Only patch if it doesn’t exist
-if (!(L as any).markerClusterGroup) {
-  (L as any).MarkerClusterGroup = class {
-    private _markers: Marker[] = [];
-    private _map?: LeafletMap;
-    private _options: any;
-
-    constructor(options?: any) {
-      this._options = options || {};
-    }
-
-    addLayer(marker: Marker) {
-      this._markers.push(marker);
-      if (this._map) marker.addTo(this._map);
-      return this;
-    }
-
-    addLayers(markers: Marker[]) {
-      markers.forEach(m => this.addLayer(m));
-      return this;
-    }
-
-    removeLayer(marker: Marker) {
-      const index = this._markers.indexOf(marker);
-      if (index !== -1) this._markers.splice(index, 1);
-      if (this._map) this._map.removeLayer(marker);
-      return this;
-    }
-
-    clearLayers() {
-      this._markers.forEach(m => this._map?.removeLayer(m));
-      this._markers = [];
-      return this;
-    }
-
-    addTo(map: LeafletMap) {
-      this._map = map;
-      this._markers.forEach(m => m.addTo(map));
-      return this;
-    }
-
-    getClusters() {
-      return [this._markers];
-    }
-  };
-
-  (L as any).markerClusterGroup = function (options?: any) {
-    return new (L as any).MarkerClusterGroup(options);
-  };
-}
-
+import L, { Map as LeafletMap, MarkerClusterGroup } from 'leaflet';
+import { addMarkerClusterToLeaflet } from 'leaflet.markercluster';
 class InitializeOsm {
-    constructor(private container: HTMLElement) {}
+    private leaflet: typeof L;
 
-    public create(): [LeafletMap, L.MarkerClusterGroup] {
+    constructor(private container: HTMLElement) {
+        this.leaflet = addMarkerClusterToLeaflet(L);
+    }
+
+    public create(): [LeafletMap, MarkerClusterGroup] {
         const id = this.container.getAttribute('data-js-map-id') ?? "";
 
-        const map = L.map(`openstreetmap__map-${id}`, {
+        const map = this.leaflet.map(`openstreetmap__map-${id}`, {
             scrollWheelZoom: false,
             keyboard: false,
             attributionControl: false,
         });
 
-        const markers = L.markerClusterGroup({
+        const markers = this.leaflet.markerClusterGroup({
             showCoverageOnHover: false,
             removeOutsideVisibleBounds: true,
             maxClusterRadius: 50,
         });
 
-        return [map, markers];
+        return [map as LeafletMap, markers as MarkerClusterGroup];
     }
 }
 
