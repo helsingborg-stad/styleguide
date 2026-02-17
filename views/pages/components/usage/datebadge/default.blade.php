49b4dcf1829b349b889b1a7c21c2dd6469c547b7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-02-17 20:13:21 +0100

    feat: token system in datebadge

--- views/pages/components/usage/datebadge/default.blade.php
+++ views/pages/components/usage/datebadge/default.blade.php
@@ -1,4 +1,16 @@
-@datebadge([
-    'date' => date("Y-m-d")
-])
-@enddatebadge
+<div style="display: flex; gap: 16px;">
+    @datebadge([
+        'date' => date("Y-m-d")
+    ])
+    @enddatebadge
+    @datebadge([
+        'date' => date("Y-m-d"),
+        'color' => 'primary',
+    ])
+    @enddatebadge
+    @datebadge([
+        'date' => date("Y-m-d"),
+        'color' => 'secondary',
+    ])
+    @enddatebadge
+</div>
