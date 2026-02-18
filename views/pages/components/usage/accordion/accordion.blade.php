0e1a6de0b98c0ab1becf17ad9aaef4b612371b2b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-02-18 16:36:01 +0100

    feat: initial commit accordion.

--- views/pages/components/usage/accordion/accordion.blade.php
+++ views/pages/components/usage/accordion/accordion.blade.php
@@ -1,11 +1,13 @@
-@accordion(
-    [
-        'list'=> [
-            ['heading' => "Your heading", 'content' => "Lorem ipsum dolor sit amet."],
-            ['heading' => "Your heading", 'content' => "Lorem ipsum dolor sit amet."],
-            ['heading' => "Your heading", 'content' => "Lorem ipsum dolor sit amet."],
-            ['heading' => "Your heading", 'content' => "Lorem ipsum dolor sit amet."]
+@paper(['padding' => 0]) 
+    @accordion(
+        [
+            'list'=> [
+                ['heading' => "Your heading", 'content' => "Lorem ipsum dolor sit amet."],
+                ['heading' => "Your heading", 'content' => "Lorem ipsum dolor sit amet."],
+                ['heading' => "Your heading", 'content' => "Lorem ipsum dolor sit amet."],
+                ['heading' => "Your heading", 'content' => "Lorem ipsum dolor sit amet."]
+            ]
         ]
-    ]
-)
-@endaccordion
+    )
+    @endaccordion
+@endpaper
