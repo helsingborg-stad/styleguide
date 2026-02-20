579dfe7a1a0424630f1d2d5586dd204fd2f6771f
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-02-20 11:14:30 +0100

    feat: add example for pill shaped buttons

--- views/pages/components/usage/button/pill.blade.php
+++ views/pages/components/usage/button/pill.blade.php
@@ -1,23 +1,51 @@
-@button([
-    'text' => 'Primary (href)',
-    'color' => 'primary',
-    'style' => 'filled',
-    'shape' => 'pill'
-]) 
-@endbutton
+@foreach(['basic', 'outlined', 'filled'] as $style)
+<div style="display: flex; gap: 1rem; flex-wrap: wrap; align-items: center; margin-bottom: 2rem;">
+    @button([
+        'text' => 'Primary (href)',
+        'color' => 'primary',
+        'style' => $style,
+        'href' => 'https://helsingborg.se',
+        'shape' => 'pill',
+        'classList' => [
+            'u-margin--0'
+        ]
+    ]) 
+    @endbutton
 
-@button([
-    'text' => 'Secondary',
-    'color' => 'secondary',
-    'style' => 'filled',
-    'shape' => 'pill'
-])
-@endbutton
+    @button([
+        'text' => 'Secondary',
+        'color' => 'secondary',
+        'style' => $style,
+        'shape' => 'pill',
+        'classList' => [
+            'u-margin--0'
+        ]
+    ])
+    @endbutton
 
-@button([
-    'text' => 'Default',
-    'color' => 'default',
-    'style' => 'filled',
-    'shape' => 'pill'
-])
-@endbutton
+    @button([
+        'text' => 'Default',
+        'color' => 'default',
+        'style' => $style,
+        'shape' => 'pill',
+        'classList' => [
+            'u-margin--0'
+        ]
+    ])
+    @endbutton
+
+
+    @button([
+        'color' => 'primary',
+        'style' => $style,
+        'href' => 'https://helsingborg.se',
+        'icon' => 'home',
+        'reversePositions' => 'true',
+        'shape' => 'pill',
+        'classList' => [
+            'u-margin--0'
+        ]
+    ])
+    @endbutton
+    </div>
+@endforeach
