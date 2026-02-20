3338ab8911bac8b362636895866d4abae2953777
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-02-20 10:46:30 +0100

    fix: add example of pill-shape.

---
+++ views/pages/components/usage/button/pill.blade.php
@@ -0,0 +1,23 @@
+@button([
+    'text' => 'Primary (href)',
+    'color' => 'primary',
+    'style' => 'filled',
+    'shape' => 'pill'
+]) 
+@endbutton
+
+@button([
+    'text' => 'Secondary',
+    'color' => 'secondary',
+    'style' => 'filled',
+    'shape' => 'pill'
+])
+@endbutton
+
+@button([
+    'text' => 'Default',
+    'color' => 'default',
+    'style' => 'filled',
+    'shape' => 'pill'
+])
+@endbutton
