d48b1724577d169acc0d4f31204b0966f54d12e5
Thor Brink <thor.brink@helsingborg.se>
2026-02-24 11:32:33 +0000

    feat: implement design tokens for signature

---
+++ views/pages/components/usage/signature/published-only.blade.php
@@ -0,0 +1,5 @@
+@signature([
+    'author' => 'Frej Karlsson',
+    'published' => '2020-01-01',
+])
+@endsignature
---
+++ views/pages/components/usage/signature/with-avatar.blade.php
@@ -0,0 +1,7 @@
+@signature([
+    'author' => 'Frej Karlsson',
+    'authorRole' => 'Author',
+    'avatar' => 'https://picsum.photos/id/64/200/200',
+    'published' => '2020-01-01',
+])
+@endsignature
