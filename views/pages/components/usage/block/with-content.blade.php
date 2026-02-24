ea16c0e6424fb40a4b1957925d031fa347422e22
Thor Brink <thor.brink@helsingborg.se>
2026-02-24 13:25:00 +0000

    feat: implement design tokens in block

---
+++ views/pages/components/usage/block/with-content.blade.php
@@ -0,0 +1,44 @@
+<div class="o-grid">
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg o-grid-3@xl">
+        @block([
+            'heading' => 'Justo Nibh Condimentum',
+            'meta' => 'Etiam Tristique',
+            'secondaryMeta' => 'Magna Nullam',
+            'content' => 'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+            'image' => [
+                'src' => 'https://picsum.photos/id/20/680/510',
+                'alt' => 'ALT',
+                'backgroundColor' => 'secondary',
+            ],
+            'link' => '#',
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg o-grid-3@xl">
+        @block([
+            'heading' => 'Nulla vitae elit libero',
+            'meta' => 'Porta Risus',
+            'secondaryMeta' => 'Venenatis Adipiscing',
+            'content' => 'Aenean lacinia bibendum nulla sed consectetur.',
+            'image' => [
+                'src' => 'https://picsum.photos/id/21/680/510',
+                'alt' => 'ALT',
+                'backgroundColor' => 'secondary',
+            ],
+        ])
+        @endblock
+    </div>
+    <div class="o-grid-12@xs o-grid-6@sm o-grid-4@md o-grid-3@lg o-grid-3@xl">
+        @block([
+            'heading' => 'Nullam quis risus eget',
+            'meta' => 'Commodo Fringilla',
+            'content' => 'Cras mattis consectetur purus sit amet fermentum.',
+            'image' => [
+                'src' => 'https://picsum.photos/id/22/680/510',
+                'alt' => 'ALT',
+                'backgroundColor' => 'secondary',
+            ],
+        ])
+        @endblock
+    </div>
+</div>
