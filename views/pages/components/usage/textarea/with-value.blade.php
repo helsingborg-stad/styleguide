6b76de9a23388902d9459f745d334d6fc23c48ff
Thor Brink <thor.brink@helsingborg.se>
2026-02-24 12:25:56 +0000

    fix: remove obsolete styles for textarea

---
+++ views/pages/components/usage/textarea/with-value.blade.php
@@ -0,0 +1,8 @@
+@field([
+    'type' => 'text',
+    'name' => 'notes',
+    'label' => 'Notes',
+    'multiline' => true,
+    'value' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+])
+@endfield
