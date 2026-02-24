6e26f625d1f9d56156b88fdda959f6f65ec5418d
Thor Brink <thor.brink@helsingborg.se>
2026-02-24 15:35:41 +0000

    docs: add more footer examples

---
+++ views/pages/components/usage/footer/with-subfooter.blade.php
@@ -0,0 +1,19 @@
+@footer([
+    'slotOnly' => true,
+    'logotype' => '',
+    'subfooter' => [
+        'flexDirection' => 'row',
+        'alignment' => 'center',
+        'content' => [
+            ['title' => 'Phone', 'content' => '042-10 50 00', 'link' => 'tel:0421050000'],
+            ['title' => 'Email', 'content' => 'info@example.se', 'link' => 'mailto:info@example.se'],
+            ['title' => 'Visit us', 'content' => 'Stortorget 1, 251 89 Helsingborg', 'link' => false],
+        ]
+    ]
+])
+    <div class="o-container">
+        <div class="o-grid-12">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    </div>
+@endfooter
