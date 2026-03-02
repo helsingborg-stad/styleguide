8028d1ab6a61cf6c2ae2047a1f9ef36281facb09
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-03-02 16:59:52 +0100

    feat: add breadcrumb, remove reference style

--- views/pages/component.blade.php
+++ views/pages/component.blade.php
@@ -1,6 +1,16 @@
 @extends('layout.containers.doc')
 
 @section('doc-content')
+    @breadcrumb([
+        'classList' => ['u-margin__bottom--3'],
+        'list' => [
+            ['href' => '/', 'label' => 'Home'],
+            ['href' => '/components', 'label' => 'Components'],
+            ['label' => $headline ?? 'Component']
+        ]
+    ])
+    @endbreadcrumb
+
     @typography([
         'element' => 'h1',
         'variant' => 'h1',
