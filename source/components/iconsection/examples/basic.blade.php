c163cc298581ef65f4e73478cc626d1c1c341429
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-03-03 10:54:00 +0100

    feat: add iconsection docs.

---
+++ source/components/iconsection/examples/basic.blade.php
@@ -0,0 +1,28 @@
+@iconSection()
+    @iconSection__item([
+        'icon' => [
+            'icon' => 'home'
+        ]
+    ])
+        <h4>Housing</h4>
+        <p>Find services and information related to housing and homes.</p>
+    @endiconSection__item
+
+    @iconSection__item([
+        'icon' => [
+            'icon' => 'school'
+        ]
+    ])
+        <h4>Education</h4>
+        <p>Explore schools, programs, and support for students.</p>
+    @endiconSection__item
+
+    @iconSection__item([
+        'icon' => [
+            'icon' => 'directions_bus'
+        ]
+    ])
+        <h4>Transport</h4>
+        <p>Get updates and details about local public transport.</p>
+    @endiconSection__item
+@endiconSection
