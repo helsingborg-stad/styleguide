3958158bac5b711366ca05e3194082a70b7e6497
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-03-04 15:32:28 +0100

    fix: shadow example

--- views/pages/utilities/shadows.blade.php
+++ views/pages/utilities/shadows.blade.php
@@ -1,6 +1,6 @@
-@extends('layout.containers.doc')
+@extends("layout.containers.doc")
 
-@section('doc-content')
+@section("doc-content")
 <article>
 
     
@@ -8,16 +8,14 @@
         ###Box shadow
     @endmarkdown
     
-    @utility_doc(['viewDoc' => ['type' => 'utility', 'root' => 'shadows', 'config' => 'box-shadow']])
+    @utility_doc(["viewDoc" => ["type" => "utility", "root" => "shadows", "config" => "box-shadow"]])
         <div class="d-shadows">
-            <div class='u-box-shadow--1 u-padding--2 u-rounded' >Level 1</div>
-            <div class='u-box-shadow--2 u-padding--2 u-rounded' >Level 2</div>
-            <div class='u-box-shadow--3 u-padding--2 u-rounded' >Level 3</div>
-            <div class='u-box-shadow--4 u-padding--2 u-rounded' >Level 4</div>
-            <div class='u-box-shadow--5 u-padding--2 u-rounded' >Level 5</div>
+            <div class="u-box-shadow--1 u-padding--2 u-rounded" style="background-color: #fff;" >Level 1</div>
+            <div class="u-box-shadow--2 u-padding--2 u-rounded" style="background-color: #fff;" >Level 2</div>
+            <div class="u-box-shadow--3 u-padding--2 u-rounded" style="background-color: #fff;" >Level 3</div>
+            <div class="u-box-shadow--4 u-padding--2 u-rounded" style="background-color: #fff;" >Level 4</div>
+            <div class="u-box-shadow--5 u-padding--2 u-rounded" style="background-color: #fff;" >Level 5</div>
         </div>
-        
     @endutility_doc 
-
 </article>
 @stop
