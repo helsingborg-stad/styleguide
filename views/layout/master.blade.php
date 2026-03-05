aaab9eb84f0bd1e6188c2eb0a1e8cecd2098efc3
Thor Brink <thor.brink@helsingborg.se>
2026-03-05 12:13:01 +0000

    fix(icon): layering

--- views/layout/master.blade.php
+++ views/layout/master.blade.php
@@ -22,7 +22,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <style>
+        {!! "@import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined') layer(components);" !!}
+    </style>
 
     <noscript>
         <style>
