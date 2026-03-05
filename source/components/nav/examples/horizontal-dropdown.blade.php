149744cf9b27e8ad09feb4256b86e47a25833cbd
Thor Brink <thor.brink@helsingborg.se>
2026-03-05 10:42:55 +0000

    fix: set new layer order

--- source/components/nav/examples/horizontal-dropdown.blade.php
+++ source/components/nav/examples/horizontal-dropdown.blade.php
@@ -1,41 +1,38 @@
-<div class="u-color__bg--lighter">
-    @nav([
-        'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
-        'classList' => ['u-position--relative'],
-        'childItemsUrl' => '/',
-        'direction' => 'horizontal',
-        'includeToggle' => true,
-        'allowStyle' => true,
-        'height' => 'sm'
-    ])
-    @endnav
-</div>
+@nav([
+    'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
+    'classList' => ['u-position--relative'],
+    'childItemsUrl' => '/',
+    'direction' => 'horizontal',
+    'includeToggle' => true,
+    'allowStyle' => true,
+    'height' => 'sm'
+])
+@endnav
+
 <div style="height: 100px;"></div><!-- Padder -->
 
-<div class="u-color__bg--lighter">
-    @nav([
-        'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
-        'classList' => ['u-position--relative'],
-        'childItemsUrl' => '/',
-        'direction' => 'horizontal',
-        'includeToggle' => true,
-        'allowStyle' => true,
-        'height' => 'md'
-    ])
-    @endnav
-</div>
+@nav([
+    'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
+    'classList' => ['u-position--relative'],
+    'childItemsUrl' => '/',
+    'direction' => 'horizontal',
+    'includeToggle' => true,
+    'allowStyle' => true,
+    'height' => 'md'
+])
+@endnav
+
 <div style="height: 100px;"></div><!-- Padder -->
 
-<div class="u-color__bg--lighter">
-    @nav([
-        'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
-        'classList' => ['u-position--relative'],
-        'childItemsUrl' => '/',
-        'direction' => 'horizontal',
-        'includeToggle' => true,
-        'allowStyle' => true,
-        'height' => 'lg'
-    ])
-    @endnav
-</div>
+@nav([
+    'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
+    'classList' => ['u-position--relative'],
+    'childItemsUrl' => '/',
+    'direction' => 'horizontal',
+    'includeToggle' => true,
+    'allowStyle' => true,
+    'height' => 'lg'
+])
+@endnav
+
 <div style="height: 100px;"></div><!-- Padder -->
--- source/components/nav/examples/horizontal-extended-dropdown.blade.php
+++ source/components/nav/examples/horizontal-extended-dropdown.blade.php
@@ -1,12 +1,10 @@
-<div class="u-color__bg--lighter" style="position: relative;">
-    @nav([
-        'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
-        'classList' => ['u-position--relative'],
-        'direction' => 'horizontal',
-        'includeToggle' => true,
-        'isExtendedDropdown' => true,
-        'height' => 'md',
-    ])
-    @endnav
-</div>
+@nav([
+    'items' => \HbgStyleGuide\Navigation::getMockedMultilevel(),
+    'classList' => ['u-position--relative'],
+    'direction' => 'horizontal',
+    'includeToggle' => true,
+    'isExtendedDropdown' => true,
+    'height' => 'md',
+])
+@endnav
 <div style="height: 300px;"></div><!-- Padder -->
