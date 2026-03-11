391bd35602b6115d7a21fb53cf840b685ea23c37
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-03-11 13:30:24 +0100

    fix: allow combo borders

--- source/utilities/border/examples/borders-sides.blade.php
+++ source/utilities/border/examples/borders-sides.blade.php
@@ -2,5 +2,6 @@
     <div class="u-border__top"></div>
     <div class="u-border__right"></div>
     <div class="u-border__left"></div>
+    <div class="u-border__left u-border__right"></div>
     <div class="u-border__bottom"></div>
 </div>
