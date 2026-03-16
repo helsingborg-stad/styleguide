b43bda8df30bf34c28b6429248aca35d5beda867
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-03-16 15:39:10 +0100

    fix: prevent stretch from overflowing its container.

--- source/components/segment/examples/stretch.blade.php
+++ source/components/segment/examples/stretch.blade.php
@@ -1,8 +1,10 @@
-@segment([
-    'title'     => 'Fusce Amet Parturient Etiam',
-    'content'   => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
-    'layout'    => 'split',
-    'image'     => 'https://picsum.photos/id/29/1080/720',
-    'stretch'   => true,
-])
-@endsegment
+<div style="overflow: hidden;">
+    @segment([
+        'title'     => 'Fusce Amet Parturient Etiam',
+        'content'   => 'Overflow hidden is applied on container. Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+        'layout'    => 'split',
+        'image'     => 'https://picsum.photos/id/29/1080/720',
+        'stretch'   => true,
+    ])
+    @endsegment
+</div>
