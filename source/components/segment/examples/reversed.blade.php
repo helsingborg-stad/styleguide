40beec5dbd9d120fa6501f1bd207ac66b6a9281b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-03-17 16:12:12 +0100

    fix: margins, fontsize and alignment for segments.

--- source/components/segment/examples/reversed.blade.php
+++ source/components/segment/examples/reversed.blade.php
@@ -3,9 +3,8 @@
     'content'       => 'Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
     'layout'        => 'split',
     'image'         => 'https://picsum.photos/1080/720?d',
-    'height'        => 'full-screen',
     'textColor'     => 'dark',
-    'textAlignment' => 'bottom',
+    'textAlignment' => 'center',
     'reverseColumns' => true
 ])
 
