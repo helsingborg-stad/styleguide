7a2a1750d7cdc88923a3d3bf01005fd30316d317
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-03-19 18:07:25 +0100

    fix: multiple stacked fabs

---
+++ source/components/fab/examples/multi.blade.php
@@ -0,0 +1,29 @@
+@fab([
+    'position' => 'bottom-right',
+    'heading' => "Vestibulum id ligula porta felis euismod semper",
+    'button' => [
+        'icon' => 'home',
+        'size' => 'md',
+        'color' => 'primary',
+        'text' => '',
+        'reversePositions' => true,
+    ],
+    'classList' => []
+])
+
+    Hello! I'm home!
+@endfab
+
+@fab([
+    'position' => 'bottom-right',
+    'heading' => "Vestibulum id ligula porta felis euismod semper",
+    'button' => [
+        'icon' => 'apps',
+        'size' => 'md',
+        'color' => 'primary',
+        'text' => '',
+    ],
+    'classList' => []
+])
+Hello I'am a FAB!
+@endfab
