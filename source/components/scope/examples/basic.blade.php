753580c6b6668a493936b0c18863b97c0351ebe5
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-03-30 12:45:08 +0200

    feat: scope examples.

---
+++ source/components/scope/examples/basic.blade.php
@@ -0,0 +1,58 @@
+@scope(['name' => 'a-unique-scope-id'])
+    @button([
+        'text' => 'Scope 1 - Button 1',
+        'color' => 'primary',
+        'style' => 'filled',
+        'href' => 'https://getmunicipio.com'
+
+    ])
+    @endbutton
+
+    @button([
+        'text' => 'Scope 1 - Button 2',
+        'color' => 'primary',
+        'style' => 'filled',
+        'href' => 'https://getmunicipio.com'
+
+    ])
+    @endbutton
+@endscope
+
+@scope(['name' => 'a-unique-scope-id-2'])
+    @button([
+        'text' => 'Scope 2 - Button 1',
+        'color' => 'primary',
+        'style' => 'filled',
+        'href' => 'https://getmunicipio.com'
+
+    ])
+    @endbutton
+
+    @button([
+        'text' => 'Scope 2 - Button 2',
+        'color' => 'primary',
+        'style' => 'filled',
+        'href' => 'https://getmunicipio.com'
+
+    ])
+    @endbutton
+@endscope
+
+
+@button([
+    'text' => 'Unscoped - Button 1',
+    'color' => 'primary',
+    'style' => 'filled',
+    'href' => 'https://getmunicipio.com'
+
+])
+@endbutton
+
+@button([
+    'text' => 'Unscoped - Button 2',
+    'color' => 'primary',
+    'style' => 'filled',
+    'href' => 'https://getmunicipio.com'
+
+])
+@endbutton
