cc1776d0227f9a5c3889053324546d0e1572f64a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-04-07 15:39:22 +0200

    feat: add max width and different sizes on fab panels.

---
+++ source/components/fab/examples/width-sm.blade.php
@@ -0,0 +1,14 @@
+@fab([
+    'position' => 'bottom-right',
+    'heading' => 'Small width modifier',
+    'button' => [
+        'icon' => 'apps',
+        'size' => 'md',
+        'color' => 'primary',
+        'text' => 'Open menu',
+        'reversePositions' => true,
+    ],
+    'classList' => ['c-fab--width-sm']
+])
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+@endfab
