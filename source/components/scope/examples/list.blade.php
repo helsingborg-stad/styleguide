1b1f8ac5d2964cbc8718b85773c7d42e6de584ce
Thor Brink <thor.brink@helsingborg.se>
2026-04-17 04:58:19 +0000

    fix(Scope): update examples and component-library

--- source/components/scope/examples/list.blade.php
+++ source/components/scope/examples/list.blade.php
@@ -1,4 +1,4 @@
-@scope(['name' => ['scope-1', 'scope-2'], 'classList' => ['u-margin__bottom--2']])
+@scope(['name' => ['scope-1', 'scope-2']])
     @button([
         'text' => 'Scope 1 + Scope 2 - Button',
         'color' => 'primary',
