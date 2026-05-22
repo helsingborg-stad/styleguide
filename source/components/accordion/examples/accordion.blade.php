e1b3d0653a7258f50ae6311df4e6bc3c0b15294f
NiclasNorin <niclasn@outlook.com>
2026-05-22 10:09:02 +0200

    feat: animation

--- source/components/accordion/examples/accordion.blade.php
+++ source/components/accordion/examples/accordion.blade.php
@@ -11,3 +11,15 @@
     )
     @endaccordion
 @endpaper
+
+@element(['classList' => ['c-test']])
+<details>
+  <summary>Title</summary>
+  <div class="content">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+  </div>
+</details>
+@endelement
