bdd8dbc6a871c755efd871c66d84c9e82a655539
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-05-27 16:42:00 +0200

    fix: autolint.

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,10 @@
 export {};
 
+declare module '*?inline' {
+	const content: string;
+	export default content;
+}
+
 declare module '*.css?inline' {
 	const content: string;
 	export default content;
