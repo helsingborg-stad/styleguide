c0de0b17d1809ae2fb6db96f208fe123702497ef
Thor Brink <thor.brink@helsingborg.se>
2026-06-08 08:07:42 +0000

    refactor: buttons styles and examples

---
+++ source/components/button/examples/fullWidth.blade.php
@@ -0,0 +1,26 @@
+@button([
+    'text' => 'Primary full width',
+    'color' => 'primary',
+    'style' => 'filled',
+    'fullWidth' => true,
+    'href' => 'https://getmunicipio.com'
+
+])
+@endbutton
+<br>
+@button([
+    'text' => 'Secondary full width',
+    'color' => 'secondary',
+    'style' => 'filled',
+    'fullWidth' => true
+
+])
+@endbutton
+<br>
+@button([
+    'text' => 'Default full width',
+    'color' => 'default',
+    'style' => 'filled',
+    'fullWidth' => true
+])
+@endbutton
