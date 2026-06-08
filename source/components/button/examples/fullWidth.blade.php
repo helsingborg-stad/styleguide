a691d527fe912b06efd8fd1d9971726d89f7f09e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-06-08 16:12:16 +0200

    fix: center labels in fw buttons (enshure icon placement)

--- source/components/button/examples/fullWidth.blade.php
+++ source/components/button/examples/fullWidth.blade.php
@@ -24,3 +24,34 @@
     'fullWidth' => true
 ])
 @endbutton
+<br>
+@button([
+    'text' => 'Primary full width (ico)',
+    'color' => 'primary',
+    'style' => 'filled',
+    'fullWidth' => true,
+    'href' => 'https://getmunicipio.com',
+    'icon' => 'link',
+    'reversePositions' => true
+])
+@endbutton
+<br>
+@button([
+    'text' => 'Secondary full width (ico)',
+    'color' => 'secondary',
+    'style' => 'filled',
+    'fullWidth' => true,
+    'icon' => 'link',
+    'reversePositions' => true
+])
+@endbutton
+<br>
+@button([
+    'text' => 'Default full width (ico)',
+    'color' => 'default',
+    'style' => 'filled',
+    'fullWidth' => true,
+    'icon' => 'link',
+    'reversePositions' => true
+])
+@endbutton
