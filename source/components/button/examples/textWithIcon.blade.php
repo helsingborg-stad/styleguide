40aa7e131c58740dfafc7d67ba4278918bede734
Thor Brink <thor.brink@helsingborg.se>
2026-06-09 13:13:45 +0200

    fix: button sizes and icons

--- source/components/button/examples/textWithIcon.blade.php
+++ source/components/button/examples/textWithIcon.blade.php
@@ -1,14 +1,26 @@
-@button([
-    'icon' => 'close',
-    'reversePositions' => true,
-    'text' => 'Reversed',
-    'style' => 'filled'
-])
-@endbutton
+@foreach (['basic', 'filled', 'outlined'] as $style)
 
-@button([
-    'icon' => 'close',
-    'text' => 'Not reversed',
-    'style' => 'filled'
-])
-@endbutton
+    @foreach (['sm', 'md', 'lg'] as $size)
+        
+            @button([
+                'icon' => 'close',
+                'reversePositions' => true,
+                'text' => 'Reversed',
+                'style' => $style,
+                'size' => $size
+            ])
+            @endbutton
+
+            @button([
+                'icon' => 'close',
+                'text' => 'Not reversed',
+                'style' => $style,
+                'size' => $size
+            ])
+            @endbutton
+
+    @endforeach
+
+    <br>
+
+@endforeach
