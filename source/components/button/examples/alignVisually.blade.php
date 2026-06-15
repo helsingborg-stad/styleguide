789b516285ea9c351fadd88d57ea0ee729609236
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-06-15 11:09:50 +0200

    feat: add visual aligner in basic buttons

---
+++ source/components/button/examples/alignVisually.blade.php
@@ -0,0 +1,22 @@
+<div class="u-display--flex u-gap--5">
+  <div class="u-display--inline-block u-border--1" style="line-height: 0em;">
+      @button([
+          'style' => 'basic',
+          'icon' => 'close',
+          'size' => 'md',
+          'classList' => ['c-button--align-visually']
+      ])
+      @endbutton
+  </div>
+
+  <div class="u-display--inline-block u-border--1" style="line-height: 0em;">
+      @button([
+          'text' => 'Button',
+          'style' => 'basic',
+          'icon' => 'close',
+          'size' => 'md',
+          'classList' => ['c-button--align-visually']
+      ])
+      @endbutton
+  </div>
+</div>
