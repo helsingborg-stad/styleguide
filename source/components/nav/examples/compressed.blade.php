95ce87baac158419d64ef7faa941e5f51161afa9
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-06-17 16:35:17 +0200

    feat(nav): remove compressed layouts

--- source/components/nav/examples/compressed.blade.php
+++
@@ -1,8 +0,0 @@
-@nav([
-    'items' => \MunicipioStyleGuide\Navigation::getMockedMultilevel(),
-    'classList' => ['u-position--relative'],
-    'direction' => 'vertical',
-    'includeToggle' => true,
-    'compressed' => true,
-])
-@endnav
