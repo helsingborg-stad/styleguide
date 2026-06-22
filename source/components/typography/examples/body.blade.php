43d46a145a652579e305b1f0f9051ffdf429db5b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-06-22 12:32:26 +0200

    fix: indicate what type of text format it is in examples

--- source/components/typography/examples/body.blade.php
+++ source/components/typography/examples/body.blade.php
@@ -1,16 +1,16 @@
 {{-- Document body text variants (body, p, bold) --}}
 @typography(["variant" => "body"])
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, a efficitur ligula. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+    Variant: body. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, a efficitur ligula. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
 @endtypography
 
 @typography(["variant" => "p"])
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, a efficitur ligula. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+    Variant: p. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, a efficitur ligula. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
 @endtypography
 
 @typography([ "variant" => "bold", ])
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, a efficitur ligula. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+    Variant: bold. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, a efficitur ligula. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
 @endtypography
 
 @typography([ "variant" => "lead", ])
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, a efficitur ligula. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+    Variant: lead. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, a efficitur ligula. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
 @endtypography
