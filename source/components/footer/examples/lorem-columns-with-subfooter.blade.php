20633d07ef53bbdf1c6ed865b7d92ce0a774f875
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-06-25 15:17:15 +0200

    fix: background

--- source/components/footer/examples/lorem-columns-with-subfooter.blade.php
+++ source/components/footer/examples/lorem-columns-with-subfooter.blade.php
@@ -8,6 +8,7 @@
     'id' => 'site-footer-abc',
     'classList' => ['site-footer', 's-footer'],
     'logotype' => '/assets/img/logotype-grey-full.svg',
+    'subfooterLogotype' => '/assets/img/logotype-grey-full.svg',
     'prefooter' => 'This prefooter text appears above the footer columns and uses the footer prefooter alignment token.',
     'subfooter' => [
         'flexDirection' => 'row',
