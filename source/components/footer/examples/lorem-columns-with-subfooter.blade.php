7252aeb292d6000c42cfc603a9a0cee119692a68
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-06-25 14:37:49 +0200

    fix: adjust settings.

--- source/components/footer/examples/lorem-columns-with-subfooter.blade.php
+++ source/components/footer/examples/lorem-columns-with-subfooter.blade.php
@@ -7,7 +7,8 @@
     'slotOnly' => true,
     'id' => 'site-footer-abc',
     'classList' => ['site-footer', 's-footer'],
-    'logotype' => '',
+    'logotype' => '/assets/img/logotype-grey-full.svg',
+    'prefooter' => 'This prefooter text appears above the footer columns and uses the footer prefooter alignment token.',
     'subfooter' => [
         'flexDirection' => 'row',
         'alignment' => 'center',
