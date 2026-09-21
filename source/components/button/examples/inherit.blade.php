d58473ad2969ddde34e8ba12e1ba4f63f3466ddd
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-21 17:08:01 +0200

    fix: reduce number of tokens needed for inherit buttons

---
+++ source/components/button/examples/inherit.blade.php
@@ -0,0 +1,43 @@
+<div class="o-grid">
+    <div class="o-grid-4@md">
+        <section style="background-color: var(--color--surface); border: 1px solid var(--color--surface-border); color: var(--color--surface-contrast); padding: calc(var(--base) * 3); --inherit-color-background: var(--color--surface); --inherit-color-contrast: var(--color--surface-contrast);">
+            <p><strong>Surface</strong></p>
+            <div style="display: flex; flex-wrap: wrap; gap: var(--base);">
+                @button(['text' => 'Basic', 'color' => 'inherit', 'style' => 'basic'])
+                @endbutton
+                @button(['text' => 'Outlined', 'color' => 'inherit', 'style' => 'outlined'])
+                @endbutton
+                @button(['text' => 'Filled', 'color' => 'inherit', 'style' => 'filled'])
+                @endbutton
+            </div>
+        </section>
+    </div>
+
+    <div class="o-grid-4@md">
+        <section style="background-color: var(--color--primary); color: var(--color--primary-contrast); padding: calc(var(--base) * 3); --inherit-color-background: var(--color--primary); --inherit-color-contrast: var(--color--primary-contrast);">
+            <p><strong>Primary surface</strong></p>
+            <div style="display: flex; flex-wrap: wrap; gap: var(--base);">
+                @button(['text' => 'Basic', 'color' => 'inherit', 'style' => 'basic'])
+                @endbutton
+                @button(['text' => 'Outlined', 'color' => 'inherit', 'style' => 'outlined'])
+                @endbutton
+                @button(['text' => 'Filled', 'color' => 'inherit', 'style' => 'filled'])
+                @endbutton
+            </div>
+        </section>
+    </div>
+
+    <div class="o-grid-4@md">
+        <section style="background-color: var(--color--secondary); color: var(--color--secondary-contrast); padding: calc(var(--base) * 3); --inherit-color-background: var(--color--secondary); --inherit-color-contrast: var(--color--secondary-contrast);">
+            <p><strong>Secondary surface</strong></p>
+            <div style="display: flex; flex-wrap: wrap; gap: var(--base);">
+                @button(['text' => 'Basic', 'color' => 'inherit', 'style' => 'basic'])
+                @endbutton
+                @button(['text' => 'Outlined', 'color' => 'inherit', 'style' => 'outlined'])
+                @endbutton
+                @button(['text' => 'Filled', 'color' => 'inherit', 'style' => 'filled'])
+                @endbutton
+            </div>
+        </section>
+    </div>
+</div>
